doolii/epoch

Unit test mining first block after genesis

apps/aecore/test/aec_mining_tests.erl
@@ -172,4 +172,63 @@ mine_block_test_() ->
      ]
      } || PoWMod <- PoWModules].
 
+mine_block_from_genesis_test_() ->
+    PoWModules = [aec_pow_sha256, aec_pow_cuckoo],
+    [{setup,
+      fun() ->
+              meck:new(aec_chain, [passthrough]),
+              meck:new(aec_pow, [passthrough]),
+              meck:expect(
+                aec_chain, top,
+                fun() ->
+                        %% TODO Remove this workaround once
+                        %% `aec_chain:top/0` returns state trees.
+                        GB = aec_genesis:genesis_block_as_deserialized_from_network(),
+                        {ok, B} = meck:passthrough([]),
+                        {GB, _} = {B, {B, GB}},
+                        {ok, B#block{trees = (aec_genesis:genesis_block())#block.trees}}
+                end),
+              meck:expect(aec_pow, pow_module, 0, PoWMod),
+              meck:expect(aec_pow, pick_nonce, 0, 1),
+              {ok, Pid} = aec_chain:start_link(aec_genesis:genesis_block()),
+              TmpKeysDir = mktempd(),
+              ok = application:ensure_started(crypto),
+              {ok, _} = aec_keys:start_link(["mypassword", TmpKeysDir]),
+              TmpKeysDir
+      end,
+      fun(TmpKeysDir) ->
+              ok = aec_keys:stop(),
+              ok = application:stop(crypto),
+              {ok, KeyFiles} = file:list_dir(TmpKeysDir),
+              %% Expect two filenames - private and public keys.
+              [KF1, KF2] = KeyFiles,
+              lists:foreach(
+                fun(F) ->
+                        AbsF = filename:absname_join(TmpKeysDir, F),
+                        {ok, _} = {file:delete(AbsF), {F, AbsF}}
+                end,
+                KeyFiles),
+              ok = file:del_dir(TmpKeysDir),
+              ok = aec_chain:stop(),
+              ?assert(meck:validate(aec_pow)),
+              ?assert(meck:validate(aec_chain)),
+              meck:unload(aec_pow),
+              meck:unload(aec_chain),
+              file:delete(TmpKeysDir)
+      end,
+      fun(TmpKeysDir) ->
+              [{"Find a new block (PoW module " ++ atom_to_list(PoWMod) ++ ")",
+                fun() ->
+                        {ok, Block} = ?TEST_MODULE:mine(),
+                        ?assertEqual(1, aec_blocks:height(Block)),
+                        ?assertEqual(1, length(Block#block.txs))
+                end}]
+      end} || PoWMod <- PoWModules].
+
+mktempd() ->
+    mktempd(os:type()).
+
+mktempd({unix, _}) ->
+    lib:nonl(?cmd("mktemp -d")).
+
 -endif.

aeternity/aeternity

apps/aecore/test/aec_mining_tests.erl
@@ -175,4 +175,63 @@ mine_block_test_() ->
       ]
      } || PoWMod <- PoWModules].
 
+mine_block_from_genesis_test_() ->
+    PoWModules = [aec_pow_sha256, aec_pow_cuckoo],
+    [{setup,
+      fun() ->
+              meck:new(aec_chain, [passthrough]),
+              meck:new(aec_pow, [passthrough]),
+              meck:expect(
+                aec_chain, top,
+                fun() ->
+                        %% TODO Remove this workaround once
+                        %% `aec_chain:top/0` returns state trees.
+                        GB = aec_block_genesis:genesis_block_as_deserialized_from_network(),
+                        {ok, B} = meck:passthrough([]),
+                        {GB, _} = {B, {B, GB}},
+                        {ok, B#block{trees = (aec_block_genesis:genesis_block())#block.trees}}
+                end),
+              meck:expect(aec_pow, pow_module, 0, PoWMod),
+              meck:expect(aec_pow, pick_nonce, 0, 1),
+              {ok, Pid} = aec_chain:start_link(aec_block_genesis:genesis_block()),
+              TmpKeysDir = mktempd(),
+              ok = application:ensure_started(crypto),
+              {ok, _} = aec_keys:start_link(["mypassword", TmpKeysDir]),
+              TmpKeysDir
+      end,
+      fun(TmpKeysDir) ->
+              ok = aec_keys:stop(),
+              ok = application:stop(crypto),
+              {ok, KeyFiles} = file:list_dir(TmpKeysDir),
+              %% Expect two filenames - private and public keys.
+              [KF1, KF2] = KeyFiles,
+              lists:foreach(
+                fun(F) ->
+                        AbsF = filename:absname_join(TmpKeysDir, F),
+                        {ok, _} = {file:delete(AbsF), {F, AbsF}}
+                end,
+                KeyFiles),
+              ok = file:del_dir(TmpKeysDir),
+              ok = aec_chain:stop(),
+              ?assert(meck:validate(aec_pow)),
+              ?assert(meck:validate(aec_chain)),
+              meck:unload(aec_pow),
+              meck:unload(aec_chain),
+              file:delete(TmpKeysDir)
+      end,
+      fun(TmpKeysDir) ->
+              [{"Find a new block (PoW module " ++ atom_to_list(PoWMod) ++ ")",
+                fun() ->
+                        {ok, Block} = ?TEST_MODULE:mine(),
+                        ?assertEqual(1, aec_blocks:height(Block)),
+                        ?assertEqual(1, length(Block#block.txs))
+                end}]
+      end} || PoWMod <- PoWModules].
+
+mktempd() ->
+    mktempd(os:type()).
+
+mktempd({unix, _}) ->
+    lib:nonl(?cmd("mktemp -d")).
+
 -endif.