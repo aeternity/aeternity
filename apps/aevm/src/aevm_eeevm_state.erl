154f997c0a0eed495f709e59e595e2b09d45142c
Erik Stenman <happi@happihacking.com>
2017-10-04 18:16:58 +0200

    Hanlde Stack operations and vmPushDupSwapTest Handle CALLER, CALL, GAS, SUICIDE. Not well tested. External framework not in place. Handle DUP1 and pop on empty stack. Handle and test all dup instructions. Handle and test all push instructions. Handle and test push out of bounds. Handle and test all swap instructions. Handle JUMPI.

--- apps/aevm/src/aevm_eeevm_state.erl
+++ apps/aevm/src/aevm_eeevm_state.erl
@@ -8,17 +8,20 @@
 %%% Created : 2 Oct 2017
 %%%-------------------------------------------------------------------
 
--export([ code/1
+-export([ caller/1
+	, code/1
 	, cp/1
 	, data/1
 	, gas/1
 	, init/1
 	, mem/1
+	, set_call/2
 	, set_code/2
 	, set_cp/2
 	, set_gas/2
 	, set_mem/2
 	, set_out/2
+	, set_selfdestruct/2
 	, set_stack/2
 	, set_storage/2
 	, stack/1
@@ -31,6 +34,7 @@ init(Spec) ->
     Opts = maps:get(opts, Spec),
     Code = maps:get(code, Exec),
     Data = maps:get(data, Exec),
+    Caller = maps:get(caller, Exec),
     Gas = maps:get(gas, Exec),
     Trace = maps:get(trace, Opts, false),
     TraceFun = maps:get(trace_fun, Opts, fun(S,A) -> io:format(S,A) end),
@@ -40,6 +44,7 @@ init(Spec) ->
      , storage   => #{}   %% For now. Should be permanent.
      , code      => Code
      , data      => Data
+     , caller    => Caller
      , gas       => Gas
      , out       => <<>>
      , cp        => 0
@@ -49,6 +54,7 @@ init(Spec) ->
      }.
 
 
+caller(State)    -> maps:get(caller, State).
 cp(State)        -> maps:get(cp, State).
 code(State)      -> maps:get(code, State).
 data(State)      -> maps:get(data, State).
@@ -62,6 +68,7 @@ trace(State)     -> maps:get(trace, State).
 trace_fun(State) -> maps:get(trace_fun, State).
 
 
+set_call(Value, State)    -> maps:put(call, Value, State).
 set_cp(Value, State)      -> maps:put(cp, Value, State).
 set_code(Value, State)    -> maps:put(code, Value, State).
 set_stack(Value, State)   -> maps:put(stack, Value, State).
@@ -69,6 +76,7 @@ set_mem(Value, State)     -> maps:put(memory, Value, State).
 set_out(Value, State)     -> maps:put(out, Value, State).
 set_gas(Value, State)     -> maps:put(gas, Value, State).
 set_storage(Value, State) -> maps:put(storage, Value, State).
+set_selfdestruct(Value, State) -> maps:put(selfdestruct, Value, State).
   
 
 add_trace(T, State) ->
