fb38a23862e98cf03b3dba93645cf838f08b19de
Tobias Lindahl <tobias@happihacking.com>
2017-10-05 10:59:34 +0200

    Make eeevm unaware of the test framework
    Also, some refactoring in the test framework

--- apps/aevm/test/aevm_test_utils.erl
+++ apps/aevm/test/aevm_test_utils.erl
@@ -6,71 +6,129 @@
 
 -module(aevm_test_utils).
 
--export([ get_config/2
-        , get_post_storage/1
-        , is_external_available/0
+-export([ testcase_generate/2
+        , testcase_generate/3
         ]).
+
+-define(EUNIT_NOAUTO, true).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(opt_format(___Opts__, ___Fmt___, ___Args___),
+        case maps:get(trace, ___Opts__, false) of
+            true -> ?debugFmt(___Fmt___, ___Args___);
+            _    -> ok
+        end).
+
+-define(wrap_run(___Expr___),
+        try ___Expr___
+        catch ___X___:___Y___ ->
+                error({___X___, ___Y___, erlang:get_stacktrace()})
+        end).
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
-is_external_available() ->
-    case external_dir() of
-      false -> false;
-      DirPath ->
-        VMTests = filename:join([DirPath, "etherium_tests", "VMTests"]),
-        filelib:is_dir(VMTests)
+-spec testcase_generate(string(), [atom()]) -> EunitTestSpec :: any().
+
+testcase_generate(Path, Tests) ->
+  testcase_generate(Path, Tests, #{}).
+
+-spec testcase_generate(string(), [atom()], function() | map()) ->
+                               EunitTestSpec :: any().
+
+testcase_generate(Path, Tests, Opts) ->
+    case is_external_available() of
+        true ->
+            {foreachx
+            , fun get_config/1
+            , fun(_, _) -> ok end
+            , [{{Path, TestName, expand_opts(Opts, TestName)}, fun testcase/2}
+               || TestName <- Tests]
+            };
+        false ->
+            []
     end.
 
-get_config(DirPath, TestName) ->
-    Bin    = get_config_file(DirPath, TestName),
-    Json   = jsx:decode(Bin, [return_maps, {labels, attempt_atom}]),
-    Config = build_config(Json),
-    maps:get(TestName, Config).
+%%====================================================================
+%% Internal functions
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Running the actual test case
+
+testcase({Path, Name, Opts}, Spec) ->
+    { Path ++ "/" ++ atom_to_list(Name)
+    , fun() ->
+              ?opt_format(Opts, "Setting up state: ~w~n", [Name]),
+              InitState = init_state(Spec, Opts),
+              ?opt_format(Opts, "Running: ~w~n", [Name]),
+              State = ?wrap_run(run_eeevm(InitState)),
+              ?opt_format(Opts, "Checking: ~w~n", [Name]),
+              PostStorageSpec = get_post_storage(Spec),
+              Storage = aevm_eeevm_state:storage(State),
+              ?opt_format(Opts, "State of ~w: ~p~n", [Name, State]),
+              ?assertEqual(PostStorageSpec, Storage)
+      end
+    }.
 
 get_post_storage(#{post := Post}) ->
     %% TODO: The hash needs to be checked
     [{_, #{storage := Storage}}] = maps:to_list(Post),
     Storage.
 
-%%====================================================================
-%% Internal functions
-%%====================================================================
+%%--------------------------------------------------------------------
+%% Interfacing to aevm_eevm
+
+init_state(Spec, Opts) ->
+    aevm_eeevm_state:init(Spec, Opts).
+
+run_eeevm(State) ->
+    try aevm_eeevm:eval(State) of
+	NewState ->
+	    %% Executed to completion
+	    NewState
+	    %% TODO: Possibly flag callouts here
+    catch throw:{Error, ErrorState} ->
+	    %% Handle execution exceptions gracefully here.
+	    io:format("Error ~p~n", [Error]),
+	    ErrorState
+    end.
+
+%%--------------------------------------------------------------------
+%% Expanding the opts that is sent to the aevm_eeevm:run/1
+
+expand_opts(#{} = Map, TestName) when is_atom(TestName) ->
+    Map;
+expand_opts(Fun, TestName) when is_function(Fun, 1), is_atom(TestName) ->
+    Fun(TestName);
+expand_opts(Fun, TestName) ->
+    error({illegal_test_specs, Fun, TestName}).
+
+%%--------------------------------------------------------------------
+%% Check if the external tests are available, and only generate the
+%% tests if the config files are there. We don't want to fail if there
+%% are no tests to be run.
+
+is_external_available() ->
+    case external_dir() of
+      false -> false;
+      DirPath ->
+        VMTests = filename:join([DirPath, "etherium_tests"]),
+        filelib:is_dir(VMTests)
+    end.
 
 external_dir() ->
     os:getenv("AEVM_EXTERNAL_TEST_DIR").
 
-atom_or_binint_to_int(Bin) when is_binary(Bin) ->
-    binint_to_int(Bin);
-atom_or_binint_to_int(Atom) when is_atom(Atom) ->
-    binint_to_int(atom_to_binary(Atom, utf8)).
+%%--------------------------------------------------------------------
+%% Read a test config file
 
-to_atom(Atom) when is_atom(Atom) ->
-    Atom;
-to_atom(Bin) when is_binary(Bin) ->
-    binary_to_atom(Bin, utf8).
-
-
-binint_to_int(<<"0x", Bin/binary>>) ->
-    binary_to_integer(Bin, 16);
-binint_to_int(Bin) when is_binary(Bin) ->
-    binary_to_integer(Bin, 16).
-
-binint_to_bin(<<"0x", Bin/binary>>) ->
-    << <<(hex_to_int(X)):4>> || <<X:8>> <= Bin>>;
-binint_to_bin(<<"0", _/binary>> = Bin) ->
-    %% Don't know what to do.
-    %% Is this an attempt to pad?
-    error({unexpected, Bin});
-binint_to_bin(Bin) when is_binary(Bin) ->
-    Int = binary_to_integer(Bin),
-    binary:encode_unsigned(Int).
-
-hex_to_int(X) when $A =< X, X =< $F -> 10 + X - $A;
-hex_to_int(X) when $a =< X, X =< $f -> 10 + X - $a;
-hex_to_int(X) when $0 =< X, X =< $9 -> X - $0.
-
+get_config({DirPath, TestName,_Opts}) ->
+    Bin    = get_config_file(DirPath, TestName),
+    Json   = jsx:decode(Bin, [return_maps, {labels, attempt_atom}]),
+    Config = build_config(Json),
+    maps:get(TestName, Config).
 
 get_config_file(DirPath, TestName) ->
     FileName = config_filename(DirPath, TestName),
@@ -84,6 +142,11 @@ config_filename(DirPath, TestName) ->
                   , "etherium_tests"
                   , DirPath
                   , atom_to_list(TestName) ++ ".json"]).
+
+%%--------------------------------------------------------------------
+%% Build the test config by simultaneously traverse the defined config
+%% structure and the json config from file. The json values are
+%% translated to the internal representation.
 
 build_config(ConfigIn) ->
     try build_config(config_structure(), ConfigIn)
@@ -131,7 +194,6 @@ build_config_body([_] = Body, Spec) when is_list(Spec) ->
 build_config_body(unclear, X) ->
     X.
 
-
 build_data_array(Data) ->
     build_data_array(Data, <<>>).
 
@@ -147,6 +209,9 @@ build_data_array([Int|T], Acc) when is_integer(Int) ->
     build_data_array(T, <<Acc/binary, Int:64>>);
 build_data_array([], Acc) ->
     Acc.
+
+%%--------------------------------------------------------------------
+%% Define the structure of a test config.
 
 -define(OPTIONAL_CONFIG_FIELDS, [ {callcreates, default, []}
                                 , {gas, default, <<"0x00">>}
@@ -209,3 +274,36 @@ config_structure() ->
                      }
            }
      }.
+
+%%--------------------------------------------------------------------
+%% Primitive operations for building the test config.
+
+atom_or_binint_to_int(Bin) when is_binary(Bin) ->
+    binint_to_int(Bin);
+atom_or_binint_to_int(Atom) when is_atom(Atom) ->
+    binint_to_int(atom_to_binary(Atom, utf8)).
+
+to_atom(Atom) when is_atom(Atom) ->
+    Atom;
+to_atom(Bin) when is_binary(Bin) ->
+    binary_to_atom(Bin, utf8).
+
+
+binint_to_int(<<"0x", Bin/binary>>) ->
+    binary_to_integer(Bin, 16);
+binint_to_int(Bin) when is_binary(Bin) ->
+    binary_to_integer(Bin, 16).
+
+binint_to_bin(<<"0x", Bin/binary>>) ->
+    << <<(hex_to_int(X)):4>> || <<X:8>> <= Bin>>;
+binint_to_bin(<<"0", _/binary>> = Bin) ->
+    %% Don't know what to do.
+    %% Is this an attempt to pad?
+    error({unexpected, Bin});
+binint_to_bin(Bin) when is_binary(Bin) ->
+    Int = binary_to_integer(Bin),
+    binary:encode_unsigned(Int).
+
+hex_to_int(X) when $A =< X, X =< $F -> 10 + X - $A;
+hex_to_int(X) when $a =< X, X =< $f -> 10 + X - $a;
+hex_to_int(X) when $0 =< X, X =< $9 -> X - $0.
