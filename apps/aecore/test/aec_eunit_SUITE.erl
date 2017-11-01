f8398f2770a6b7018f86d6f31cd54cd4d62f5d8a
Ulf Wiger <ulf@feuerlabs.com>
2017-11-01 11:38:01 +0100

    Wrap aecore EUnit tests as CT suite

---
+++ apps/aecore/test/aec_eunit_SUITE.erl
@@ -0,0 +1,38 @@
+-module(aec_eunit_SUITE).
+
+-export([all/0, groups/0, suite/0,
+         init_per_suite/1, end_per_suite/1,
+         init_per_group/2, end_per_group/2,
+         init_per_testcase/2, end_per_testcase/2
+        ]).
+
+-include_lib("common_test/include/ct.hrl").
+-compile({parse_transform, ct_eunit_xform}).
+
+all() ->
+    [{group, eunit}].
+
+groups() ->
+    [].
+
+suite() ->
+    [].
+
+init_per_suite(Config) ->
+    eunit:start(),
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+init_per_group(_Grp, Config) ->
+    Config.
+
+end_per_group(_Grp, _Config) ->
+    ok.
+
+init_per_testcase(_TC, Config) ->
+    Config.
+
+end_per_testcase(_TC, _Config) ->
+    ok.
