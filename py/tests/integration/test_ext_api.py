cee4ddb76c7edca70329747fd6fbcfd4f2425dfb
Dimitar Ivanov <dimitar.p.ivanov@gmail.com>
2017-11-10 11:37:50 +0200

    PT-152642152 Test persistence [Finishes #152642152]

--- py/tests/integration/test_ext_api.py
+++ py/tests/integration/test_ext_api.py
@@ -5,9 +5,11 @@ import common
 import time
 
 from py.tests.swagger_client.models.ping import Ping 
+from py.tests import chain_downloader
 
-settings = common.test_settings(__name__)
+settings = common.test_settings(__name__.split(".")[-1])
 node = settings['node_to_test']
+chain_data_file = settings['chain_data']
 
 def setup_func():
     if common.should_start_node(node):
@@ -65,8 +67,62 @@ def test_block_post():
     Test case for posting a new block
     """
     api = common.external_api(node)
-    common.post_fake_block(api)
+    top = api.get_top()
+    ## ensure there is only genesis block
+    assert_equals(top.height, 0)
+
+    premined_blocks = chain_downloader.load_from_file(chain_data_file)
+    block = chain_downloader.get_block(premined_blocks, height=1)
+    res = api.post_block(block)
     time.sleep(1)
     top = api.get_top()
     assert_equals(top.height, 1)
+    assert_equals(top.pow, str(block.pow))
+    assert_equals(top.state_hash, block.state_hash)
+    assert_equals(top.txs_hash, block.txs_hash)
+    assert_equals(top.version, block.version)
+
+@with_setup(setup_func, teardown_func)
+def test_block_post_3():
+    """
+    Test case for posting 3 blocks
+    """
+    api = common.external_api(node)
+    top = api.get_top()
+    ## ensure there is only genesis block
+    assert_equals(top.height, 0)
+
+    premined_blocks = chain_downloader.load_from_file(chain_data_file)
+    for i in range(1, 4):
+        block = chain_downloader.get_block(premined_blocks, height=i)
+        api.post_block(block)
+        time.sleep(1)
+        top = api.get_top()
+        # ensure block is accepted
+        assert_equals(top.height, i)
+        assert_equals(top.pow, str(block.pow))
+        assert_equals(top.state_hash, block.state_hash)
+        assert_equals(top.txs_hash, block.txs_hash)
+        assert_equals(top.version, block.version)
+
+@with_setup(setup_func, teardown_func)
+def test_block_post_wrong_pow():
+    """
+    Test rejecting a block with wrong PoW 
+    """
+    api = common.external_api(node)
+    top = api.get_top()
+    ## ensure there is only genesis block
+    assert_equals(top.height, 0)
+
+    premined_blocks = chain_downloader.load_from_file(chain_data_file)
+    block1 = chain_downloader.get_block(premined_blocks, height=1)
+    block2 = chain_downloader.get_block(premined_blocks, height=2)
+
+    # modify the pow of block1 to be invalid against the header
+    block1.pow = block2.pow
+    res = api.post_block(block1)
+    time.sleep(1)
+    top = api.get_top()
+    assert_equals(top.height, 0)
 
--- py/tests/integration/test_use_cases.py
+++ py/tests/integration/test_use_cases.py
@@ -1,8 +1,14 @@
 # coding: utf-8
 
+import tempfile
+import os
+import shutil
 from nose.tools import assert_equals, assert_not_equals, with_setup
 import common
 from waiting import wait
+from py.tests import chain_downloader
+
+settings = common.test_settings(__name__.split(".")[-1])
 
 def test_syncing():
     """
@@ -10,30 +16,82 @@ def test_syncing():
     Alice should be able to connect to peers on startup and download the
     blockchain up to the current height.
     """
+    test_settings = settings["test_syncing"]
+    chain_data_file = test_settings["chain_data"]
     # start Bob's node
-    bob_node = "dev1"
+    bob_node = test_settings["nodes"]["bob"]
     common.start_node(bob_node)
     bob_api = common.external_api(bob_node)
 
-    # Insert a block in Bob's chain
-    common.post_fake_block(bob_api)
-    wait(lambda: bob_api.get_top().height == 1, timeout_seconds=3, sleep_seconds=0.5)
+    # Insert some blocks in Bob's chain
+    blocks_to_post = test_settings["blocks_to_post"]
+    post_blocks(bob_api, blocks_to_post, chain_data_file)
     bob_top = bob_api.get_top()
-    assert_equals(bob_top.height, 1)
-    # Now Bob has 2 blocks: with height 0 (genesis) and 1
+    assert_equals(bob_top.height, blocks_to_post)
+    # Now Bob has blocks_to_post blocks
 
     # start Alice's node and let it connect with Bob's
-    alice_node = "dev2"
+    alice_node = test_settings["nodes"]["alice"]
     common.start_node(alice_node)
     alice_api = common.external_api(alice_node)
-    wait(lambda: alice_api.get_top().height == 1, timeout_seconds=3, sleep_seconds=0.5)
+    wait(lambda: alice_api.get_top().height == blocks_to_post, timeout_seconds=3, sleep_seconds=0.5)
     alice_top = alice_api.get_top()
-    assert_equals(alice_top.height, 1)
-    # Alice has 2 blocks, too
+    assert_equals(alice_top.height, blocks_to_post)
+    # Alice has blocks_to_post blocks, too
 
-    # ensure that both nodes have the same hash on block with height=1
+    # ensure that both nodes have the same hash on top 
     assert_equals(alice_top.hash, bob_top.hash)
 
     # stop both nodes
     common.stop_node(bob_node)
     common.stop_node(alice_node)
+
+def test_persistence():
+    """
+    Test persistence:
+    Bob's downloaded blockchain should persist between restarts. He should
+    only download updates to his blockchain when his node starts.
+    """
+    test_settings = settings["test_persistence"]
+    chain_data_file = test_settings["chain_data"]
+
+    # prepare a dir to hold the config and DB files 
+    root_dir = tempfile.mkdtemp()
+    sys_config = os.path.join(root_dir, "sys.config")
+    f = open(sys_config, "w")
+
+    # this should be moved to an YAML once we have YAML node configuration
+    f.write('[{aecore, [{db_path, "' + root_dir + '"},{persist, true}]}].')
+    f.close()
+
+    bob_node = test_settings["nodes"]["bob"]
+    common.start_node(bob_node, sys_config)
+    bob_api = common.external_api(bob_node)
+    
+    # there is no old persisted data
+    bob_top = bob_api.get_top()
+    assert_equals(bob_top.height, 0)
+
+    # Insert some blocks in Bob's chain
+    blocks_to_post = test_settings["blocks_to_post"]
+    post_blocks(bob_api, blocks_to_post, chain_data_file)
+    bob_top = bob_api.get_top()
+    assert_equals(bob_top.height, blocks_to_post)
+    # Now Bob has blocks_to_post blocks
+
+    common.stop_node(bob_node)
+
+    common.start_node(bob_node, sys_config)
+    bob_new_top = bob_api.get_top()
+    assert_equals(bob_new_top.height, blocks_to_post)
+    assert_equals(bob_top.hash, bob_new_top.hash)
+
+    shutil.rmtree(root_dir)
+    common.stop_node(bob_node)
+
+def post_blocks(api, blocks_cnt, chain_data_file):
+    premined_blocks = chain_downloader.load_from_file(chain_data_file)
+    for i in range(1, blocks_cnt + 1):
+        block = chain_downloader.get_block(premined_blocks, height=i)
+        api.post_block(block)
+        wait(lambda: api.get_top().height == i, timeout_seconds=3, sleep_seconds=0.25)
