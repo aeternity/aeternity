41ad57e5edfb84cfaf9b5219ce5451ae76c86453
Ulf Norell <ulf.norell@quviq.com>
2017-11-17 15:59:34 +0100

    Added a test utility module for aering tests

---
+++ apps/aering/test/aer_test_utils.erl
@@ -0,0 +1,22 @@
+%%% -*- erlang-indent-level:4; indent-tabs-mode: nil -*-
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc Test utilities for the Ring language tests.
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+
+-module(aer_test_utils).
+
+-export([read_contract/1]).
+
+%% TODO: does eunit have some path machinery?
+contract_path() ->
+    "apps/aering/test/contracts".
+
+%% Read a contract file from the test/contracts directory.
+-spec read_contract(string() | atom()) -> string().
+read_contract(Name) ->
+    {ok, Bin} = file:read_file(filename:join(contract_path(), lists:concat([Name, ".aer"]))),
+    binary_to_list(Bin).
+
