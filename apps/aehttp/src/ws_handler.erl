f43851b3319dc4e1ee95bdb06371571d79ef983f
Dimitar Ivanov <dimitar.p.ivanov@gmail.com>
2017-12-07 16:27:38 +0200

    Websockets using aec_events [Finishes #153476145]

--- apps/aehttp/src/ws_handler.erl
+++ apps/aehttp/src/ws_handler.erl
@@ -11,6 +11,9 @@
 
 -define(GPROC_KEY, {p, l, {?MODULE, broadcast}}).
 
+-define(SUBSCRIBE_EVENTS, [block_created %% node mined a block
+                          ]).
+
 init({tcp, http}, _Req, _Opts) ->
     {upgrade, protocol, cowboy_websocket}.
 
@@ -18,6 +21,11 @@ websocket_init(_TransportName, Req, _Opts) ->
     case jobs:ask(ws_handlers_queue) of
         {ok, JobId} ->
             gproc:reg(?GPROC_KEY),
+            lists:foreach(
+                fun(Event) ->
+                    aec_events:subscribe(Event)
+                end,
+                ?SUBSCRIBE_EVENTS),
             {ok, Req, JobId};
         {error, rejected} ->
             {shutdown, Req}
@@ -29,30 +37,21 @@ websocket_handle({text, MsgBin}, Req, State) ->
 websocket_handle(_Data, Req, State) ->
     {ok, Req, State}.
 
-websocket_info({send, SenderName, Action, Payload}, Req, State) ->
-    MsgWithoutPayload = #{origin =>SenderName, action => Action},
-    EmptyPayload =
-        case Payload of
-            _ when is_list(Payload) ->
-                length(Payload) == 0;
-            _ when is_map(Payload) ->
-                maps:size(Payload) == 0
-        end,
-    Msg =
-        case EmptyPayload of
-            true ->
-                MsgWithoutPayload;
-            false ->
-                maps:put(payload, Payload, MsgWithoutPayload)
-        end,
+websocket_info({gproc_ps_event, Event, Data}, Req, State) ->
+    Msg = create_message_from_event(Event, Data),
     {reply, {text, jsx:encode(Msg)}, Req, State};
-websocket_info(_Info, Req, State) ->
+websocket_info({send, SenderName, Action, Payload}, Req, State) ->
+    Msg = create_message(SenderName, Action, Payload),
+    {reply, {text, jsx:encode(Msg)}, Req, State};
+websocket_info(Info, Req, State) ->
+    lager:info("Unhandled message ~p", [Info]),
     {ok, Req, State}.
 
 websocket_terminate(_Reason, _Req, JobId) ->
     jobs:done(JobId),
     ok.
 
+%% Should be used only in case when aec_events is not appropriate
 broadcast(SenderName, Action) ->
     broadcast(SenderName, Action, []).
 broadcast(SenderName, Action, Payload) ->
@@ -64,3 +63,28 @@ broadcast(SenderName, Action, Payload) ->
 
 send_msg(WsPid, SenderName, Action, Payload) ->
     WsPid ! {send, SenderName, Action, Payload}.
+
+create_message(SenderName, Action, Payload) ->
+    MsgWithoutPayload = #{origin => SenderName,
+                          action => Action},
+    EmptyPayload =
+        case Payload of
+            _ when is_list(Payload) ->
+                Payload == [];
+            _ when is_map(Payload) ->
+                maps:size(Payload) == 0
+        end,
+    case EmptyPayload of
+        true ->
+            MsgWithoutPayload;
+        false ->
+            maps:put(payload, Payload, MsgWithoutPayload)
+    end.
+
+create_message_from_event(block_created, #{info := Block}) ->
+    BlockHeight = aec_blocks:height(Block),
+    {ok, BlockHash} = aec_blocks:hash_internal_representation(Block),
+    Payload =
+        [{height, BlockHeight},
+         {hash, base64:encode(BlockHash)}],
+    create_message(miner, mined_block, Payload).
