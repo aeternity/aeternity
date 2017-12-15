4dc4b1ac89de1164cd6c9f049e1db0a28dd0c7ce
Ulf Norell <ulf.norell@quviq.com>
2017-12-15 11:28:42 +0100

    Update all example contracts to new syntax

--- apps/aering/test/aer_parser_tests.erl
+++ apps/aering/test/aer_parser_tests.erl
@@ -67,7 +67,7 @@ simple_contracts_test_() ->
      %% Parse tests of example contracts
      [ {lists:concat(["Parse the ", Contract, " contract."]),
         fun() -> roundtrip_contract(Contract) end}
-        || Contract <- [counter, voting, all_syntax] ]
+        || Contract <- [counter, voting, all_syntax, '05_greeter', aeproof, multi_sig, simple_storage, withdrawal] ]
     }.
 
 parse_contract(Name) ->
