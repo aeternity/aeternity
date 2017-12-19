8f7a41ffb421266596af86425bb693c75fe77d91
Tobias Lindahl <tobias@happihacking.se>
2017-12-19 15:29:40 +0100

    Break out test utils for oracles

---
+++ apps/aeoracle/test/aeo_test_utils.erl
@@ -0,0 +1,170 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc Test utils for oracles
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aeo_test_utils).
+
+-export([ new_state/0
+        , priv_key/2
+        , query_tx/3
+        , query_tx/4
+        , register_tx/2
+        , register_tx/3
+        , response_tx/4
+        , response_tx/5
+        , set_account_balance/3
+        , set_trees/2
+        , setup_new_account/1
+        , setup_new_account/3
+        , trees/1
+        ]).
+
+-include_lib("apps/aeoracle/include/oracle_txs.hrl").
+
+%%%===================================================================
+%%% Test state
+%%%===================================================================
+
+new_state() ->
+    #{}.
+
+trees(#{} = S) ->
+    maps:get(trees, S, aec_trees:new()).
+
+set_trees(Trees, S) ->
+    S#{trees => Trees}.
+
+insert_key_pair(Pub, Priv, S) ->
+    Old = key_pairs(S),
+    S#{key_pairs => Old#{Pub => Priv}}.
+
+key_pairs(S) -> maps:get(key_pairs, S, #{}).
+
+next_nonce(PubKey, S) ->
+    Account = aec_accounts_trees:get(PubKey, aec_trees:accounts(trees(S))),
+    aec_accounts:nonce(Account) + 1.
+
+priv_key(PubKey, State) ->
+    maps:get(PubKey, key_pairs(State)).
+
+%%%===================================================================
+%%% Register tx
+%%%===================================================================
+
+register_tx(PubKey, State) ->
+    register_tx(PubKey, #{}, State).
+
+register_tx(PubKey, Spec0, State) ->
+    Spec = maps:merge(register_tx_default_spec(PubKey, State), Spec0),
+    #oracle_register_tx{ account   = PubKey
+                       , nonce     = maps:get(nonce, Spec)
+                       , ttl       = maps:get(ttl, Spec)
+                       , fee       = maps:get(fee, Spec)
+                       , query_fee = maps:get(query_fee, Spec)
+                       }.
+
+register_tx_default_spec(PubKey, State) ->
+    #{ ttl       => {delta, 10}
+     , fee       => 5
+     , nonce     => try next_nonce(PubKey, State) catch _:_ -> 0 end
+     , query_fee => 5
+     }.
+
+%%%===================================================================
+%%% Query tx
+%%%===================================================================
+
+query_tx(PubKey, OracleKey, State) ->
+    query_tx(PubKey, OracleKey, #{}, State).
+
+query_tx(PubKey, OracleKey, Spec0, State) ->
+    Spec = maps:merge(query_tx_default_spec(PubKey, State), Spec0),
+    #oracle_query_tx{ sender = PubKey
+                    , nonce  = maps:get(nonce, Spec)
+                    , oracle = OracleKey
+                    , query  = maps:get(query, Spec)
+                    , query_fee = maps:get(query_fee, Spec)
+                    , query_ttl = maps:get(query_ttl, Spec)
+                    , response_ttl = maps:get(response_ttl, Spec)
+                    , fee = maps:get(fee, Spec)
+                    }.
+
+query_tx_default_spec(PubKey, State) ->
+    #{ query        => <<"Hello world">>
+     , query_fee    => 5
+     , query_ttl    => {delta, 50}
+     , response_ttl => {delta, 25}
+     , fee          => 5
+     , nonce        => try next_nonce(PubKey, State) catch _:_ -> 0 end
+     }.
+
+%%%===================================================================
+%%% Response tx
+%%%===================================================================
+
+response_tx(PubKey, ID, Response, State) ->
+    response_tx(PubKey, ID, Response, #{}, State).
+
+response_tx(PubKey, ID, Response, Spec0, State) ->
+    Spec = maps:merge(response_tx_default_spec(PubKey, State), Spec0),
+    #oracle_response_tx{ oracle         = PubKey
+                       , nonce          = maps:get(nonce, Spec)
+                       , interaction_id = ID
+                       , response       = Response
+                       , fee            = maps:get(fee, Spec)
+                       }.
+
+response_tx_default_spec(PubKey, State) ->
+    #{ nonce    => try next_nonce(PubKey, State) catch _:_ -> 0 end
+     , fee      => 3
+     }.
+
+
+%%%===================================================================
+%%% Accounts
+%%%===================================================================
+
+setup_new_account(State) ->
+    setup_new_account(1000, 1, State).
+
+setup_new_account(Balance, Height, State) ->
+    {PubKey, PrivKey} = new_key_pair(),
+    State1            = insert_key_pair(PubKey, PrivKey, State),
+    State2            = set_account(aec_accounts:new(PubKey, Balance, Height), State1),
+    {PubKey, State2}.
+
+set_account_balance(PubKey, NewBalance, State) ->
+    A        = get_account(PubKey, State),
+    Balance  = aec_accounts:balance(A),
+    Height   = aec_accounts:height(A),
+    Nonce    = aec_accounts:nonce(A),
+    {ok, A1} = aec_accounts:spend(A, Balance, Nonce, Height),
+    {ok, A2} = aec_accounts:earn(A1, NewBalance, Height),
+    set_account(A2, State).
+
+get_account(PubKey, State) ->
+    aec_accounts_trees:get(PubKey, aec_trees:accounts(trees(State))).
+
+set_account(Account, State) ->
+    Trees   = trees(State),
+    AccTree = aec_accounts_trees:enter(Account, aec_trees:accounts(Trees)),
+    set_trees(aec_trees:set_accounts(Trees, AccTree), State).
+
+%%%===================================================================
+%%% Keys TODO: Should move
+%%%===================================================================
+
+-define(PUB_SIZE, 65).
+-define(PRIV_SIZE, 32).
+
+new_key_pair() ->
+    {Pubkey, PrivKey} = crypto:generate_key(ecdh, crypto:ec_curve(secp256k1)),
+    {Pubkey, pad_privkey(PrivKey)}.
+
+%% crypto:generate_keys/2 gives you a binary with as many bytes as are needed to fit the
+%% private key. It does not pad with zeros.
+
+pad_privkey(Bin) ->
+    Pad = ?PRIV_SIZE - size(Bin),
+    <<0:(Pad*8), Bin/binary>>.
--- apps/aeoracle/test/aeoracle_SUITE.erl
+++ apps/aeoracle/test/aeoracle_SUITE.erl
@@ -6,7 +6,10 @@
 -module(aeoracle_SUITE).
 
 %% common_test exports
--export([all/0, init_per_suite/1, end_per_suite/1,
+-export([ all/0
+        , init_per_suite/1
+        , end_per_suite/1
+        ,
          init_per_testcase/2, end_per_testcase/2]).
 
 %% test case exports
@@ -42,181 +45,127 @@ end_per_testcase(_, _) ->
     ok.
 
 register_oracle_negative(_Cfg) ->
-    Trees = aec_trees:new(),
-    CurrHeight  = 1,
-    OracleKey   = <<"oracle">>,
+    {PubKey, S1} = aeo_test_utils:setup_new_account(aeo_test_utils:new_state()),
+    Trees        = aeo_test_utils:trees(S1),
+    CurrHeight   = 1,
 
-    RegisterTx = #oracle_register_tx{ account = OracleKey,
-                                      nonce   = 0,
-                                      query_fee = 5,
-                                      ttl = {block, 25},
-                                      fee = 5 },
+    %% Test registering a bogus account
+    BadPubKey = <<42:65/unit:8>>,
+    RTx1      = aeo_test_utils:register_tx(BadPubKey, S1),
+    {error, account_not_found} = aeo_register_tx:check(RTx1, Trees, CurrHeight),
 
-    {error, account_not_found} =
-        aeo_register_tx:check(RegisterTx, Trees, CurrHeight),
-
-    %% Add the account with 4 tokens
-    Trees1 = add_account(Trees, OracleKey, 4, CurrHeight),
-
-    %% Insufficient funds fee = 5
+    %% Insufficient funds
+    S2     = aeo_test_utils:set_account_balance(PubKey, 0, S1),
+    Trees2 = aeo_test_utils:trees(S2),
+    RTx2 = aeo_test_utils:register_tx(PubKey, S1),
     {error, insufficient_funds} =
-        aeo_register_tx:check(RegisterTx, Trees1, CurrHeight),
+        aeo_register_tx:check(RTx2, Trees2, CurrHeight),
 
-    %% Add the account with 8 tokens
-    Trees2 = add_account(Trees, OracleKey, 8, CurrHeight),
-
+    %% Test too high account nonce
+    RTx3 = aeo_test_utils:register_tx(PubKey, #{nonce => 0}, S1),
     {error, account_nonce_too_high} =
-        aeo_register_tx:check(RegisterTx, Trees2, CurrHeight),
+        aeo_register_tx:check(RTx3, Trees, CurrHeight),
 
-    RegisterTx1 = RegisterTx#oracle_register_tx{ nonce = 1, fee = 4 },
-
-    {error, too_low_fee} =
-        aeo_register_tx:check(RegisterTx1, Trees2, CurrHeight).
+    %% Test too low fee
+    RTx4 = aeo_test_utils:register_tx(PubKey, #{fee => 0}, S1),
+    {error, too_low_fee} = aeo_register_tx:check(RTx4, Trees, CurrHeight),
+    ok.
 
 register_oracle(_Cfg) ->
-    Trees = aec_trees:new(),
-    CurrHeight  = 1,
-    OracleKey   = <<"oracle                                                           ">>,
-
-    RegisterTx = #oracle_register_tx{ account = OracleKey,
-                                      nonce   = 1,
-                                      query_fee = 5,
-                                      ttl = {block, 25},
-                                      fee = 5 },
-
-    %% Add the account with enough funds
-    Trees1 = add_account(Trees, OracleKey, 20, CurrHeight),
+    {PubKey, S1} = aeo_test_utils:setup_new_account(aeo_test_utils:new_state()),
+    Tx           = aeo_test_utils:register_tx(PubKey, S1),
+    PrivKey      = aeo_test_utils:priv_key(PubKey, S1),
 
     %% Test that RegisterTX is accepted
-    SignedTx = aec_tx_sign:sign(RegisterTx, <<"pkey1">>),
-    {ok, [SignedTx], Trees2} = aec_tx:apply_signed([SignedTx], Trees1, CurrHeight),
-    Trees2.
-
+    SignedTx = aec_tx_sign:sign(Tx, PrivKey),
+    Trees    = aeo_test_utils:trees(S1),
+    Height   = 1,
+    {ok, [SignedTx], Trees1} = aec_tx:apply_signed([SignedTx], Trees, Height),
+    S2       = aeo_test_utils:set_trees(Trees1, S1),
+    {PubKey, S2}.
 
 query_oracle_negative(Cfg) ->
-    Trees = register_oracle(Cfg),
+    {OracleKey, S}  = register_oracle(Cfg),
+    {SenderKey, S2} = aeo_test_utils:setup_new_account(S),
+    Trees           = aeo_test_utils:trees(S2),
+    CurrHeight      = 3,
 
-    CurrHeight   = 3,
-    OracleKey    = <<"oracle                                                           ">>,
-    BadOracleKey = <<"oracle2                                                          ">>,
-    SenderKey    = <<"sender                                                           ">>,
+    %% Test bad sender key
+    BadSenderKey = <<42:65/unit:8>>,
+    Q1 = aeo_test_utils:query_tx(BadSenderKey, OracleKey, S2),
+    {error, account_not_found} = aeo_query_tx:check(Q1, Trees, CurrHeight),
 
-    QueryTx = #oracle_query_tx{ sender = SenderKey,
-                                nonce = 0,
-                                oracle = OracleKey,
-                                query  = <<"Hello world">>,
-                                query_fee = 5,
-                                query_ttl = {delta, 50},
-                                response_ttl = {delta, 25},
-                                fee = 3 },
+    %% Test unsufficient funds.
+    S3     = aeo_test_utils:set_account_balance(SenderKey, 0, S2),
+    Trees1 = aeo_test_utils:trees(S3),
+    Q2     = aeo_test_utils:query_tx(SenderKey, OracleKey, S2),
+    {error, insufficient_funds} = aeo_query_tx:check(Q2, Trees1, CurrHeight),
 
-    {error, account_not_found} =
-        aeo_query_tx:check(QueryTx, Trees, CurrHeight),
+    %% Test too high nonce in account
+    Q3 = aeo_test_utils:query_tx(SenderKey, OracleKey, #{nonce => 0}, S2),
+    {error, account_nonce_too_high} = aeo_query_tx:check(Q3, Trees, CurrHeight),
 
-    %% Add the account with 4 tokens
-    Trees1 = add_account(Trees, SenderKey, 4, CurrHeight),
+    %% Test too low query fee
+    Q4 = aeo_test_utils:query_tx(SenderKey, OracleKey, #{fee => 0}, S2),
+    {error, too_low_fee} = aeo_query_tx:check(Q4, Trees, CurrHeight),
 
-    %% Insufficient funds fee = 3 + 5
-    {error, insufficient_funds} =
-        aeo_query_tx:check(QueryTx, Trees1, CurrHeight),
-
-    %% Add the account with 8 tokens
-    Trees2 = add_account(Trees, SenderKey, 8, CurrHeight),
-
-    {error, account_nonce_too_high} =
-        aeo_query_tx:check(QueryTx, Trees2, CurrHeight),
-
-    QueryTx1 = QueryTx#oracle_query_tx{ nonce = 1, fee = 2 },
-
-    {error, too_low_fee} =
-        aeo_query_tx:check(QueryTx1, Trees2, CurrHeight),
-
-    QueryTx2 = QueryTx1#oracle_query_tx{ fee = 3, oracle = BadOracleKey },
-
-    {error, oracle_does_not_exist} =
-        aeo_query_tx:check(QueryTx2, Trees2, CurrHeight).
+    %% Test bad oracle key
+    BadOracleKey = <<42:65/unit:8>>,
+    Q5 = aeo_test_utils:query_tx(SenderKey, BadOracleKey, S2),
+    {error, oracle_does_not_exist} = aeo_query_tx:check(Q5, Trees, CurrHeight),
+    ok.
 
 query_oracle(Cfg) ->
-    Trees = register_oracle(Cfg),
+    {OracleKey, S1} = register_oracle(Cfg),
+    {SenderKey, S2} = aeo_test_utils:setup_new_account(S1),
+    Trees           = aeo_test_utils:trees(S2),
+    CurrHeight      = 3,
+    PrivKey         = aeo_test_utils:priv_key(SenderKey, S2),
 
-    CurrHeight  = 3,
-    OracleKey   = <<"oracle                                                           ">>,
-    SenderKey   = <<"sender                                                           ">>,
-
-    QueryTx = #oracle_query_tx{ sender = SenderKey,
-                                nonce = 1,
-                                oracle = OracleKey,
-                                query  = <<"Hello world">>,
-                                query_fee = 5,
-                                query_ttl = {delta, 50},
-                                response_ttl = {delta, 25},
-                                fee = 8 },
-
-    OIO = aeo_interaction:new(QueryTx, CurrHeight),
-
-    %% Add the account with enough funds
-    Trees1 = add_account(Trees, SenderKey, 20, CurrHeight),
-
+    Q1 = aeo_test_utils:query_tx(SenderKey, OracleKey, S2),
     %% Test that QueryTX is accepted
-    SignedTx = aec_tx_sign:sign(QueryTx, <<"pkey1">>),
-    {ok, [SignedTx], Trees2} = aec_tx:apply_signed([SignedTx], Trees1, CurrHeight),
-    {Trees2, OIO}.
+    SignedTx = aec_tx_sign:sign(Q1, PrivKey),
+    {ok, [SignedTx], Trees2} =
+        aec_tx:apply_signed([SignedTx], Trees, CurrHeight),
+    S3 = aeo_test_utils:set_trees(Trees2, S2),
+    {OracleKey, aeo_interaction:new(Q1, CurrHeight), S3}.
 
 query_response_negative(Cfg) ->
-    {Trees, OIO} = query_oracle(Cfg),
+    {OracleKey, OIO, S1} = query_oracle(Cfg),
+    Trees                = aeo_test_utils:trees(S1),
+    CurrHeight           = 5,
+    ID                   = aeo_interaction:id(OIO),
 
-    CurrHeight   = 5,
-    OracleKey    = <<"oracle                                                           ">>,
-    BadOracleKey = <<"oracle2                                                          ">>,
-
-    ResponseTx = #oracle_response_tx{ oracle         = OracleKey,
-                                      nonce          = 0,
-                                      interaction_id = aeo_interaction:id(OIO),
-                                      response       = <<42>>,
-                                      fee            = 3 },
-
-    {error, account_nonce_too_high} =
-        aeo_response_tx:check(ResponseTx, Trees, CurrHeight),
-
-    ResponseTx1 = ResponseTx#oracle_response_tx{ nonce = 2, fee = 2 },
-
-    {error, too_low_fee} =
-        aeo_response_tx:check(ResponseTx1, Trees, CurrHeight),
-
-    ResponseTx2 = ResponseTx1#oracle_response_tx{ fee = 3, oracle = BadOracleKey },
-
+    %% Test bad oracle key
+    BadOracleKey = <<42:65/unit:8>>,
+    RTx1 = aeo_test_utils:response_tx(BadOracleKey, ID, <<"42">>, S1),
     {error, oracle_does_not_match_interaction_id} =
-        aeo_response_tx:check(ResponseTx2, Trees, CurrHeight),
+        aeo_response_tx:check(RTx1, Trees, CurrHeight),
 
+    %% Test too high nonce for account
+    RTx2 = aeo_test_utils:response_tx(OracleKey, ID, <<"42">>, #{nonce => 0}, S1),
+    {error, account_nonce_too_high} =
+        aeo_response_tx:check(RTx2, Trees, CurrHeight),
+
+    %% Test fee too low
+    RTx3 = aeo_test_utils:response_tx(OracleKey, ID, <<"42">>, #{fee => 0}, S1),
+    {error, too_low_fee} = aeo_response_tx:check(RTx3, Trees, CurrHeight),
+
+    %% Test bad interaction id
     BadId = aeo_interaction:id(aeo_interaction:set_sender_nonce(42, OIO)),
-    ResponseTx3 = ResponseTx2#oracle_response_tx{ oracle = OracleKey,
-                                                  interaction_id = BadId },
-
+    RTx4 = aeo_test_utils:response_tx(OracleKey, BadId, <<"42">>, S1),
     {error, oracle_interaction_id_does_not_exist} =
-        aeo_response_tx:check(ResponseTx3, Trees, CurrHeight).
+        aeo_response_tx:check(RTx4, Trees, CurrHeight),
+    ok.
 
 query_response(Cfg) ->
-    {Trees, OIO} = query_oracle(Cfg),
-
-    CurrHeight  = 5,
-    OracleKey   = <<"oracle                                                           ">>,
-
-    ResponseTx = #oracle_response_tx{ oracle         = OracleKey,
-                                      nonce          = 2,
-                                      interaction_id = aeo_interaction:id(OIO),
-                                      response       = <<42>>,
-                                      fee            = 3 },
+    {OracleKey, OIO, S1} = query_oracle(Cfg),
+    Trees                = aeo_test_utils:trees(S1),
+    CurrHeight           = 5,
+    ID                   = aeo_interaction:id(OIO),
 
     %% Test that ResponseTX is accepted
-    SignedTx = aec_tx_sign:sign(ResponseTx, <<"pkey1">>),
-    {ok, [SignedTx], _Trees2} = aec_tx:apply_signed([SignedTx], Trees, CurrHeight).
-
-%% -- Helper functions -------------------------------------------------------
-
-add_account(Trees, AccountKey, Balance, Height) ->
-    As  = aec_trees:accounts(Trees),
-    A   = aec_accounts:new(AccountKey, Balance, Height),
-    As1 = aec_accounts_trees:enter(A, As),
-    aec_trees:set_accounts(Trees, As1).
-
-
+    RTx      = aeo_test_utils:response_tx(OracleKey, ID, <<"42">>, S1),
+    SignedTx = aec_tx_sign:sign(RTx, <<"pkey1">>),
+    {ok, [SignedTx], _Trees2} =
+        aec_tx:apply_signed([SignedTx], Trees, CurrHeight).
