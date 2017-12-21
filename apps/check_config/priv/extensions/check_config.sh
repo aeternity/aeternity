97eb9b90d71b71829d57c723cf6fd7c5060c08ef
Dincho Todorov <dincho@users.noreply.github.com>
2017-12-21 13:44:42 +0200

    Add ERTS BINDIR to PATH to run escripts

--- apps/check_config/priv/extensions/check_config.sh
+++ apps/check_config/priv/extensions/check_config.sh
@@ -2,5 +2,7 @@
 
 # Simple wrapper to run check_config
 # Relx extensions cannot be binary files but sourced bash files only
+# Also append ERTS BINDIR to PATH to run the escript
 
+PATH=$BINDIR:$PATH
 $SCRIPT_DIR/check_config $@
