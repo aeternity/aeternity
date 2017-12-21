4b24cf5c34383946e62ab47aede47b237571fe4c
Dincho Todorov <dincho@users.noreply.github.com>
2017-12-21 12:33:49 +0200

    Better warning message

--- hooks/pre_start.sh
+++ hooks/pre_start.sh
@@ -5,5 +5,5 @@ OPEN_FILES_LIMIT=$(ulimit -n)
 
 if [ "$OPEN_FILES_RECOMMENDED" -gt "$OPEN_FILES_LIMIT" ]; then
     echo "WARNING: ulimit -n is $OPEN_FILES_LIMIT; $OPEN_FILES_RECOMMENDED is the recommended minimum."
-    echo "Try 'ulimit -n $OPEN_FILES_RECOMMENDED' before starting the node."
+    echo "You are recommended to ensure the node is stopped and raise the maximum number of open files (try 'ulimit -n $OPEN_FILES_RECOMMENDED') before starting the node."
 fi
