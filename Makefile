ErikLee89/epoch

This commit introduces an implementation of the EVM and an initial parser and compiler for the Ring language.
The VM is called the EEEVM (Experimental Erlang Etherium VM).
Also known as AEVM.
The VM can handle almost all EVM operations except for
the operations on the block chain.
The VM passes over 500 EVM tests.

The parser handles a subset of the Ocaml like language Reason.
This subset will be extended with block chain operations to
giv the Ring language.

The compiler only compiles the identity function so far and
is only to be seen as a start for compilation from the Ring
AST to the AEVM.

Makefile
@@ -23,6 +23,7 @@ PYTHON = $(PYTHON_BIN)/python
 PYTHON_TESTS = $(PYTHON_DIR)/tests
 PIP = $(PYTHON_BIN)/pip
 
+export AEVM_EXTERNAL_TEST_DIR=aevm_external
 
 HTTP_APP = apps/aehttp
 SWTEMP := $(shell mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
@@ -126,6 +127,15 @@ test:
 eunit:
 	@./rebar3 do eunit $(EUNIT_TEST_FLAGS)
 
+aevm-test: aevm-test-deps
+	@./rebar3 eunit --application=aevm
+
+aevm-test-deps: $(AEVM_EXTERNAL_TEST_DIR)/etherium_tests
+
+$(AEVM_EXTERNAL_TEST_DIR)/etherium_tests:
+	@git clone https://github.com/ethereum/tests.git $(AEVM_EXTERNAL_TEST_DIR)/etherium_tests
+
+
 venv-present:
 	@virtualenv -q $(PYTHON_DIR)
 
@@ -227,6 +237,6 @@ internal-clean: $$(KIND)
 	dev2-start, dev2-stop, dev2-attach, dev2-clean \
 	dev3-start, dev3-stop, dev3-attach, dev3-clean \
 	dialyzer \
-	test \
+	test aevm-test-deps\
 	kill killall \
 	clean distclean

apps/aebytecode/src/aeb_disassemble.erl
@@ -1,0 +1,92 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc
+%%%     Prettyprint aevm machine code
+%%% @end
+%%% Created : 2 Oct 2017
+%%%-------------------------------------------------------------------
+
+-module(aeb_disassemble).
+
+-export([ pp/1
+        ]).
+
+-include_lib("aebytecode/include/aeb_opcodes.hrl").
+
+
+pp(Binary) ->
+    pp(0, binary:bin_to_list(Binary), []).
+
+pp(Address, [Op|Ops], Assembly) ->
+    case Op of
+        X when (X >= ?STOP) andalso (X =< ?SIGNEXTEND) -> 
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?LT) andalso (X =< ?BYTE) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?SHA3) andalso (X =< ?SHA3) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?ADDRESS) andalso (X =< ?EXTCODECOPY) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?BLOCKHASH) andalso (X =< ?GASLIMIT) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?POP) andalso (X =< ?JUMPDEST) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?PUSH1) andalso (X =< ?PUSH32) ->
+            Bytes = X-?PUSH1+1,
+            {ArgList, NextOps} = lists:split(Bytes, Ops),
+            Arg = arglist_to_arg(ArgList),
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), [{Arg,8*Bytes}]),
+            next(Address+Bytes, NextOps, Instr, Assembly);
+        X when (X >= ?DUP1) andalso (X =< ?LOG4) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?CREATE) andalso (X =< ?DELEGATECALL) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?INVALID) andalso (X =< ?SUICIDE) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        _ ->
+            io:format("unhandled op ~p",[Op]),
+            next(Address, Ops, "", Assembly)
+    end;
+pp(_, [], Assembly) -> lists:reverse(Assembly).
+
+
+arglist_to_arg([B|Bs]) ->
+    arglist_to_arg(Bs, B).
+
+arglist_to_arg([B|Bs], Acc) ->
+    arglist_to_arg(Bs, Acc*256 + B);
+arglist_to_arg([], Acc) -> Acc.
+
+pp_instruction(Address, Op, Args) ->
+    [io_lib:format("0x~8.16.0B   ",[Address]),
+     pad_op(atom_to_list(Op)),
+     pp_args(Args),
+     "\n"].
+
+pad_op(Op) ->
+    N = length(Op),
+    Pad = 17 - N,
+    [Op,lists:duplicate(Pad, 32)].
+
+pp_args([]) -> [];
+pp_args([{Arg, Size}]) -> 
+    case Size of
+        8 -> io_lib:format("0x~2.16.0B",[Arg]);
+        160 -> io_lib:format("0x~64.16.0B",[Arg]);
+        232 -> io_lib:format("0x~64.16.0B",[Arg]);
+        256 -> io_lib:format("0x~64.16.0B",[Arg])
+    end;
+pp_args([{Arg, Size}|Args]) ->
+    [pp([{Arg, Size}]), " ", pp_args(Args)].
+
+next(Address, Ops, Instr, Assembly) ->
+    pp(Address+1, Ops, [Instr|Assembly]).

apps/aering/src/aer_compiler.erl
@@ -1,0 +1,145 @@
+%%%-------------------------------------------------------------------
+%%% @author Happi (Erik Stenman)
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc
+%%%     Compiler from Aeterinty Ring language to the Aeternity VM, aevm.
+%%% @end
+%%% Created : 12 Dec 2017
+%%% aec_conductor:stop_mining(),aer_compiler:file(a,c).
+%%%-------------------------------------------------------------------
+-module(aer_compiler).
+
+-export([ file/1
+        , file/2]).
+
+-export([test/0]).
+
+file(Filename) ->
+    file(Filename, []).
+
+file(Filename, Options) ->
+    Ast = parse(Filename, Options),
+    ByteCode = compile(Ast, Options),
+    ByteCode.
+
+
+parse(_Filename,_Options) ->
+     parse_string(read_contract(identity)).
+    
+compile(Ast,_Options) ->
+    ast_to_icode(Ast, #{functions => [], env => []}).
+
+ast_to_icode([{contract_type,_Attribs, {con, _, Name}, _TypeDecls}|Rest], Icode) ->
+    %% TODO: Handle types for future type check.
+    ast_to_icode(Rest, set_name(Name, Icode));
+ast_to_icode([{contract, Attribs, {con, _, Name}, Code}|Rest], Icode) ->
+    try get_name(Icode) of
+        Name ->
+            NewIcode = ast_code_to_icode(Code, Icode),
+            ast_to_icode(Rest, NewIcode);
+        _ -> error({contract_name_mismatch, get_line(Attribs)})
+    catch
+        error:name_not_defined ->
+            error({contract_name_not_defined, get_line(Attribs)})
+    end;
+ast_to_icode([], Icode) -> Icode.
+
+ast_code_to_icode([{type_def, Attrib, _, _, _}|Rest], Icode) ->
+    %% TODO: Handle types
+    ast_code_to_icode(Rest, Icode);
+ast_code_to_icode([{letfun, Attrib, Name, Args, What, Body}|Rest], Icode) ->
+    %% TODO: Handle types
+    FunName = ast_id(Name),
+    %% TODO: push funname to env
+    FunArgs = ast_args(Args, []),
+    %% TODO: push args to env
+    FunBody = ast_body(Body, Icode),
+    NewIcode = ast_fun_to_icode(FunName, FunArgs, FunBody, Icode),
+    ast_code_to_icode(Rest, NewIcode);
+ast_code_to_icode([], Icode) -> Icode;
+ast_code_to_icode(Code, Icode) ->
+    io:format("Unhandled code ~p~n",[Code]),
+    Icode.
+
+ast_id({id, _, Id}) -> Id.
+
+ast_args([{arg, _, Name, Type}|Rest], Acc) ->
+    ast_args(Rest, [{ast_id(Name), ast_id(Type)}| Acc]);
+ast_args([], Acc) -> lists:reverse(Acc).
+                                 
+
+ast_body({id, _, Name}, Icode) ->
+    %% TODO Look up id in env
+    ['DUP'].
+    
+
+ast_fun_to_icode(Name, Args, Body, #{functions := Funs} = Icode) ->
+    NewFuns = [{Name, Args, Body}| Funs],
+    set_functions(NewFuns, Icode).
+
+%% -------------------------------------------------------------------
+%% AST
+%% -------------------------------------------------------------------
+get_line(Attribs) -> %% TODO: use AST primitives.
+     proplists:get_value(line, Attribs).
+                    
+
+%% -------------------------------------------------------------------
+%% Icode
+%% -------------------------------------------------------------------
+set_name(Name, Icode) ->
+    maps:put(contract_name, Name, Icode).
+
+get_name(#{contract_name := Name}) -> Name;
+get_name(_) -> error(name_not_defined).
+    
+set_functions(NewFuns, Icode) ->
+    maps:put(functions, NewFuns, Icode).
+
+%% -------------------------------------------------------------------
+%% TODO: Tempoary parser hook below...
+
+parse_string(Text) ->
+    case aer_parser:string(Text) of
+        {ok, Contract} -> Contract;
+        Err = {error, {Line, aer_scan, Reason}} ->
+            io:format("Lexical error at line ~p:\n  ~s\n", [Line, Reason]),
+            error(Err);
+        Err = {error, {Line, aer_parser, Reason}} ->
+            io:format("Parse error at line ~p:\n  ~s\n", [Line, Reason]),
+            error(Err)
+    end.
+
+%% parse_expr(Text) ->
+%%     [{contract, _, _, [{letval, _, _, _, Expr}]}] =
+%%         parse_string("contract Dummy = { let _ = " ++ Text ++ "}"),
+%%     Expr.
+
+read_contract(Name) ->
+    {ok, Bin} = file:read_file(filename:join(contract_path(), lists:concat([Name, ".aer"]))),
+    binary_to_list(Bin).
+
+contract_path() ->
+    "apps/aering/test/contracts".
+
+
+solidity_id_contract() ->
+    binint_to_bin(<< "0x606060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000" >>).
+
+test() ->
+    io:format(aeb_disassemble:pp(solidity_id_contract()),[]).
+
+ 
+binint_to_bin(<<"0x", Bin/binary>>) ->
+    << <<(hex_to_int(X)):4>> || <<X:8>> <= Bin>>;
+binint_to_bin(<<"0", _/binary>> = Bin) ->
+    %% Don't know what to do.
+    %% Is this an attempt to pad?
+    error({unexpected, Bin});
+binint_to_bin(Bin) when is_binary(Bin) ->
+    Int = binary_to_integer(Bin),
+    binary:encode_unsigned(Int).
+
+hex_to_int(X) when $A =< X, X =< $F -> 10 + X - $A;
+hex_to_int(X) when $a =< X, X =< $f -> 10 + X - $a;
+hex_to_int(X) when $0 =< X, X =< $9 -> X - $0.

aeternity/aeternity

Makefile
@@ -18,6 +18,9 @@ endif
 
 PYTHON_DIR = py
 PYTHON_TESTS = $(PYTHON_DIR)/tests
+PIP = $(PYTHON_BIN)/pip
+
+export AEVM_EXTERNAL_TEST_DIR=aevm_external
 
 HTTP_APP = apps/aehttp
 SWTEMP := $(shell mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
@@ -121,6 +124,15 @@ test:
 eunit:
 	@./rebar3 do eunit $(EUNIT_TEST_FLAGS)
 
+aevm-test: aevm-test-deps
+	@./rebar3 eunit --application=aevm
+
+aevm-test-deps: $(AEVM_EXTERNAL_TEST_DIR)/ethereum_tests
+
+$(AEVM_EXTERNAL_TEST_DIR)/ethereum_tests:
+	@git clone https://github.com/ethereum/tests.git $(AEVM_EXTERNAL_TEST_DIR)/ethereum_tests
+
+
 venv-present:
 	@virtualenv -q $(PYTHON_DIR)
 
@@ -216,6 +228,6 @@ internal-clean: $$(KIND)
 	dev2-start, dev2-stop, dev2-attach, dev2-clean \
 	dev3-start, dev3-stop, dev3-attach, dev3-clean \
 	dialyzer \
-	test \
+	test aevm-test-deps\
 	kill killall \
 	clean distclean

apps/aebytecode/src/aeb_disassemble.erl
@@ -1,0 +1,92 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc
+%%%     Prettyprint aevm machine code
+%%% @end
+%%% Created : 2 Oct 2017
+%%%-------------------------------------------------------------------
+
+-module(aeb_disassemble).
+
+-export([ pp/1
+        ]).
+
+-include_lib("aebytecode/include/aeb_opcodes.hrl").
+
+
+pp(Binary) ->
+    pp(0, binary:bin_to_list(Binary), []).
+
+pp(Address, [Op|Ops], Assembly) ->
+    case Op of
+        X when (X >= ?STOP) andalso (X =< ?SIGNEXTEND) -> 
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?LT) andalso (X =< ?BYTE) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?SHA3) andalso (X =< ?SHA3) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?ADDRESS) andalso (X =< ?EXTCODECOPY) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?BLOCKHASH) andalso (X =< ?GASLIMIT) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?POP) andalso (X =< ?JUMPDEST) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?PUSH1) andalso (X =< ?PUSH32) ->
+            Bytes = X-?PUSH1+1,
+            {ArgList, NextOps} = lists:split(Bytes, Ops),
+            Arg = arglist_to_arg(ArgList),
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), [{Arg,8*Bytes}]),
+            next(Address+Bytes, NextOps, Instr, Assembly);
+        X when (X >= ?DUP1) andalso (X =< ?LOG4) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?CREATE) andalso (X =< ?DELEGATECALL) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        X when (X >= ?INVALID) andalso (X =< ?SUICIDE) ->
+            Instr = pp_instruction(Address, aeb_opcodes:mnemonic(Op), []),
+            next(Address, Ops, Instr, Assembly);
+        _ ->
+            io:format("unhandled op ~p",[Op]),
+            next(Address, Ops, "", Assembly)
+    end;
+pp(_, [], Assembly) -> lists:reverse(Assembly).
+
+
+arglist_to_arg([B|Bs]) ->
+    arglist_to_arg(Bs, B).
+
+arglist_to_arg([B|Bs], Acc) ->
+    arglist_to_arg(Bs, Acc*256 + B);
+arglist_to_arg([], Acc) -> Acc.
+
+pp_instruction(Address, Op, Args) ->
+    [io_lib:format("0x~8.16.0B   ",[Address]),
+     pad_op(atom_to_list(Op)),
+     pp_args(Args),
+     "\n"].
+
+pad_op(Op) ->
+    N = length(Op),
+    Pad = 17 - N,
+    [Op,lists:duplicate(Pad, 32)].
+
+pp_args([]) -> [];
+pp_args([{Arg, Size}]) -> 
+    case Size of
+        8 -> io_lib:format("0x~2.16.0B",[Arg]);
+        160 -> io_lib:format("0x~64.16.0B",[Arg]);
+        232 -> io_lib:format("0x~64.16.0B",[Arg]);
+        256 -> io_lib:format("0x~64.16.0B",[Arg])
+    end;
+pp_args([{Arg, Size}|Args]) ->
+    [pp_args([{Arg, Size}]), " ", pp_args(Args)].
+
+next(Address, Ops, Instr, Assembly) ->
+    pp(Address+1, Ops, [Instr|Assembly]).

apps/aering/src/aer_compiler.erl
@@ -1,0 +1,145 @@
+%%%-------------------------------------------------------------------
+%%% @author Happi (Erik Stenman)
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc
+%%%     Compiler from Aeterinty Ring language to the Aeternity VM, aevm.
+%%% @end
+%%% Created : 12 Dec 2017
+%%% aec_conductor:stop_mining(),aer_compiler:file(a,c).
+%%%-------------------------------------------------------------------
+-module(aer_compiler).
+
+-export([ file/1
+        , file/2]).
+
+-export([test/0]).
+
+file(Filename) ->
+    file(Filename, []).
+
+file(Filename, Options) ->
+    Ast = parse(Filename, Options),
+    ByteCode = compile(Ast, Options),
+    ByteCode.
+
+
+parse(_Filename,_Options) ->
+     parse_string(read_contract(identity)).
+    
+compile(Ast,_Options) ->
+    ast_to_icode(Ast, #{functions => [], env => []}).
+
+ast_to_icode([{contract_type,_Attribs, {con, _, Name}, _TypeDecls}|Rest], Icode) ->
+    %% TODO: Handle types for future type check.
+    ast_to_icode(Rest, set_name(Name, Icode));
+ast_to_icode([{contract, Attribs, {con, _, Name}, Code}|Rest], Icode) ->
+    try get_name(Icode) of
+        Name ->
+            NewIcode = ast_code_to_icode(Code, Icode),
+            ast_to_icode(Rest, NewIcode);
+        _ -> error({contract_name_mismatch, get_line(Attribs)})
+    catch
+        error:name_not_defined ->
+            error({contract_name_not_defined, get_line(Attribs)})
+    end;
+ast_to_icode([], Icode) -> Icode.
+
+ast_code_to_icode([{type_def,_Attrib, _, _, _}|Rest], Icode) ->
+    %% TODO: Handle types
+    ast_code_to_icode(Rest, Icode);
+ast_code_to_icode([{letfun,_Attrib, Name, Args,_What, Body}|Rest], Icode) ->
+    %% TODO: Handle types
+    FunName = ast_id(Name),
+    %% TODO: push funname to env
+    FunArgs = ast_args(Args, []),
+    %% TODO: push args to env
+    FunBody = ast_body(Body, Icode),
+    NewIcode = ast_fun_to_icode(FunName, FunArgs, FunBody, Icode),
+    ast_code_to_icode(Rest, NewIcode);
+ast_code_to_icode([], Icode) -> Icode;
+ast_code_to_icode(Code, Icode) ->
+    io:format("Unhandled code ~p~n",[Code]),
+    Icode.
+
+ast_id({id, _, Id}) -> Id.
+
+ast_args([{arg, _, Name, Type}|Rest], Acc) ->
+    ast_args(Rest, [{ast_id(Name), ast_id(Type)}| Acc]);
+ast_args([], Acc) -> lists:reverse(Acc).
+                                 
+
+ast_body({id, _,_Name},_Icode) ->
+    %% TODO Look up id in env
+    ['DUP'].
+    
+
+ast_fun_to_icode(Name, Args, Body, #{functions := Funs} = Icode) ->
+    NewFuns = [{Name, Args, Body}| Funs],
+    set_functions(NewFuns, Icode).
+
+%% -------------------------------------------------------------------
+%% AST
+%% -------------------------------------------------------------------
+get_line(Attribs) -> %% TODO: use AST primitives.
+     proplists:get_value(line, Attribs).
+                    
+
+%% -------------------------------------------------------------------
+%% Icode
+%% -------------------------------------------------------------------
+set_name(Name, Icode) ->
+    maps:put(contract_name, Name, Icode).
+
+get_name(#{contract_name := Name}) -> Name;
+get_name(_) -> error(name_not_defined).
+    
+set_functions(NewFuns, Icode) ->
+    maps:put(functions, NewFuns, Icode).
+
+%% -------------------------------------------------------------------
+%% TODO: Tempoary parser hook below...
+
+parse_string(Text) ->
+    case aer_parser:string(Text) of
+        {ok, Contract} -> Contract;
+        Err = {error, {Line, aer_scan, Reason}} ->
+            io:format("Lexical error at line ~p:\n  ~s\n", [Line, Reason]),
+            error(Err);
+        Err = {error, {Line, aer_parser, Reason}} ->
+            io:format("Parse error at line ~p:\n  ~s\n", [Line, Reason]),
+            error(Err)
+    end.
+
+%% parse_expr(Text) ->
+%%     [{contract, _, _, [{letval, _, _, _, Expr}]}] =
+%%         parse_string("contract Dummy = { let _ = " ++ Text ++ "}"),
+%%     Expr.
+
+read_contract(Name) ->
+    {ok, Bin} = file:read_file(filename:join(contract_path(), lists:concat([Name, ".aer"]))),
+    binary_to_list(Bin).
+
+contract_path() ->
+    "apps/aering/test/contracts".
+
+
+solidity_id_contract() ->
+    binint_to_bin(<< "0x606060405260043610603f576000357c0100000000000000000000000000000000000000000000000000000000" >>).
+
+test() ->
+    io:format(aeb_disassemble:pp(solidity_id_contract()),[]).
+
+ 
+binint_to_bin(<<"0x", Bin/binary>>) ->
+    << <<(hex_to_int(X)):4>> || <<X:8>> <= Bin>>;
+binint_to_bin(<<"0", _/binary>> = Bin) ->
+    %% Don't know what to do.
+    %% Is this an attempt to pad?
+    error({unexpected, Bin});
+binint_to_bin(Bin) when is_binary(Bin) ->
+    Int = binary_to_integer(Bin),
+    binary:encode_unsigned(Int).
+
+hex_to_int(X) when $A =< X, X =< $F -> 10 + X - $A;
+hex_to_int(X) when $a =< X, X =< $f -> 10 + X - $a;
+hex_to_int(X) when $0 =< X, X =< $9 -> X - $0.