2a7b5e3476d5850366fc68ce0275c6f7ff6c2788
Ulf Norell <ulf.norell@quviq.com>
2018-01-23 13:58:57 +0100

    Set up contract state tree representation

--- apps/aecontract/src/aect_state_tree.erl
+++ apps/aecontract/src/aect_state_tree.erl
@@ -18,7 +18,16 @@
 %%% Types
 %%%===================================================================
 
--record(contract_tree, {}).
+-type contract_tree() :: aeu_mtrees:tree(aect_contracts:id(), aect_contracts:serialized()).
+-type call_tree()     :: aeu_mtrees:tree(aect_call:id(), aect_call:serialized()).
+-type call_trees()    :: gb_trees:tree(aect_contracts:id(), call_tree()).
+
+-record(contract_tree, {
+    contracts = aeu_mtrees:empty() :: contract_tree(),
+    calls     = gb_trees:empty()   :: call_trees()
+        %% Keep calls in separate trees to avoid having to serialize/deserialize the
+        %% entire call history every time we're adding a call.
+    }).
 
 -opaque tree() :: #contract_tree{}.
 
@@ -28,7 +37,6 @@
 
 -spec empty() -> tree().
 empty() ->
-    %% PLACEHOLDER
     #contract_tree{}.
 
 -spec insert_contract(aect_contracts:contract(), tree()) -> tree().
@@ -37,6 +45,8 @@ insert_contract(_Contract, Tree) ->
     Tree.
 
 -spec root_hash(tree()) -> {ok, aeu_mtrees:root_hash()} | {error, empty}.
-root_hash(#contract_tree{}) ->
-    {error, empty}.
+root_hash(#contract_tree{contracts = CtTree}) ->
+    %% The contract tree references the hashes of the call trees so we only
+    %% need the hash of the contract tree here.
+    aeu_mtrees:root_hash(CtTree).
 
