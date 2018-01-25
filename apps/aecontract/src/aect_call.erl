b128ac3cee6f797e536b67be3faf532759641db4
Ulf Norell <ulf.norell@quviq.com>
2018-01-25 14:47:14 +0100

    Update the fields of contract call objects

--- apps/aecontract/src/aect_call.erl
+++ apps/aecontract/src/aect_call.erl
@@ -9,17 +9,21 @@
 
 %% API
 -export([ deserialize/1
-        , fee/1
         , id/1
         , new/2
         , contract_address/1
-        , sender_address/1
-        , sender_nonce/1
+        , caller_address/1
+        , caller_nonce/1
+        , height/1
+        , return_value/1
+        , gas_used/1
         , serialize/1
-        , set_fee/2
         , set_contract_address/2
-        , set_sender_address/2
-        , set_sender_nonce/2
+        , set_caller_address/2
+        , set_caller_nonce/2
+        , set_height/2
+        , set_return_value/2
+        , set_gas_used/2
         ]).
 
 -include_lib("apps/aecore/include/common.hrl").
@@ -31,15 +35,18 @@
 %%% Types
 %%%===================================================================
 
--record(call, { sender_address   :: pubkey()
-              , sender_nonce     :: integer()
+-record(call, { caller_address   :: pubkey()
+              , caller_nonce     :: integer()
+              , height           :: height()
               , contract_address :: pubkey()
-              , fee              :: integer()
+              , gas_used         :: amount()
+              , return_value     :: binary()
               }).
 
 -opaque call() :: #call{}.
 -type id() :: binary().
 -type serialized() :: binary().
+-type amount() :: aect_contracts:amount().
 
 -export_type([ id/0
              , call/0
@@ -54,18 +61,20 @@
 %%%===================================================================
 
 -spec new(aect_call_tx:call_tx(), height()) -> call().
-new(CallTx, _BlockHeight) ->
-    C = #call{ sender_address   = aect_call_tx:caller(CallTx)
-             , sender_nonce     = aect_call_tx:nonce(CallTx)
+new(CallTx, BlockHeight) ->
+    C = #call{ caller_address   = aect_call_tx:caller(CallTx)
+             , caller_nonce     = aect_call_tx:nonce(CallTx)
+             , height           = BlockHeight
              , contract_address = aect_call_tx:contract(CallTx)
-             , fee              = aect_call_tx:fee(CallTx)
+             , gas_used         = 0
+             , return_value     = <<>>
              },
     assert_fields(C).
 
 -spec id(call()) -> id().
 id(I) ->
-    Bin = <<(I#call.sender_address):?PUB_SIZE/binary,
-            (I#call.sender_nonce):?NONCE_SIZE,
+    Bin = <<(I#call.caller_address):?PUB_SIZE/binary,
+            (I#call.caller_nonce):?NONCE_SIZE,
             (I#call.contract_address):?PUB_SIZE/binary>>,
     aec_sha256:hash(Bin).
 
@@ -73,10 +82,12 @@ id(I) ->
 serialize(#call{} = I) ->
     msgpack:pack([ #{<<"type">>             => ?CONTRACT_INTERACTION_TYPE}
                  , #{<<"vsn">>              => ?CONTRACT_INTERACTION_VSN}
-                 , #{<<"sender_address">>   => sender_address(I)}
-                 , #{<<"sender_nonce">>     => sender_nonce(I)}
+                 , #{<<"caller_address">>   => caller_address(I)}
+                 , #{<<"caller_nonce">>     => caller_nonce(I)}
+                 , #{<<"height">>           => height(I)}
                  , #{<<"contract_address">> => contract_address(I)}
-                 , #{<<"fee">>              => fee(I)}
+                 , #{<<"gas_used">>         => gas_used(I)}
+                 , #{<<"return_value">>     => return_value(I)}
                  ]).
 
 -spec deserialize(binary()) -> call().
@@ -84,61 +95,81 @@ deserialize(B) ->
     {ok, List} = msgpack:unpack(B),
     [ #{<<"type">>             := ?CONTRACT_INTERACTION_TYPE}
     , #{<<"vsn">>              := ?CONTRACT_INTERACTION_VSN}
-    , #{<<"sender_address">>   := SenderAddress}
-    , #{<<"sender_nonce">>     := SenderNonce}
+    , #{<<"caller_address">>   := CallerAddress}
+    , #{<<"caller_nonce">>     := CallerNonce}
+    , #{<<"height">>           := Height}
     , #{<<"contract_address">> := ContractAddress}
-    , #{<<"fee">>              := Fee}
+    , #{<<"gas_used">>         := GasUsed}
+    , #{<<"return_value">>     := ReturnValue}
     ] = List,
-    #call{ sender_address   = SenderAddress
-         , sender_nonce     = SenderNonce
+    #call{ caller_address   = CallerAddress
+         , caller_nonce     = CallerNonce
+         , height           = Height
          , contract_address = ContractAddress
-         , fee              = Fee
+         , gas_used         = GasUsed
+         , return_value     = ReturnValue
          }.
 
 
 %%%===================================================================
 %%% Getters
 
--spec sender_address(call()) -> pubkey().
-sender_address(I) -> I#call.sender_address.
+-spec caller_address(call()) -> pubkey().
+caller_address(I) -> I#call.caller_address.
 
--spec sender_nonce(call()) -> integer().
-sender_nonce(I) -> I#call.sender_nonce.
+-spec caller_nonce(call()) -> integer().
+caller_nonce(I) -> I#call.caller_nonce.
+
+-spec height(call()) -> height().
+height(I) -> I#call.height.
 
 -spec contract_address(call()) -> pubkey().
 contract_address(I) -> I#call.contract_address.
 
--spec fee(call()) -> integer().
-fee(I) -> I#call.fee.
+-spec return_value(call()) -> binary().
+return_value(I) -> I#call.return_value.
+
+-spec gas_used(call()) -> amount().
+gas_used(I) -> I#call.gas_used.
 
 %%%===================================================================
 %%% Setters
 
--spec set_sender_address(pubkey(), call()) -> call().
-set_sender_address(X, I) ->
-    I#call{sender_address = assert_field(sender_address, X)}.
+-spec set_caller_address(pubkey(), call()) -> call().
+set_caller_address(X, I) ->
+    I#call{caller_address = assert_field(caller_address, X)}.
 
--spec set_sender_nonce(integer(), call()) -> call().
-set_sender_nonce(X, I) ->
-    I#call{sender_nonce = assert_field(sender_nonce, X)}.
+-spec set_caller_nonce(integer(), call()) -> call().
+set_caller_nonce(X, I) ->
+    I#call{caller_nonce = assert_field(caller_nonce, X)}.
+
+-spec set_height(integer(), call()) -> call().
+set_height(X, I) ->
+    I#call{height = assert_field(height, X)}.
 
 -spec set_contract_address(pubkey(), call()) -> call().
 set_contract_address(X, I) ->
     I#call{contract_address = assert_field(contract_address, X)}.
 
--spec set_fee(integer(), call()) -> call().
-set_fee(X, I) ->
-    I#call{fee = assert_field(fee, X)}.
+-spec set_return_value(integer(), call()) -> call().
+set_return_value(X, I) ->
+    I#call{return_value = assert_field(return_value, X)}.
+
+-spec set_gas_used(integer(), call()) -> call().
+set_gas_used(X, I) ->
+    I#call{gas_used = assert_field(gas_used, X)}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
 assert_fields(I) ->
-    List = [ {sender_address,   I#call.sender_address}
-           , {sender_nonce  ,   I#call.sender_nonce}
+    List = [ {caller_address,   I#call.caller_address}
+           , {caller_nonce,     I#call.caller_nonce}
+           , {height,           I#call.height}
            , {contract_address, I#call.contract_address}
-           , {fee           ,   I#call.fee}
+           , {return_value,     I#call.return_value}
+           , {gas_used,         I#call.gas_used}
            ],
     List1 = [try assert_field(X, Y), [] catch _:X -> X end
              || {X, Y} <- List],
@@ -147,8 +178,10 @@ assert_fields(I) ->
         Other -> error({missing, Other})
     end.
 
-assert_field(sender_address,   <<_:?PUB_SIZE/binary>> = X) -> X;
-assert_field(sender_nonce,     X) when is_integer(X), X >= 0 -> X;
+assert_field(caller_address,   <<_:?PUB_SIZE/binary>> = X) -> X;
+assert_field(caller_nonce,     X) when is_integer(X), X >= 0 -> X;
+assert_field(height,           X) when is_integer(X), X > 0 -> X;
 assert_field(contract_address, <<_:?PUB_SIZE/binary>> = X) -> X;
-assert_field(fee,              X) when is_integer(X), X >= 0 -> X;
+assert_field(return_value,     X) when is_binary(X) -> X;
+assert_field(gas_used,         X) when is_integer(X), X >= 0 -> X;
 assert_field(Field,            X) -> error({illegal, Field, X}).
--- apps/aecontract/test/aect_call_tests.erl
+++ apps/aecontract/test/aect_call_tests.erl
@@ -10,17 +10,21 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -import(aect_call, [ deserialize/1
-                   , fee/1
+                   , return_value/1
+                   , gas_used/1
+                   , height/1
                    , id/1
                    , new/2
                    , contract_address/1
-                   , sender_address/1
-                   , sender_nonce/1
+                   , caller_address/1
+                   , caller_nonce/1
                    , serialize/1
-                   , set_fee/2
+                   , set_return_value/2
+                   , set_gas_used/2
+                   , set_height/2
                    , set_contract_address/2
-                   , set_sender_address/2
-                   , set_sender_nonce/2
+                   , set_caller_address/2
+                   , set_caller_nonce/2
                    ]).
 
 basic_test_() ->
@@ -36,23 +40,29 @@ basic_serialize() ->
 
 basic_getters() ->
     I = new(call_tx(), 1),
-    ?assert(is_integer(fee(I))),
     ?assert(is_binary(id(I))),
     ?assert(is_binary(contract_address(I))),
-    ?assert(is_binary(sender_address(I))),
-    ?assert(is_integer(sender_nonce(I))),
+    ?assert(is_binary(caller_address(I))),
+    ?assert(is_integer(caller_nonce(I))),
+    ?assert(is_integer(height(I))),
+    ?assert(is_binary(return_value(I))),
+    ?assert(is_integer(gas_used(I))),
     ok.
 
 basic_setters() ->
     I = aect_call:new(call_tx(), 1),
-    ?assertError({illegal, _, _}, set_fee(foo, I)),
-    _ = set_fee(123, I),
+    ?assertError({illegal, _, _}, set_return_value(foo, I)),
+    _ = set_return_value(<<"123">>, I),
+    ?assertError({illegal, _, _}, set_gas_used(foo, I)),
+    _ = set_gas_used(123, I),
     ?assertError({illegal, _, _}, set_contract_address(foo, I)),
     _ = set_contract_address(<<123:65/unit:8>>, I),
-    ?assertError({illegal, _, _}, set_sender_address(foo, I)),
-    _ = set_sender_address(<<123:65/unit:8>>, I),
-    ?assertError({illegal, _, _}, set_sender_nonce(-1, I)),
-    _ = set_sender_nonce(1, I),
+    ?assertError({illegal, _, _}, set_caller_address(foo, I)),
+    _ = set_caller_address(<<123:65/unit:8>>, I),
+    ?assertError({illegal, _, _}, set_caller_nonce(-1, I)),
+    _ = set_caller_nonce(1, I),
+    ?assertError({illegal, _, _}, set_height(-1, I)),
+    _ = set_height(1, I),
     ok.
 
 call_tx() ->
