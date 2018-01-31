ede86d2ae93eaa324c099d88aae4c6a82a3f1289
zp-sd <zp.software.development@gmail.com>
2018-01-31 16:30:20 +0100

    Add Naming Service related Swagger Python code

--- py/tests/swagger_client/api/external_api.py
+++ py/tests/swagger_client/api/external_api.py
@@ -488,6 +488,97 @@ class ExternalApi(object):
             _request_timeout=params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def get_name(self, **kwargs):  # noqa: E501
+        """get_name  # noqa: E501
+
+        Get name entry from Naming System  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async=True
+        >>> thread = api.get_name(async=True)
+        >>> result = thread.get()
+
+        :param async bool
+        :param str name: Name to get entry from Naming System
+        :return: NameEntry
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async'):
+            return self.get_name_with_http_info(**kwargs)  # noqa: E501
+        else:
+            (data) = self.get_name_with_http_info(**kwargs)  # noqa: E501
+            return data
+
+    def get_name_with_http_info(self, **kwargs):  # noqa: E501
+        """get_name  # noqa: E501
+
+        Get name entry from Naming System  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async=True
+        >>> thread = api.get_name_with_http_info(async=True)
+        >>> result = thread.get()
+
+        :param async bool
+        :param str name: Name to get entry from Naming System
+        :return: NameEntry
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['name']  # noqa: E501
+        all_params.append('async')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_name" % key
+                )
+            params[key] = val
+        del params['kwargs']
+
+        collection_formats = {}
+
+        path_params = {}
+
+        query_params = []
+        if 'name' in params:
+            query_params.append(('name', params['name']))  # noqa: E501
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = []  # noqa: E501
+
+        return self.api_client.call_api(
+            '/name', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='NameEntry',  # noqa: E501
+            auth_settings=auth_settings,
+            async=params.get('async'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_top(self, **kwargs):  # noqa: E501
         """get_top  # noqa: E501
 
