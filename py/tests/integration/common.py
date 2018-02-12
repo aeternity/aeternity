3e0e94df1244a1711af67a550c4aec44efab9c82
Dincho Todorov <dincho@users.noreply.github.com>
2018-02-12 18:31:25 +0200

    Increase mining timeout in UAT tests

--- py/tests/integration/common.py
+++ py/tests/integration/common.py
@@ -98,7 +98,7 @@ def genesis_hash(api):
     return block.prev_hash
 
 def wait_until_height(api, height):
-    wait(lambda: api.get_top().height >= height, timeout_seconds=30, sleep_seconds=0.25)
+    wait(lambda: api.get_top().height >= height, timeout_seconds=120, sleep_seconds=0.25)
 
 def get_account_balance(int_api, pub_key=None):
     balance = Balance(balance=0)
