d01d24c9621d65a62bb333490973a3a7afc82efc
acid <acid@sennui.com>
2018-02-13 22:34:12 +0100

    update build config

--- deployment/deploy_api_docs.sh
+++ deployment/deploy_api_docs.sh
@@ -4,19 +4,11 @@ cp apps/aehttp/priv/swagger-docs/swagger.json /tmp/epoch-api-docs/
 cd /tmp/epoch-api-docs/
 
 git add swagger.json;
-
-git config user.email "circleci@aeternity.com";
-git config user.name "CircleCi Aeternity";
-
 STATUS=`git status --porcelain`
 
 if [ -z "$STATUS" ]; then
     echo "Nothing to commit, docs did not change";
 else
-    if [ -n "$CIRCLE_TAG" ]; then
-        git commit -a -m 'Update epoch docs CircleCI';
-        git push origin master
-    else
-        echo "no master, no tag, not commit!"
-    fi
+    git commit -a -m 'Update epoch docs CircleCI';
+    git push origin master
 fi
