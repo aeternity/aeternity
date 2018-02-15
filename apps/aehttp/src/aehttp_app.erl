c28dbd3758185f414037d846bff0ca5f87e30e37
Amir Hadi <amir-hadi@users.noreply.github.com>
2018-02-15 14:03:30 +0100

    Pt 154904411 ws binding address (#740)
    * (Naive) configuration of WS binding address
    
    This should allow configuring the websocket binding address using the epoch.yaml file. Instead it crashes the application.
    
    * Fixed the project not starting
    
    thanks to the help of @dincho we made this work
    
    * Enabling users to configure other listen addresses
    
    Not changing the defaults (yet). Updated epoch_full.yaml to showcase the new configuration options.
    
    * Refactored some functions
    
    to get the IP address from configs easier
    
    * Changing default internal listen address
    
    It was 0.0.0.0 before which is not recommended, now it is 127.0.0.1
    
    * Switched back to binary defaults
    
    * Allowing external access to internal port
    
    Since we set the default listening to 127.0.0.1, but in the docker-compose localnet environment we want to have access to the internal port, too.

--- apps/aehttp/src/aehttp_app.erl
+++ apps/aehttp/src/aehttp_app.erl
@@ -9,8 +9,11 @@
 
 
 -define(DEFAULT_SWAGGER_EXTERNAL_PORT, 8043).
+-define(DEFAULT_SWAGGER_EXTERNAL_LISTEN_ADDRESS, <<"0.0.0.0">>).
 -define(DEFAULT_SWAGGER_INTERNAL_PORT, 8143).
+-define(DEFAULT_SWAGGER_INTERNAL_LISTEN_ADDRESS, <<"127.0.0.1">>).
 -define(DEFAULT_WEBSOCKET_INTERNAL_PORT, 8144).
+-define(DEFAULT_WEBSOCKET_LISTEN_ADDRESS, <<"127.0.0.1">>).
 -define(INT_ACCEPTORS_POOLSIZE, 10).
 
 %% Application callbacks
@@ -46,8 +49,9 @@ stop(_State) ->
 %%====================================================================
 start_swagger_external() ->
     Port = get_external_port(),
+    ListenAddress = get_external_listen_address(),
     Spec = swagger_server:child_spec(swagger_ext, #{
-                                       ip => {0, 0, 0, 0},
+                                       ip => ListenAddress,
                                        port => Port,
                                        net_opts => [],
                                        logic_handler => aehttp_dispatch_ext
@@ -57,8 +61,9 @@ start_swagger_external() ->
 
 start_swagger_internal() ->
     Port = get_internal_port(),
+    ListenAddress = get_internal_listen_address(),
     Spec = swagger_server:child_spec(swagger_int, #{
-                                       ip => {0, 0, 0, 0},
+                                       ip => ListenAddress,
                                        port => Port,
                                        net_opts => [],
                                        logic_handler => aehttp_dispatch_int
@@ -69,31 +74,48 @@ start_swagger_internal() ->
 start_websocket_internal() ->
     Port = get_internal_websockets_port(),
     PoolSize = get_internal_websockets_acceptors(),
+    ListenAddress = get_internal_websockets_listen_address(),
     Dispatch = cowboy_router:compile([
         {'_', [
             {"/websocket", ws_handler, []}
         ]}
     ]),
     {ok, _} = cowboy:start_http(http, PoolSize,
-                                 [{port, Port}, {ip, {127, 0, 0, 1}}],
+                                 [{port, Port}, {ip, ListenAddress}],
                                  [{env, [{dispatch, Dispatch}]}]),
     ok.
+
+get_and_parse_ip_address_from_config_or_env(CfgKey, App, EnvKey, Default) ->
+    Config = aeu_env:user_config_or_env(CfgKey, App, EnvKey, Default),
+    {ok, IpAddress} = inet:parse_address(binary_to_list(Config)),
+    IpAddress.
 
 get_external_port() ->
     aeu_env:user_config_or_env([<<"http">>, <<"external">>, <<"port">>],
                                aehttp, swagger_port_external, ?DEFAULT_SWAGGER_EXTERNAL_PORT).
 
+get_external_listen_address() ->
+    get_and_parse_ip_address_from_config_or_env([<<"http">>, <<"external">>, <<"listen_address">>],
+                                                aehttp, [http, websocket, listen_address], ?DEFAULT_SWAGGER_EXTERNAL_LISTEN_ADDRESS).
+
 get_internal_port() ->
     aeu_env:user_config_or_env([<<"http">>, <<"internal">>, <<"port">>],
                                aehttp, [internal, swagger_port], ?DEFAULT_SWAGGER_INTERNAL_PORT).
 
+get_internal_listen_address() ->
+    get_and_parse_ip_address_from_config_or_env([<<"http">>, <<"internal">>, <<"listen_address">>],
+                                                aehttp, [http, websocket, listen_address], ?DEFAULT_SWAGGER_INTERNAL_LISTEN_ADDRESS).
+
+get_internal_websockets_listen_address() ->
+    get_and_parse_ip_address_from_config_or_env([<<"websocket">>, <<"internal">>, <<"listen_address">>],
+                                                aehttp, [internal, websocket, listen_address], ?DEFAULT_WEBSOCKET_LISTEN_ADDRESS).
+
 get_internal_websockets_port() ->
     aeu_env:user_config_or_env([<<"websocket">>, <<"internal">>, <<"port">>],
-                               aehttp, [internal, websocket, port],?DEFAULT_WEBSOCKET_INTERNAL_PORT).
+                               aehttp, [internal, websocket, port], ?DEFAULT_WEBSOCKET_INTERNAL_PORT).
 
 get_internal_websockets_acceptors() ->
     aeu_env:user_config_or_env([<<"websocket">>, <<"internal">>, <<"acceptors">>],
                                aehttp, [internal, websocket, handlers], ?INT_ACCEPTORS_POOLSIZE).
 
 ws_handlers_queue_max_size() -> 5.
-
