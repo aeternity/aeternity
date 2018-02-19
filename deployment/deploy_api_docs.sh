1787dd04326b03a18d06ade276b2f1a98168e608
Michał <github@bagrowski.com>
2018-02-19 13:10:03 +0100

    Update docs from CircleCI (#705)
    Tagging epoch repo would update the docs repo master with swagger.json

---
+++ deployment/deploy_api_docs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+git clone git@github.com:aeternity/epoch-api-docs.git /tmp/epoch-api-docs
+cp apps/aehttp/priv/swagger-docs/swagger.json /tmp/epoch-api-docs/
+cd /tmp/epoch-api-docs/
+
+git add swagger.json;
+STATUS=`git status --porcelain`
+
+if [ -z "$STATUS" ]; then
+    echo "Nothing to commit, docs did not change";
+else
+    git commit -a -m 'Update epoch docs CircleCI';
+    git push origin master
+fi
