be237c51c6162295fcb422327a47aa4af39db347
Fabian Krol <fabian@krol.io>
2018-03-06 08:35:32 +0100

    POC: Use cowboy to handle http api requests
    - Remove handlers generated by swagger
    - Add middleware for request params validation

--- apps/aehttp/src/aehttp_app.erl
+++ apps/aehttp/src/aehttp_app.erl
@@ -29,8 +29,7 @@
 start(_StartType, _StartArgs) ->
     {ok, Pid} = aehttp_sup:start_link(),
     {ok, _} = ws_task_worker_sup:start_link(),
-    ok = start_swagger_external(),
-    ok = start_swagger_internal(),
+    ok = start_http_api(),
     ok = start_websocket_internal(),
     MaxWsHandlers = get_internal_websockets_acceptors(),
     ok = jobs:add_queue(ws_handlers_queue, [{standard_counter, MaxWsHandlers},
@@ -47,28 +46,27 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
-start_swagger_external() ->
-    Port = get_external_port(),
-    ListenAddress = get_external_listen_address(),
-    Spec = swagger_server:child_spec(swagger_ext, #{
-                                       ip => ListenAddress,
-                                       port => Port,
-                                       net_opts => [],
-                                       logic_handler => aehttp_dispatch_ext
-                                      }),
-    {ok, _} = supervisor:start_child(aehttp_sup, Spec),
-    ok.
 
-start_swagger_internal() ->
-    Port = get_internal_port(),
-    ListenAddress = get_internal_listen_address(),
-    Spec = swagger_server:child_spec(swagger_int, #{
-                                       ip => ListenAddress,
-                                       port => Port,
-                                       net_opts => [],
-                                       logic_handler => aehttp_dispatch_int
-                                      }),
-    {ok, _} = supervisor:start_child(aehttp_sup, Spec),
+start_http_api() ->
+    ok = start_http_api(external, aehttp_dispatch_ext, swagger_external_handler),
+    ok = start_http_api(internal, aehttp_dispatch_int, swagger_internal_handler).
+
+start_http_api(Target, LogicHandler, SwaggerHandler) ->
+    PoolSize = get_http_api_acceptors(Target),
+    Port = get_http_api_port(Target),
+    ListenAddress = get_http_api_listen_address(Target),
+
+    [{'_', Paths0}] = swagger_router:get_paths(LogicHandler),
+    Paths = [{Path, aehttp_api_handler, Args}
+             || {Path, TmpSwaggerHandler, Args} <- Paths0,
+                TmpSwaggerHandler == SwaggerHandler
+            ],
+    Dispatch = cowboy_router:compile([{'_', Paths}]),
+
+    {ok, _} = cowboy:start_http(Target, PoolSize, [{port, Port}, {ip, ListenAddress}], [
+            {env, [ {dispatch, Dispatch} ]},
+            {middlewares, [cowboy_router, aehttp_api_middleware, cowboy_handler]}
+        ]),
     ok.
 
 start_websocket_internal() ->
@@ -90,19 +88,20 @@ get_and_parse_ip_address_from_config_or_env(CfgKey, App, EnvKey, Default) ->
     {ok, IpAddress} = inet:parse_address(binary_to_list(Config)),
     IpAddress.
 
-get_external_port() ->
+get_http_api_acceptors(_) -> ?INT_ACCEPTORS_POOLSIZE.
+
+get_http_api_port(external) ->
     aeu_env:user_config_or_env([<<"http">>, <<"external">>, <<"port">>],
-                               aehttp, swagger_port_external, ?DEFAULT_SWAGGER_EXTERNAL_PORT).
-
-get_external_listen_address() ->
-    get_and_parse_ip_address_from_config_or_env([<<"http">>, <<"external">>, <<"listen_address">>],
-                                                aehttp, [http, websocket, listen_address], ?DEFAULT_SWAGGER_EXTERNAL_LISTEN_ADDRESS).
-
-get_internal_port() ->
+                               aehttp, swagger_port_external, ?DEFAULT_SWAGGER_EXTERNAL_PORT);
+get_http_api_port(internal) ->
     aeu_env:user_config_or_env([<<"http">>, <<"internal">>, <<"port">>],
                                aehttp, [internal, swagger_port], ?DEFAULT_SWAGGER_INTERNAL_PORT).
 
-get_internal_listen_address() ->
+
+get_http_api_listen_address(external) ->
+    get_and_parse_ip_address_from_config_or_env([<<"http">>, <<"external">>, <<"listen_address">>],
+                                                aehttp, [http, websocket, listen_address], ?DEFAULT_SWAGGER_EXTERNAL_LISTEN_ADDRESS);
+get_http_api_listen_address(internal) ->
     get_and_parse_ip_address_from_config_or_env([<<"http">>, <<"internal">>, <<"listen_address">>],
                                                 aehttp, [http, websocket, listen_address], ?DEFAULT_SWAGGER_INTERNAL_LISTEN_ADDRESS).
 
