9f89554f007be53f92c21f40c32860d427dec977
Dincho Todorov <dincho@users.noreply.github.com>
2018-03-06 10:49:51 +0200

    Improve API docs deployment script

--- deployment/deploy_api_docs.sh
+++ deployment/deploy_api_docs.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -ex
+#!/bin/bash
+
+set -e
+
 git clone git@github.com:aeternity/epoch-api-docs.git /tmp/epoch-api-docs
 cp apps/aehttp/priv/swagger.json /tmp/epoch-api-docs/
 cd /tmp/epoch-api-docs/
@@ -9,6 +12,8 @@ STATUS=`git status --porcelain`
 if [ -z "$STATUS" ]; then
     echo "Nothing to commit, docs did not change";
 else
+    git config user.name "CircleCI"
+    git config user.email "circleci@aeternity.com"
     git commit -a -m 'Update epoch docs CircleCI';
     git push origin master
 fi
