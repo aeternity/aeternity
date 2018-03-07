bf9d7d14f05859322c73d370d9ba52d46fcd4108
Erik Stenman <happi@happihacking.com>
2018-03-07 15:25:38 -0800

    Handle RETURNDATASIZE and RETURNDATACOPY

--- apps/aevm/src/aevm_eeevm.erl
+++ apps/aevm/src/aevm_eeevm.erl
@@ -495,17 +495,26 @@ loop(StateIn) ->
 		    State5 = aevm_eeevm_memory:write_area(Us1, CodeArea, State4),
 		    next_instruction(OP, State, State5);
 		?RETURNDATASIZE ->
-		    %% 0x3d RETURNDATASIZE
-		    %% Not in yellow paper
-		    error({opcode_not_implemented,
-			   lists:flatten(
-			     io_lib:format("~2.16.0B",[OP]))});
+		    %% 0x3d RETURNDATASIZE  δ=0 α=1
+		    %% Get size of output data from the previous call from the current
+		    %% environment.
+		    %% µ's[0] ≡ |µo|
+		    Val = byte_size(aevm_eeevm_state:return_data(State0)),
+		    State1 = push(Val, State0),
+		    next_instruction(OP, State, State1);
 		?RETURNDATACOPY ->
-		    %% 0x3e RETURNDATACOPY
-		    %% Not in yellow paper
-		    error({opcode_not_implemented,
-			   lists:flatten(
-			     io_lib:format("~2.16.0B",[OP]))});
+		    %% 0x3e RETURNDATACOPY δ=3 α=0
+		    %% Copy output data from the previous call to memory.
+		    %% ∀i∈{0...µs[2]−1}µ'm[µs[0] + i] ≡  µo[µs[1] + i] if µs[1] + i < |µo|
+		    %%                                   0                otherwise
+		    %% The additions in µs[1] + i are not subject to the 2^256 modulo.
+		    %% µ'i ≡ M(µi, µs[0], µs[2])
+		    {Us0, State1} = pop(State0), %% memOffset
+		    {Us1, State2} = pop(State1), %% dataOffset
+		    {Us2, State3} = pop(State2), %% length
+		    ReturnData = return_data_get_bytes(Us1, Us2, State3),
+		    State4 = aevm_eeevm_memory:write_area(Us0, ReturnData, State3),
+		    next_instruction(OP, State, State4);
 		%% No opcode 0x3f
 		16#3f -> throw({illegal_instruction, OP, State0});
 		%% 40s Block Information
@@ -1300,6 +1309,14 @@ data_get_val(Address, State) ->
 %% Get a binary of size Size bytes from input data.
 data_get_bytes(Address, Size, State) ->
     Data = aevm_eeevm_state:data(State),
+    try aevm_eeevm_utils:bin_copy(Address, Size, Data)
+    catch error:system_limit ->
+	    throw({out_of_memory, State})
+    end.
+
+%% Get a binary of size Size bytes from return data.
+return_data_get_bytes(Address, Size, State) ->
+    Data = aevm_eeevm_state:return_data(State),
     try aevm_eeevm_utils:bin_copy(Address, Size, Data)
     catch error:system_limit ->
 	    throw({out_of_memory, State})
