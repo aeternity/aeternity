de26c2c47089c6b20578ad7373414956ab7348b0
Fabian Krol <fabian@krol.io>
2018-03-09 06:58:02 +0100

    Add custom http api router

--- apps/aehttp/src/aehttp_api_middleware.erl
+++ apps/aehttp/src/aehttp_api_middleware.erl
@@ -4,20 +4,20 @@
 -export([execute/2]).
 
 execute(Req0, Env0) ->
-    {Operations, LogicHandler, ValidatorState} = proplists:get_value(handler_opts, Env0),
+    {OperationId, Method, LogicHandler, Validator} = proplists:get_value(handler_opts, Env0),
 
-    {Method, Req} = cowboy_req:method(Req0),
-    case maps:get(Method, Operations, undefined) of
-        undefined -> {error, 405, Req};
-        OperationId ->
-            case swagger_api:populate_request(OperationId, Req, ValidatorState) of
+    case cowboy_req:method(Req0) of
+        {Method, Req} ->
+            case swagger_api:populate_request(OperationId, Req, Validator) of
                 {ok, Params, Req1} ->
                     Env1 = proplists:delete(handler_opts, Env0),
-                    AllowedMethods = maps:keys(Operations),
+                    AllowedMethods = [Method],
                     HandlerOpts = {OperationId, AllowedMethods, LogicHandler, Params},
                     {ok, Req1, [{handler_opts, HandlerOpts} | Env1]};
                 {error, Reason, Req1} ->
                     error_logger:error_msg("Unable to process params for ~p: ~p", [OperationId, Reason]),
                     {error, 400, Req1}
-            end
+            end;
+        _ ->
+            {error, 405, Req0}
     end.
