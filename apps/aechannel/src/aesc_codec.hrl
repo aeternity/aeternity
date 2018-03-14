06fbe12cff09d81ab17b0dd4b6cb23afb508a2ee
Ulf Wiger <ulf@feuerlabs.com>
2018-03-14 18:44:25 +0100

    added aesc_codec and session module. Still w.i.p.

---
+++ apps/aechannel/src/aesc_codec.hrl
@@ -0,0 +1,22 @@
+%% Message codes for ae state channels
+
+-define(CH_OPEN     , channel_open).
+-define(CH_ACCEPT   , channel_accept).
+-define(CH_REESTABL , channel_reestablish).
+-define(FND_CREATED , funding_created).
+-define(FND_SIGNED  , funding_signed).
+-define(FND_LOCKED  , funding_locked).
+-define(UPD_DEPOSIT , update_deposit).
+-define(UPD_WITHDRAW, update_withdrawal).
+-define(SHUTDOWN    , shutdown).
+
+-define(ID_CH_OPEN     , 1).
+-define(ID_CH_ACCEPT   , 2).
+-define(ID_CH_REESTABL , 3).
+-define(ID_FND_CREATED , 4).
+-define(ID_FND_SIGNED  , 5).
+-define(ID_FND_LOCKED  , 6).
+-define(ID_UPD_DEPOSIT , 7).
+-define(ID_UPD_WITHDRAW, 8).
+-define(ID_SHUTDOWN    , 99).
+
