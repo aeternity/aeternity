e3cc849f3fbacd76d800863795b0acf8acf8f3fc
Ulf Norell <ulf.norell@quviq.com>
2018-03-16 09:21:09 +0100

    tabs to spaces

--- apps/aevm/src/aevm_eeevm_state.erl
+++ apps/aevm/src/aevm_eeevm_state.erl
@@ -9,56 +9,56 @@
 
 -module(aevm_eeevm_state).
 -export([ accountbalance/2
-	, add_trace/2
-	, add_callcreates/2
-	, address/1
-	, blockhash/3
-	, calldepth/1
+        , add_trace/2
+        , add_callcreates/2
+        , address/1
+        , blockhash/3
+        , calldepth/1
         , caller/1
         , callcreates/1
-	, code/1
-	, coinbase/1
-	, cp/1
-	, data/1
-	, difficulty/1
-	, extbalance/2
-	, extcode/2
-	, extcode/4
-	, extcodesize/2
-	, gas/1
-	, gaslimit/1
-	, gasprice/1
-	, init/1
-	, init/2
-	, jumpdests/1
-	, logs/1
-	, origin/1
+        , code/1
+        , coinbase/1
+        , cp/1
+        , data/1
+        , difficulty/1
+        , extbalance/2
+        , extcode/2
+        , extcode/4
+        , extcodesize/2
+        , gas/1
+        , gaslimit/1
+        , gasprice/1
+        , init/1
+        , init/2
+        , jumpdests/1
+        , logs/1
+        , origin/1
         , out/1
         , prepare_for_call/7
-	, mem/1
+        , mem/1
         , no_recursion/1
-	, number/1
-	, return_data/1
+        , number/1
+        , return_data/1
         , chain_state/1
         , chain_api/1
-	, set_code/2
-	, set_cp/2
-	, set_gas/2
-	, set_jumpdests/2
-	, set_logs/2
-	, set_mem/2
-	, set_out/2
-	, set_selfdestruct/2
-	, set_stack/2
-	, set_storage/2
+        , set_code/2
+        , set_cp/2
+        , set_gas/2
+        , set_jumpdests/2
+        , set_logs/2
+        , set_mem/2
+        , set_out/2
+        , set_selfdestruct/2
+        , set_stack/2
+        , set_storage/2
         , set_chain_state/2
-	, stack/1
-	, storage/1
-	, timestamp/1
-	, trace/1
-	, trace_format/3
-	, value/1
-	]).
+        , stack/1
+        , storage/1
+        , timestamp/1
+        , trace/1
+        , trace_format/3
+        , value/1
+        ]).
 
 -include("aevm_eeevm.hrl").
 
@@ -72,68 +72,68 @@ init(#{ env  := Env
     NoRecursion = maps:get(no_recursion, Opts, false),
 
     State =
-	#{ address     => Address
-	 , caller      => maps:get(caller, Exec)
-	 , return_data => maps:get(return_data, Exec, <<>>)
-	 , data        => maps:get(data, Exec)
-	 , gas         => maps:get(gas, Exec)
-	 , gas_price   => maps:get(gasPrice, Exec)
-	 , origin      => maps:get(origin, Exec)
-	 , value       => maps:get(value, Exec)
-	 , calldepth   => 0
+        #{ address     => Address
+         , caller      => maps:get(caller, Exec)
+         , return_data => maps:get(return_data, Exec, <<>>)
+         , data        => maps:get(data, Exec)
+         , gas         => maps:get(gas, Exec)
+         , gas_price   => maps:get(gasPrice, Exec)
+         , origin      => maps:get(origin, Exec)
+         , value       => maps:get(value, Exec)
+         , calldepth   => 0
 
-	 , coinbase   => maps:get(currentCoinbase, Env)
-	 , difficulty => maps:get(currentDifficulty, Env)
-	 , gas_limit  => maps:get(currentGasLimit, Env)
-	 , number     => maps:get(currentNumber, Env)
-	 , timestamp  => maps:get(currentTimestamp, Env)
+         , coinbase   => maps:get(currentCoinbase, Env)
+         , difficulty => maps:get(currentDifficulty, Env)
+         , gas_limit  => maps:get(currentGasLimit, Env)
+         , number     => maps:get(currentNumber, Env)
+         , timestamp  => maps:get(currentTimestamp, Env)
 
-	 , balances        => get_balances(Pre)
-	 , ext_code_blocks => get_ext_code_blocks(Pre)
-	 , ext_code_sizes  => get_ext_code_sizes(Pre)
-	 , block_hash_fun  => BlockHashFun
-	 , no_recursion => NoRecursion
+         , balances        => get_balances(Pre)
+         , ext_code_blocks => get_ext_code_blocks(Pre)
+         , ext_code_sizes  => get_ext_code_sizes(Pre)
+         , block_hash_fun  => BlockHashFun
+         , no_recursion => NoRecursion
 
-	 , do_trace  => maps:get(trace, Opts, false)
-	 , trace => []
-	 , trace_fun => init_trace_fun(Opts)
+         , do_trace  => maps:get(trace, Opts, false)
+         , trace => []
+         , trace_fun => init_trace_fun(Opts)
 
          , chain_state => maps:get(chainState, Env)
          , chain_api   => maps:get(chainAPI, Env)
 
-	 , environment =>
-	       #{ spec => Spec
-		, options => Opts }
+         , environment =>
+               #{ spec => Spec
+                , options => Opts }
 
-	 },
+         },
 
     init_vm(State,
-	    maps:get(code, Exec),
-	    init_storage(Address, Pre)).
+            maps:get(code, Exec),
+            init_storage(Address, Pre)).
 
 init_vm(State, Code, Store) ->
     State#{ out       => <<>>
-	  , call      => #{}
-	  , callcreates => []
-	  , code      => Code
-	  , cp        => 0
-	  , logs      => []
-	  , memory    => #{}
-	  , return_data => <<>>
-	  , stack     => []
-	  , storage   => Store
-	  }.
+          , call      => #{}
+          , callcreates => []
+          , code      => Code
+          , cp        => 0
+          , logs      => []
+          , memory    => #{}
+          , return_data => <<>>
+          , stack     => []
+          , storage   => Store
+          }.
 
 prepare_for_call(Caller, Dest, CallGas, Value, Code, CallDepth, State) ->
     #{ environment := #{ spec := #{ pre := Pre}}} = State,
     Store = init_storage(Dest, Pre),
     State1 = init_vm(State, Code, Store),
     State1#{ address => Dest
-	   , gas => CallGas
-	   , value => Value
-	   , caller    => Caller
-	   , calldepth => CallDepth + 1
-	   }.
+           , gas => CallGas
+           , value => Value
+           , caller    => Caller
+           , calldepth => CallDepth + 1
+           }.
 
 init_storage(Address, #{} = Pre) ->
     case maps:get(Address, Pre, undefined) of
@@ -159,28 +159,28 @@ get_ext_code_blocks(#{} = Pre) ->
 get_balances(#{} = Pre) ->
     maps:from_list(
       [{Address, B} || {Address, #{balance := B}}
-			   <- maps:to_list(Pre)]).
+                           <- maps:to_list(Pre)]).
 
 get_blockhash_fun(Opts, Env, H) ->
     case maps:get(blockhash, Opts, default) of
-	%% default -> fun(N,A) -> aevm_eeevm_env:get_block_hash(H,N,A) end;
-	%% sha3 ->
-	_ -> fun(N,_A) ->
-			%% Because the data of the blockchain is not
-			%% given, the opcode BLOCKHASH could not
-			%% return the hashes of the corresponding
-			%% blocks. Therefore we define the hash of
-			%% block number n to be SHA3-256("n").
-			CurrentNumber = maps:get(currentNumber, Env),
-			if (N >= CurrentNumber) or (_A == 256) or (H==0) -> 0;
-			   CurrentNumber - 256 > N -> 0;
-			   true ->
-				BinN = integer_to_binary(N),
-				Hash = aec_hash:hash(evm, BinN),
-				<<Val:256/integer-unsigned>> = Hash,
-				Val
-			end
-		end
+        %% default -> fun(N,A) -> aevm_eeevm_env:get_block_hash(H,N,A) end;
+        %% sha3 ->
+        _ -> fun(N,_A) ->
+                        %% Because the data of the blockchain is not
+                        %% given, the opcode BLOCKHASH could not
+                        %% return the hashes of the corresponding
+                        %% blocks. Therefore we define the hash of
+                        %% block number n to be SHA3-256("n").
+                        CurrentNumber = maps:get(currentNumber, Env),
+                        if (N >= CurrentNumber) or (_A == 256) or (H==0) -> 0;
+                           CurrentNumber - 256 > N -> 0;
+                           true ->
+                                BinN = integer_to_binary(N),
+                                Hash = aec_hash:hash(evm, BinN),
+                                <<Val:256/integer-unsigned>> = Hash,
+                                Val
+                        end
+                end
     end.
 
 
@@ -205,14 +205,14 @@ extcodesize(Adr, State) ->
     maps:get(Adr band ?MASK160, maps:get(ext_code_sizes, State), 0).
 extcode(Account, Start, Length, State) ->
     CodeBlock = maps:get(Account band ?MASK160,
-			 maps:get(ext_code_blocks, State), <<>>),
+                         maps:get(ext_code_blocks, State), <<>>),
     aevm_eeevm_utils:bin_copy(Start, Length, CodeBlock).
 extcode(Account, State) ->
     maps:get(Account band ?MASK160,
-	     maps:get(ext_code_blocks, State), <<>>).
+             maps:get(ext_code_blocks, State), <<>>).
 extbalance(Account, State) ->
     maps:get(Account band ?MASK160,
-	     maps:get(balances, State), <<>>).
+             maps:get(balances, State), <<>>).
 no_recursion(State) ->
     maps:get(no_recursion, State).
 
@@ -267,17 +267,17 @@ trace_format(String, Argument, State) ->
     Code = aevm_eeevm_state:code(State),
     OP   = aevm_eeevm:code_get_op(CP, Code),
     case do_trace(State) of
-	true ->
-	    F = trace_fun(State),
-	    F("[~4.16.0B] ~8.16.0B : ~w",
-	      [Account, CP, aevm_opcodes:op_name(OP)]),
-	    F(" ~w", [stack(State)]),
-	    F(" ~s", [format_mem(mem(State))]),
-	    F(" ~p", [gas(State)]),
-	    F(String, Argument),
-	    add_trace([{CP, OP, aevm_opcodes:op_name(OP)}], State);
-	false ->
-	    State
+        true ->
+            F = trace_fun(State),
+            F("[~4.16.0B] ~8.16.0B : ~w",
+              [Account, CP, aevm_opcodes:op_name(OP)]),
+            F(" ~w", [stack(State)]),
+            F(" ~s", [format_mem(mem(State))]),
+            F(" ~p", [gas(State)]),
+            F(String, Argument),
+            add_trace([{CP, OP, aevm_opcodes:op_name(OP)}], State);
+        false ->
+            State
     end.
 
 -define(MAXMEMPOS,5).
@@ -285,7 +285,7 @@ trace_format(String, Argument, State) ->
 format_mem(Mem) ->
    lists:flatten(
      "[" ++
-	 format_mem(lists:sort(maps:to_list(Mem)), ?MAXMEMPOS)
+         format_mem(lists:sort(maps:to_list(Mem)), ?MAXMEMPOS)
      ++ "]").
 format_mem([],_) -> [];
 format_mem( _,0) -> " ...";
