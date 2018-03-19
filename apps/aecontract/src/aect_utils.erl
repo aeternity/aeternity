8c97ce2fc53402b616e1853dc11284a9d1e918a2
Ulf Norell <ulf.norell@quviq.com>
2018-03-19 12:01:33 +0100

    handle money attached to contract calls
    also properly handle the case when the call is from another contract

--- apps/aecontract/src/aect_utils.erl
+++ apps/aecontract/src/aect_utils.erl
@@ -7,7 +7,7 @@
 
 -module(aect_utils).
 
--export([hex_bytes/1, hex_byte/1]).
+-export([hex_bytes/1, hex_byte/1, check_balance/3]).
 
 -include_lib("apps/aecore/include/common.hrl").
 
@@ -19,3 +19,14 @@ hex_byte(N) ->
 hex_bytes(Bin) ->
     lists:flatten("0x" ++ [io_lib:format("~2.16.0B", [B]) || <<B:8>> <= Bin]).
 
+-spec check_balance(pubkey(), aec_trees:trees(), non_neg_integer()) -> ok | {error, term()}.
+check_balance(ContractKey, Trees, Amount) ->
+    ContractsTree = aec_trees:contracts(Trees),
+    case aect_state_tree:lookup_contract(ContractKey, ContractsTree) of
+        {value, Contract} ->
+            case aect_contracts:balance(Contract) >= Amount of
+                true  -> ok;
+                false -> {error, insufficient_funds}
+            end;
+        none -> {error, contract_not_found}
+    end.
