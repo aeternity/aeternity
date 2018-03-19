944edc6504fc2dcca14199e99890024447f3b00d
Ulf Norell <ulf.norell@quviq.com>
2018-03-19 14:27:51 +0100

    test suite for VM/chain api

---
+++ test/aevm_chain_SUITE.erl
@@ -0,0 +1,135 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2018, Aeternity Anstalt
+%%% @doc CT test suite for AEVM/chain interface
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aevm_chain_SUITE).
+
+%% common_test exports
+-export([ all/0
+        , groups/0
+        ]).
+
+%% test case exports
+-export([ contracts/1
+        , spend/1
+        ]).
+
+-include_lib("common_test/include/ct.hrl").
+
+%%%===================================================================
+%%% Common test framework
+%%%===================================================================
+
+all() ->
+    [{group, all_tests}
+    ].
+
+groups() ->
+    [ {all_tests, [sequence],
+        [ {group, spend}
+        , {group, contracts}
+        ]}
+    , {spend, [sequence],
+        [ spend ]}
+    , {contracts, [sequence],
+        [ contracts ]}
+    ].
+
+%%%===================================================================
+%%% Setup
+%%%===================================================================
+
+%% Uses aect_test_utils to set up the chain, but after the setup everything is
+%% done through the aevm_chain_api.
+setup_chain() ->
+    S0              = aect_test_utils:new_state(),
+    {Account1, S1}  = aect_test_utils:setup_new_account(S0),
+    {Account2, S2}  = aect_test_utils:setup_new_account(S1),
+    {Contract1, S3} = create_contract(Account1, S2),
+    {Contract2, S4} = create_contract(Account2, S3),
+    Trees = aect_test_utils:trees(S4),
+    InitS = aevm_chain:new_state(Trees, 1, Contract1),
+    {[Account1, Account2, Contract1, Contract2], InitS}.
+
+create_contract(Owner, S) ->
+    OwnerPrivKey = aect_test_utils:priv_key(Owner, S),
+    IdContract   = aect_test_utils:compile_contract("contracts/identity.aer"),
+    CreateTx     = aect_test_utils:create_tx(Owner, #{code => IdContract, amount => 2000}, S),
+    {SignedTx, [SignedTx], S1} = sign_and_apply_transaction(CreateTx, OwnerPrivKey, S),
+    {aect_contracts:compute_contract_pubkey(Owner, aetx:nonce(CreateTx)), S1}.
+
+sign_and_apply_transaction(Tx, PrivKey, S1) ->
+    SignedTx = aetx_sign:sign(Tx, PrivKey),
+    Trees    = aect_test_utils:trees(S1),
+    Height   = 1,
+    {ok, AcceptedTxs, Trees1} = aec_trees:apply_signed_txs([SignedTx], Trees, Height),
+    S2       = aect_test_utils:set_trees(Trees1, S1),
+    {SignedTx, AcceptedTxs, S2}.
+
+get_account_balance(Acc, S) ->
+    Trees    = aevm_chain:get_trees(S),
+    Accounts = aec_trees:accounts(Trees),
+    aec_accounts:balance(aec_accounts_trees:get(Acc, Accounts)).
+
+get_contract_balance(Contract, S) ->
+    Trees     = aevm_chain:get_trees(S),
+    Contracts = aec_trees:contracts(Trees),
+    aect_contracts:balance(aect_state_tree:get_contract(Contract, Contracts)).
+
+call_data(Arg) ->
+    Code = aect_test_utils:compile_contract("contracts/identity.aer"),
+    aect_ring:create_call(Code, <<"main">>, Arg).
+
+%%%===================================================================
+%%% Spend tests
+%%%===================================================================
+
+spend(_Cfg) ->
+    {[Acc, _Acc2, _Contract1, _Contract2], S} = setup_chain(),
+    AccBal1  = get_account_balance(Acc, S),
+    Bal1     = aevm_chain:get_balance(S),
+    Amount   = 50,
+    {ok, S1} = aevm_chain:spend(Acc, Amount, S),
+    Bal2     = aevm_chain:get_balance(S1),
+    Bal2     = Bal1 - Amount,
+    AccBal2  = get_account_balance(Acc, S1),
+    AccBal2  = AccBal1 + Amount,
+    {error, insufficient_funds}
+             = aevm_chain:spend(Acc, 1000000, S1),
+    ok.
+
+%%%===================================================================
+%%% Contract tests
+%%%===================================================================
+
+contracts(_Cfg) ->
+    {[_Acc, _Acc2, _Contract1, Contract2], S} = setup_chain(),
+    _S1 = lists:foldl(fun({Value, Arg}, S0) -> make_call(Contract2, Value, Arg, S0) end,
+                      S, [{I * 100, I + 100} || I <- lists:seq(1, 10)]),
+    ok.
+
+make_call(To, Value, Arg, S) ->
+    C1Bal1   = aevm_chain:get_balance(S),
+    C2Bal1   = get_contract_balance(To, S),
+    CallData = call_data(integer_to_binary(Arg)),
+    Gas      = 10000,
+    CallRes  = aevm_chain:call_contract(To, Gas, Value, CallData, S),
+    case C1Bal1 >= Value of
+        true ->
+            {ok, Res, S1} = CallRes,
+            GasUsed  = aevm_chain_api:gas_spent(Res),
+            {ok, <<Arg:256>>} = aevm_chain_api:return_value(Res),
+            true     = GasUsed > 0,
+            true     = GasUsed =< Gas,
+            C1Bal2   = aevm_chain:get_balance(S1),
+            C1Bal2   = C1Bal1 - Value,
+            C2Bal2   = get_contract_balance(To, S1),
+            C2Bal2   = C2Bal1 + Value,
+            S1;
+        false ->
+            {error, insufficient_funds} = CallRes,
+            S
+    end.
+
+
