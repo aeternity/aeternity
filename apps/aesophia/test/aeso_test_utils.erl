22e7055edb30cccdb6a9374efd48042d9e9c14f2
Ulf Norell <ulf.norell@quviq.com>
2018-03-27 14:42:54 +0200

    update aer_test_utils:run_contract for interactive debugging

--- apps/aesophia/test/aeso_test_utils.erl
+++ apps/aesophia/test/aeso_test_utils.erl
@@ -10,10 +10,12 @@
 
 -export([read_contract/1, contract_path/0, run_contract/4, pp/1, pp/2]).
 
+-export([spend/3, get_balance/1, call_contract/6]).
+
 contract_path() ->
     {ok, Cwd} = file:get_cwd(),
     N   = length(filename:split(Cwd)),
-    Rel = ["apps", "aesophia", "test", "contracts"],
+    Rel = ["..", "apps", "aesophia", "test", "contracts"],
     %% Try the first matching directory (../)*Rel
     Cand = fun(I) -> filename:join(lists:duplicate(I, "..") ++ Rel) end,
     case [ Dir || Dir <- lists:map(Cand, lists:seq(0, N)), filelib:is_dir(Dir) ] of
@@ -37,28 +39,41 @@ pp(Name, Options) ->
             io:format("Parse error at ~p:~p:~p\n~s\n", [Name, L, C, Err])
     end.
 
-dummy_state(Code,Data) ->
-  #{ gas        => 10000,
-     code       => Code,
-     cp         => 0,
-     memory     => #{},
-     stack      => [],
-     do_trace   => false,    %% set to true for step-by-step tracing
-     trace      => [],
-     trace_fun  => fun io:format/2,
-     data       => Data
-  }.
+dummy_state(Code, Data) ->
+  aevm_eeevm_state:init(
+    #{ env =>
+        #{ currentCoinbase   => 0,
+           currentDifficulty => 0,
+           currentGasLimit   => 1000000,
+           currentNumber     => 7,
+           currentTimestamp  => 0,
+           chainAPI          => ?MODULE,
+           chainState        => no_state
+         }
+     , exec =>
+        #{ gas        => 10000,
+           code       => Code,
+           address    => 16#add7e55,
+           value      => 100,
+           caller     => 0,
+           origin     => 0,
+           call_stack => [],
+           gasPrice   => 1,
+           data       => Data
+         }
+     , pre => #{}
+     }, #{ trace => true }).
 
 run_contract(Name, Fun, Args, Type) ->
-  Code = aeso_compiler:file(Name, []), %%[pp_ast,pp_typed,pp_icode]),
+  Code = aeso_compiler:from_string(read_contract(Name), [pp_ring_code, pp_typed, pp_icode]),
 %%  io:format("\nCompiled code:\n"),
 %%  io:format("~p\n\n",[Code]),
-%%  ok = aeb_disassemble:pp(Code),
+  ok = aeb_disassemble:pp(Code),
   %% Load the call
   Call = list_to_tuple([list_to_binary(atom_to_list(Fun))|Args]),
   {0,Data} = aeso_data:to_binary(Call),
   io:format("Running:\n"),
-  State = aevm_eeevm:eval(dummy_state(Code, Data)),
+  {ok, State} = aevm_eeevm:eval(dummy_state(Code, Data)),
 %%  io:format("\nFinal state:\n~p\n",[State]),
   io:format("\nFinal stack: ~p\n",[maps:get(stack,State)++[end_of_stack]]),
   io:format("\nReturn value: ~p\n",[aeso_data:from_binary(Type,maps:get(out,State))]),
@@ -118,4 +133,16 @@ new_atom() ->
     end,
     ets:insert(names,{index,I+1}),
     list_to_atom([$a+I]).
+
+%% -- Chain API for test -----------------------------------------------------
+
+spend(Recipient, Amount, S) ->
+    io:format("+++ SPEND(~p, ~p)\n", [Recipient, Amount]),
+    {ok, S}.
+
+get_balance(_) -> 1000000.
+
+call_contract(Contract, Gas, Value, CallData, CallStack, S) ->
+    io:format("+++ CALL(~p, ~p, ~p, ~p, ~p)\n", [Contract, Gas, Value, CallData, CallStack]),
+    {ok, <<42:256>>, S}.
 
