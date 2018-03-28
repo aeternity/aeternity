9e7650c52fcb02431165a2b4e4805f36a0b10f10
Hans Svensson <hans.svensson@quviq.com>
2018-03-28 13:35:42 +0200

    Improved get_peer_key.sh

--- apps/aeutils/priv/extensions/get_peer_key.sh
+++ apps/aeutils/priv/extensions/get_peer_key.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
 
+KEYSDIR=${1:-"$HOME/node/keys"}
+PASS=${2:-"top secret"}
+
 # get the peer pubkey
 CODE="{ok, PK} = aec_keys:peer_pubkey(),
       binary_to_list(aec_base58c:encode(peer_pubkey, PK))."
 
-relx_nodetool eval $CODE
+! relx_nodetool eval $CODE
 
-exit $?
+if [ $? -eq 1 ]; then
+    exit 0
+fi
+
+CODE="{ok, Bin} = file:read_file(filename:join(\"$KEYSDIR\", \"peer_key.pub\")),
+      PwdHash = crypto:hash(sha256, \"$PASS\"),
+      PK = crypto:block_decrypt(aes_ecb, PwdHash, Bin),
+      PKStr = binary_to_list(aec_base58c:encode(peer_pubkey, PK)),
+      io:format(\"~p\\n\", [PKStr]),
+      init:stop(). "
+
+
+LIBPATH1=$PWD/lib/aecore-0.1.0/ebin
+LIBPATH2=$PWD/lib/base58-0.0.1/ebin
+
+! erl -boot no_dot_erlang -sasl errlog_type error -noshell -pa "$LIBPATH1" -pa "$LIBPATH2" -eval "$CODE"
+
+RC=$?
+if [ $RC -eq 0 ]; then
+    exit 1
+fi
+
+exit 0
