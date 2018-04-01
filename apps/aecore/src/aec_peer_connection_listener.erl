fe7b4b05bc30af8de6d536f179c0662e09c9b9cc
Hans Svensson <hans.svensson@quviq.com>
2018-04-01 14:05:51 +0200

    Make aec_peer_connection_listener trap exits
    It should really be turned into a gen_server, and the child processes
    should go in a separate supervision tree. This makes it behave better in
    the meantime...

--- apps/aecore/src/aec_peer_connection_listener.erl
+++ apps/aecore/src/aec_peer_connection_listener.erl
@@ -8,6 +8,7 @@
 
 -export([start_link/0, stop/0]).
 
+%% TODO: Make this a gen_server
 start_link() ->
     Pid = spawn_link(fun pc_listener/0),
     register(?MODULE, Pid),
@@ -22,6 +23,7 @@ pc_listener() ->
     {ok, SecKey} = aec_keys:peer_privkey(),
     {ok, PubKey} = aec_keys:peer_pubkey(),
     Port = aec_peers:sync_port(),
+    erlang:process_flag(trap_exit, true),
     lager:info("Starting peer_connection_listener at port ~p", [Port]),
     {ok, LSock} = gen_tcp:listen(Port, [{active, false}, binary, {reuseaddr, true}]),
     pc_listener(LSock, #{ seckey => SecKey, pubkey => PubKey, local_host => Host, local_port => Port }).
@@ -31,10 +33,17 @@ pc_listener(LSock, Opts) ->
     spawn_link(fun() -> acceptor(Self, LSock) end),
     receive
         {accept, {ok, TcpSock}} ->
+            %% TODO: should be started by/added to a supervisor
             aec_peer_connection:accept(TcpSock, Opts),
             pc_listener(LSock, Opts);
         {accept, Err = {error, _}} ->
             lager:error("accept failed with reason ~p", [Err]),
+            pc_listener(LSock, Opts);
+        {'EXIT', _From, shutdown} ->
+            gen_tcp:close(LSock),
+            exit(shutdown);
+        {'EXIT', From, Reason} ->
+            lager:info("~p terminated with reason ~p", [From, Reason]),
             pc_listener(LSock, Opts);
         stop ->
             gen_tcp:close(LSock)
