a9689870cfd284ebd0803dd77779d9e1120dfd90
Ulf Norell <ulf.norell@quviq.com>
2018-04-20 11:44:43 +0200

    Contract call test case: factorial
    Two copies of the factorial contract make recursive calls to each other.

--- test/contract_sophia_bytecode_aevm_SUITE.erl
+++ test/contract_sophia_bytecode_aevm_SUITE.erl
@@ -10,6 +10,7 @@
 -export(
    [ execute_identity_fun_from_sophia_file/1
    , sophia_remote_call/1
+   , sophia_factorial/1
    ]).
 
 %% chain API exports
@@ -24,26 +25,30 @@ compile_contract(Name) ->
     CodeDir           = code:lib_dir(aesophia, test),
     FileName          = filename:join([CodeDir, "contracts", lists:concat([Name, ".aes"])]),
     {ok, ContractBin} = file:read_file(FileName),
-    {ok, Code}        = aect_sophia:compile(ContractBin, <<"pp_bytecode">>),
+    {ok, Code}        = aect_sophia:compile(ContractBin, <<"pp_ast pp_icode pp_bytecode">>),
     Code.
 
-execute_call(Code, CallData, Environment) ->
+execute_call(Code, CallData, ChainState) ->
+    execute_call(Code, CallData, ChainState, #{}).
+
+execute_call(Code, CallData, ChainState, Options) ->
     Res = aect_evm:execute_call(
-          #{ code => Code,
-             address => 91210,
-             caller => 0,
-             data => CallData,
-             gas => 1000000,
-             gasPrice => 1,
-             origin => 0,
-             value => 0,
-             currentCoinbase => 0,
+          maps:merge(
+          #{ code              => Code,
+             address           => 91210,
+             caller            => 0,
+             data              => CallData,
+             gas               => 1000000,
+             gasPrice          => 1,
+             origin            => 0,
+             value             => 0,
+             currentCoinbase   => 0,
              currentDifficulty => 0,
-             currentGasLimit => 1000000,
-             currentNumber => 0,
-             currentTimestamp => 0,
-             chainState => Environment,
-             chainAPI => ?MODULE},
+             currentGasLimit   => 1000000,
+             currentNumber     => 0,
+             currentTimestamp  => 0,
+             chainState        => ChainState,
+             chainAPI          => ?MODULE}, Options),
           true),
     case Res of
         {ok, #{ out := RetVal }} -> {ok, RetVal};
@@ -60,10 +65,24 @@ sophia_remote_call(_Cfg) ->
     IdCode     = compile_contract(identity),
     CallerCode = compile_contract(remote_call),
     CallData   = aect_sophia:create_call(CallerCode, <<"call42">>, <<"1234">>),
-    Env        = #{ 1234 => IdCode },
+    Env        = #{ 1234 => IdCode, running => 1 },
     case execute_call(CallerCode, CallData, Env) of
         {ok, Result} ->
             [42] = aeso_test_utils:dump_words(Result),
+            ok;
+        {error, out_of_gas, S} ->
+            io:format("S =\n  ~p\n", [S]),
+            exit(fail)
+    end.
+
+sophia_factorial(_Cfg) ->
+    Code     = compile_contract(factorial),
+    CallData = aect_sophia:create_call(Code, <<"main">>, <<"999002">>),
+    Env      = #{ 999001 => Code, 999002 => Code
+                , running => 999001 },
+    case execute_call(Code, CallData, Env) of
+        {ok, Result} ->
+            [3628800] = aeso_test_utils:dump_words(Result),
             ok;
         {error, out_of_gas, S} ->
             io:format("S =\n  ~p\n", [S]),
@@ -76,16 +95,19 @@ spend(_, _, S) -> S.
 
 get_balance(_, _) -> 0.
 
-call_contract(Contract, _Gas, _Value, CallData, _, S) ->
-    io:format("Calling contract ~p with args ~p\n", [Contract, CallData]),
+call_contract(Contract, _Gas, _Value, CallData, _, S = #{running := Caller}) ->
+    io:format("Calling contract ~p with args ~p\n",
+              [Contract, aeso_test_utils:dump_words(CallData)]),
     case S of
         #{ Contract := Code } ->
-            Res = execute_call(Code, CallData, S),
-            io:format("  result = ~p\n", [Res]),
+            Env = #{address => Contract, caller => Caller},
+            Res = execute_call(Code, CallData, S#{running := Contract}, Env),
             case Res of
                 {ok, Ret} ->
+                    io:format("  result = ~p\n", [aeso_test_utils:dump_words(Ret)]),
                     {ok, aec_vm_chain_api:call_result(Ret, 0), S};
                 {error, out_of_gas, _} ->
+                    io:format("  result = out_of_gas\n"),
                     {ok, aec_vm_chain_api:call_exception(out_of_gas, 0), S}
             end;
         _ ->
