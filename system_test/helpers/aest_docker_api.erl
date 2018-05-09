237df98e4c5087bfd65cf8b2c3b8cb14c77f9af2
Dincho Todorov <dincho@users.noreply.github.com>
2018-05-09 11:00:17 +0300

    Even higher docker timeouts

--- system_test/helpers/aest_docker_api.erl
+++ system_test/helpers/aest_docker_api.erl
@@ -31,7 +31,7 @@
 -define(BASE_URL, <<"http+unix://%2Fvar%2Frun%2Fdocker.sock/">>).
 -define(CONTAINER_STOP_TIMEOUT, 30000).
 -define(CONTAINER_KILL_TIMEOUT_EXTRA, 5000).
--define(DEFAULT_TIMEOUT, 20000).
+-define(DEFAULT_TIMEOUT, 50000).
 %% It seems there is no way to disable the killing timeout,
 %% it just default to 10 seconds. So we just set a big one...
 -define(CONTAINER_STOP_INFINTY, 24*60*60).
