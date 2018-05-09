c0a9cf6402923cdd6a6ba7c3139c14b9fea05de5
Adam Lindberg <hello@alind.io>
2018-05-09 11:59:45 +0200

    Add many nodes stay in sync test

--- system_test/aest_perf_SUITE.erl
+++ system_test/aest_perf_SUITE.erl
@@ -15,7 +15,8 @@
 % Test cases
 -export([
     startup_speed/1,
-    sync_speed/1
+    sync_speed/1,
+    stay_in_sync/1
 ]).
 
 -import(aest_nodes, [
@@ -41,6 +42,8 @@
     source  => {pull, "aeternity/epoch:local"}
 }).
 
+-define(cfg(Keys), cfg(Keys, Cfg)).
+
 %=== COMMON TEST FUNCTIONS =====================================================
 
 all() -> [
@@ -50,7 +53,8 @@ all() -> [
 groups() ->
     [{long_chain, [], [
         startup_speed,
-        sync_speed
+        sync_speed,
+        stay_in_sync
     ]}].
 
 init_per_suite(Cfg) ->
@@ -68,19 +72,17 @@ init_per_group(long_chain, InitCfg) ->
     Cfg = aest_nodes:ct_setup(InitCfg),
 
     Nodes = [n1, n2, n3],
-    Height = proplists:get_value(height, Cfg),
-    MineRate = proplists:get_value(mine_rate, Cfg),
-    MineTimeout = proplists:get_value(mine_timeout, Cfg),
+    Height = ?cfg(height),
 
-    setup_nodes(cluster(Nodes, #{mine_rate => MineRate}), Cfg),
+    setup_nodes(cluster(Nodes, #{mine_rate => ?cfg(mine_rate)}), Cfg),
     [start_node(N, Cfg) || N <- Nodes],
     wait_for_startup(Nodes, 0, Cfg),
-    wait_for_value({height, Height}, Nodes, MineTimeout * Height, Cfg),
+    wait_for_value({height, Height}, Nodes, ?cfg(mine_timeout) * Height, Cfg),
     Tag = io_lib:format("local-h~b", [Height]),
     Ref = aest_nodes:export(n1, Tag, Cfg),
     [kill_node(N, Cfg) || N <- Nodes],
     aest_nodes:ct_cleanup(Cfg),
-    [{source, Ref}, {height, Height}, {mine_rate, MineRate}|Cfg].
+    [{source, Ref}|Cfg].
 
 end_per_group(long_chain, _Cfg) -> ok.
 
@@ -93,25 +95,21 @@ end_per_suite(_Cfg) -> ok.
 %=== TEST CASES ================================================================
 
 startup_speed(Cfg) ->
-    Height = proplists:get_value(height, Cfg),
-    Source = proplists:get_value(source, Cfg),
-    StartupTimeout = proplists:get_value(startup_timeout, Cfg),
+    Height = ?cfg(height),
 
-    setup_nodes([spec(node, [], #{source => Source})], Cfg),
+    setup_nodes([spec(node, [], #{source => ?cfg(source)})], Cfg),
     start_node(node, Cfg),
     wait_for_startup([node], Height, Cfg),
-    wait_for_value({height, Height}, [node], StartupTimeout, Cfg).
+    wait_for_value({height, Height}, [node], ?cfg(startup_timeout), Cfg).
 
 sync_speed(Cfg) ->
-    Height = proplists:get_value(height, Cfg),
-    Source = proplists:get_value(source, Cfg),
-    MineRate = proplists:get_value(mine_rate, Cfg),
+    [Height, MineRate] = ?cfg([height, mine_rate]),
 
     InitialNodes = [n1, n2, n3, n4],
 
     InitialNodeSpecs = cluster(InitialNodes, #{
         mine_rate => MineRate,
-        source => Source
+        source => ?cfg(source)
     }),
     NewNodesSpec = [
         spec(N, InitialNodes, #{mine_rate => MineRate})
@@ -138,6 +136,20 @@ sync_speed(Cfg) ->
 
     [?assertEqual(A, B) || A <- AllBlocks, B <- AllBlocks, A =/= B].
 
+stay_in_sync(Cfg) ->
+    Height = ?cfg(height),
+    Nodes = [n1, n2, n3, n4, n5, n6],
+    setup_nodes(cluster(Nodes, #{
+        mine_rate => ?cfg(mine_rate),
+        source => ?cfg(source)
+    }), Cfg),
+    [start_node(N, Cfg) || N <- Nodes],
+    wait_for_startup(Nodes, Height, Cfg),
+    wait_for_value({height, Height + 500}, Nodes, ?cfg(mine_timeout) * 500, Cfg),
+
+    Blocks = [{N, get_block(N, Height + 500, Cfg)} || N <- Nodes],
+    [?assertEqual(AB, BB) || {AN, AB} <- Blocks, {BN, BB} <- Blocks, AN =/= BN].
+
 %=== INTERNAL FUNCTIONS ========================================================
 
 cluster(Names, Spec) -> [spec(N, Names -- [N], Spec) || N <- Names].
@@ -155,3 +167,11 @@ wait_for_sync(Nodes, Height, Cfg) ->
 
 get_block(NodeName, Height, Cfg) ->
     request(NodeName, [v2, 'block-by-height'], #{height => Height}, Cfg).
+
+cfg(Keys, Cfg) when is_list(Keys) ->
+    [cfg(K, Cfg) || K <- Keys];
+cfg(Key, Cfg) ->
+    case proplists:get_value(Key, Cfg) of
+        undefined -> exit({key_not_found, Key});
+        Value     -> Value
+    end.
