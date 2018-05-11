2214c6d7e885b835ecc9ef12861d4faa6dbf1e79
Robert Virding <rvirding@gmail.com>
2018-05-11 18:16:25 +0200

    Pt 157220182 compile dutch auction (#1121)
    * Make sure dutch_auction compiles
    
    Note that we don't check the compiled file yet.
    
    * Add sophia compiler tests
    
    These are very simple and currently only test that the contract is
    compilable. There is no checking of the output. We have also added a
    call to this test module in the CT suite.
    
    * Add bools as ints, true -> 1 and false -> 0
    
    * Fix getting the contract into the compiler
    
    The interface to the compiler is a bit weird so yo need a bit of
    trickery.

---
+++ apps/aesophia/test/aeso_compiler_tests.erl
@@ -0,0 +1,46 @@
+%%% -*- erlang-indent-level:4; indent-tabs-mode: nil -*-
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2018, Aeternity Anstalt
+%%% @doc Test Sophia language compiler.
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+
+-module(aeso_compiler_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+%% simple_compile_test_() -> ok.
+%%  Very simply test compile the given contracts. Only basic checks
+%%  are made on the output, just that it is a binary which indicates
+%%  that the compilation worked.
+
+simple_compile_test_() ->
+    {setup,
+     fun () -> ok end,                          %Setup
+     fun (_) -> ok end,                         %Cleanup
+     [ {"Testing the " ++ ContractName ++ " contract",
+        fun() ->
+                ContractString = aeso_test_utils:read_contract(ContractName),
+                ?assertMatch(Code when is_binary(Code),
+                                       aeso_compiler:from_string(ContractString, []))
+
+        end} ||
+         ContractName <- compilable_contracts() ]}.
+
+%% compilable_contracts() -> [ContractName].
+%%  The currently compilable contracts.
+
+compilable_contracts() ->
+    ["complex_types",
+     "counter",
+     "dutch_auction",
+     "environment",
+     "factorial",
+     "identity",
+     "remote_call",
+     "simple",
+     "spend_test",
+     "stack",
+     "test"
+    ].
