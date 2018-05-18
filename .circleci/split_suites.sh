70175c967853c27d5ec964edfe024f05ea8e51ac
Dincho Todorov <dincho@users.noreply.github.com>
2018-05-18 11:16:27 +0300

    Improfile basename readability

--- .circleci/split_suites.sh
+++ .circleci/split_suites.sh
@@ -3,6 +3,6 @@
 set -e
 
 ls -1 apps/*/test/*SUITE.erl test/*SUITE.erl > suites.txt
-cat suites.txt | sed 's/.*\///g' | sed 's/.erl//g' > classes.txt
+xargs -I {} basename {} .erl < suites.txt > classes.txt
 circleci tests split --split-by=timings --timings-type=classname classes.txt > batch.txt
 grep -f batch.txt suites.txt | paste -s -d ',' -
