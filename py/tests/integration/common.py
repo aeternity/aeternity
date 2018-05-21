f1d3fa4c4f025ccbde88510cb57440fc89313f68
Tobias Lindahl <tobias.lindahl@gmail.com>
2018-05-21 11:30:47 +0200

    Update hard forks protocol versions for python tests

--- py/tests/integration/common.py
+++ py/tests/integration/common.py
@@ -103,7 +103,7 @@ def make_no_mining_user_config(root_dir, file_name):
 ---
 chain:
     hard_forks:
-        "12": 0
+        "13": 0
 
 mining:
     autostart: false
@@ -121,7 +121,7 @@ def make_mining_user_config(root_dir, file_name):
 ---
 chain:
     hard_forks:
-        "12": 0
+        "13": 0
 
 mining:
     autostart: true
