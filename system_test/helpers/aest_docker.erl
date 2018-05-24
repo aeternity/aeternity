046ec48db11996a3d4a35255c7e659f4201bf417
Thomas Arts <thomas.arts@quviq.com>
2018-05-24 11:05:28 +0200

    Add ext_ws to aest_docker
    Add get_hostname API

--- system_test/helpers/aest_docker.erl
+++ system_test/helpers/aest_docker.erl
@@ -35,6 +35,7 @@
 -define(EXT_SYNC_PORT, 3015).
 -define(INT_HTTP_PORT, 3113).
 -define(INT_WS_PORT, 3114).
+-define(EXT_WS_PORT, 3014).
 -define(EPOCH_STOP_TIMEOUT, 30000).
 -define(PEER_KEYS_PASSWORD, <<"top secret">>).
 -define(DEFAULT_NETWORKS, [epoch]).
@@ -43,7 +44,7 @@
 
 -type log_fun() :: fun((io:format(), list()) -> ok) | undefined.
 -type test_uid() :: binary() | undefined.
--type service_label() :: ext_http | int_http | int_ws.
+-type service_label() :: ext_http | int_http | int_ws | ext_ws.
 
 %% State of the docker backend
 -type backend_state() :: #{
@@ -167,7 +168,7 @@ setup_node(Spec, BackendState) ->
         int_http => ?INT_HTTP_PORT,
         int_ws => ?INT_WS_PORT
     },
-    {LocalPorts, Sockets} = allocate_ports([sync, ext_http, int_http, int_ws]),
+    {LocalPorts, Sockets} = allocate_ports([sync, ext_http, int_http, int_ws, ext_ws]),
     NodeState = #{
         postfix => Postfix,
         log_fun => LogFun,
@@ -217,6 +218,7 @@ setup_node(Spec, BackendState) ->
             sync => #{port => ?EXT_SYNC_PORT},
             ext_http => #{port => ?EXT_HTTP_PORT},
             int_http => #{port => ?INT_HTTP_PORT},
+            ext_ws => #{port => ?EXT_WS_PORT},
             int_ws => #{port => ?INT_WS_PORT}
         },
         mining => maps:merge(#{autostart => true}, maps:get(mining, Spec, #{}))
@@ -330,8 +332,9 @@ get_service_address(Service, NodeState)
   when Service == ext_http; Service == int_http ->
     #{local_ports := #{Service := Port}} = NodeState,
     format("http://localhost:~w/", [Port]);
-get_service_address(int_ws, NodeState) ->
-    #{local_ports := #{int_ws := Port}} = NodeState,
+get_service_address(Service, NodeState) 
+  when Service == ext_ws; Service == int_ws ->
+    #{local_ports := #{Service := Port}} = NodeState,
     format("ws://localhost:~w/", [Port]).
 
 -spec get_node_pubkey(node_state()) -> binary().
