4ddabf4c9a92030f1af650f9bef61d18f8947d3c
happi <happi@happihacking.se>
2018-05-24 13:28:58 +0200

    Fix assertion in test.

--- py/tests/integration/test_unsigned_tx.py
+++ py/tests/integration/test_unsigned_tx.py
@@ -190,7 +190,8 @@ def test_contract_on_chain_call_off_chain():
                                    call_contract["data"]["argument"])
     result = external_api.call_contract(call_input)
 
-    '0x000000000000000000000000000000000000000000000000000000000000002a' = result.out
+     assert_equals('0x000000000000000000000000000000000000000000000000000000000000002a',
+                   result.out)
 
     cleanup(node, root_dir)
 
