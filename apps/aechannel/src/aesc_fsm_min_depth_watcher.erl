ecfa85a1e5249285c6ac50d8d06a12e54ca824c3
Luca Favatella <luca.favatella@erlang-solutions.com>
2018-05-31 10:06:20 +0100

    Teach channel watcher to react to top changed for block pulled from network

--- apps/aechannel/src/aesc_fsm_min_depth_watcher.erl
+++ apps/aechannel/src/aesc_fsm_min_depth_watcher.erl
@@ -28,12 +28,15 @@ watch(Watcher, Type, TxHash, MinDepth) ->
 init(#{} = Arg) ->
     lager:debug("started min_depth watcher for ~p", [maps:get(fsm, Arg)]),
     true = aec_events:subscribe(top_changed),
+    true = aec_events:subscribe(top_synced),
     true = aec_events:subscribe(block_created),
-    lager:debug("subscribed to top_changed & block_created", []),
+    lager:debug("subscribed to top_changed & top_synced & block_created", []),
     self() ! check_status,
     {ok, Arg}.
 
 handle_info({gproc_ps_event, top_changed, _}, St) ->
+    check_status(St);
+handle_info({gproc_ps_event, top_synced, _}, St) ->
     check_status(St);
 handle_info({gproc_ps_event, block_created, _}, St) ->
     check_status(St);
