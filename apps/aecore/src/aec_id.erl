26275fc2a16a00d99349033075368f08b443df6d
Tobias Lindahl <tobias.lindahl@gmail.com>
2018-06-08 15:46:57 +0200

    Add infrastructure to encode and decode aec_id in serialization

--- apps/aecore/src/aec_id.erl
+++ apps/aecore/src/aec_id.erl
@@ -13,6 +13,11 @@
         , specialize/2
         ]).
 
+%% For aec_serialization
+-export([ encode/1
+        , decode/1
+        ]).
+
 -record(id, { tag
             , val
             }).
@@ -27,6 +32,9 @@
              ]).
 
 -define(PUB_SIZE, 32).
+-define(TAG_SIZE, 1).
+-define(SERIALIZED_SIZE, 33). %% ?TAG_SIZE + ?PUB_SIZE
+
 -define(IS_TAG(___TAG___), ___TAG___ =:= account;
                            ___TAG___ =:= oracle;
                            ___TAG___ =:= name;
@@ -55,3 +63,30 @@ specialize(#id{tag = Tag, val = Val}) ->
 -spec specialize(id(), tag()) -> val().
 specialize(#id{tag = Tag, val = Val}, Tag) when ?IS_TAG(Tag), ?IS_VAL(Val) ->
     Val.
+
+-spec encode(id()) -> binary().
+encode(#id{tag = Tag, val = Val}) ->
+    Res = <<(encode_tag(Tag)):?TAG_SIZE/unit:8, Val/binary>>,
+    true = ?SERIALIZED_SIZE =:= byte_size(Res),
+    Res.
+
+-spec decode(binary()) -> id().
+decode(<<Tag:?TAG_SIZE/unit:8, Val:?PUB_SIZE/binary>>) ->
+    #id{ tag = decode_tag(Tag)
+       , val = Val}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+encode_tag(account)    -> 1;
+encode_tag(name)       -> 2;
+encode_tag(commitment) -> 3;
+encode_tag(oracle)     -> 4;
+encode_tag(Other)      -> error({illegal_id_tag_name, Other}).
+
+decode_tag(1) -> account;
+decode_tag(2) -> name;
+decode_tag(3) -> commitment;
+decode_tag(4) -> oracle;
+decode_tag(X) -> error({illegal_id_tag, X}).
---
+++ apps/aecore/test/aec_id_tests.erl
@@ -0,0 +1,51 @@
+%%% -*- erlang-indent-level:4; indent-tabs-mode: nil -*-
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2018, Aeternity Anstalt
+%%% @doc
+%%% Tests for aec_id
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aec_id_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+basic_test_() ->
+    lists:append(
+      [
+       [ {"Create and specialize " ++ atom_to_list(Type), test_create(Type)}
+       , {"Encoding/decoding " ++ atom_to_list(Type), test_encoding(Type)}
+       ]
+       || Type <- all_tags()
+      ]).
+
+%%%=============================================================================
+%%% Basic tests
+
+test_create(Type) ->
+    fun() ->
+            Bin = id_bin(),
+            Id  = aec_id:create(Type, Bin),
+            ?assertEqual({Type, Bin}, aec_id:specialize(Id)),
+            ?assertEqual(Bin, aec_id:specialize(Id, Type)),
+            ?assertError(_, aec_id:specialize(Id, foo))
+    end.
+
+test_encoding(Type) ->
+    fun() ->
+            Bin = id_bin(),
+            Id  = aec_id:create(Type, Bin),
+            ?assertEqual(Id, aec_id:decode(aec_id:encode(Id)))
+    end.
+
+%%%=============================================================================
+%%% Tools
+
+id_bin() ->
+    <<12345:32/unit:8>>.
+
+all_tags() ->
+    [ account
+    , name
+    , commitment
+    , oracle
+    ].
