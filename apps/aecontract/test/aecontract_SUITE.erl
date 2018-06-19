9143e3d97c2da7ddfc3fa8876e83f501a40b86e5
sennui <michal@sennui.com>
2018-06-19 13:24:56 +0200

    first batch of aec_block_candidate removal (chaning into micro)

--- apps/aecontract/test/aecontract_SUITE.erl
+++ apps/aecontract/test/aecontract_SUITE.erl
@@ -223,7 +223,7 @@ sign_and_apply_transaction(Tx, PrivKey, S1, Miner) ->
     Trees    = aect_test_utils:trees(S1),
     Height   = 1,
     {ok, AcceptedTxs, Trees1} =
-        aec_block_candidate:apply_block_txs([SignedTx], Miner, Trees, Height, ?PROTOCOL_VERSION),
+        aec_block_micro_candidate:apply_block_txs([SignedTx], Miner, Trees, Height, ?PROTOCOL_VERSION),
     S2       = aect_test_utils:set_trees(Trees1, S1),
     case AcceptedTxs of
         [SignedTx] -> {ok, S2};
@@ -236,7 +236,7 @@ sign_and_apply_transaction_strict(Tx, PrivKey, S1, Miner) ->
     Height   = 1,
     ConsensusVersion = aec_hard_forks:protocol_effective_at_height(Height),
     {ok, AcceptedTxs, Trees1} =
-        aec_block_candidate:apply_block_txs_strict([SignedTx], Miner, Trees, Height, ConsensusVersion),
+        aec_block_micro_candidate:apply_block_txs_strict([SignedTx], Miner, Trees, Height, ConsensusVersion),
     S2       = aect_test_utils:set_trees(Trees1, S1),
     {SignedTx, AcceptedTxs, S2}.
 
