Gabriellexionggungun/epoch

Added AENS.resolve primitive to Sophia

apps/aecontract/test/aecontract_SUITE.erl
@@ -25,6 +25,7 @@
         , sophia_oracles/1
         , sophia_maps/1
         , sophia_fundme/1
+        , sophia_aens/1
         , create_store/1
         , update_store/1
         , read_store/1
@@ -69,7 +70,8 @@ groups() ->
                                  sophia_spend,
                                  sophia_oracles,
                                  sophia_maps,
-                                 sophia_fundme ]}
+                                 sophia_fundme,
+                                 sophia_aens ]}
     , {store, [sequence], [ create_store
                           , update_store
                           , read_store
@@ -825,6 +827,90 @@ sophia_fundme(_Cfg) ->
 
     run_scenario(Funded),
     run_scenario(NotFunded).
+
+%% AENS tests
+
+preclaim(PubKey, Name, S) ->
+    preclaim(PubKey, Name, #{}, S).
+
+preclaim(PubKey, Name, Options, S) ->
+    Salt   = rand:uniform(10000),
+    Nonce  = aect_test_utils:next_nonce(PubKey, S),
+    Height = maps:get(height, Options, 1),
+    Fee    = maps:get(fee, Options, 1),
+    TTL    = maps:get(ttl, Options, 1000),
+    {ok, NameAscii} = aens_utils:to_ascii(Name),
+    CHash = aens_hash:commitment_hash(NameAscii, Salt),
+    {ok, Tx} = aens_preclaim_tx:new(#{ account => PubKey,
+                                       nonce => Nonce,
+                                       commitment => CHash,
+                                       fee => Fee,
+                                       ttl => TTL }),
+    PrivKey  = aect_test_utils:priv_key(PubKey, S),
+    {ok, S1} = sign_and_apply_transaction(Tx, PrivKey, S, ?MINER_PUBKEY, Height),
+    {Salt, S1}.
+
+claim(PubKey, Name, Salt, S) ->
+    claim(PubKey, Name, Salt, #{}, S).
+
+claim(PubKey, Name, Salt, Options, S) ->
+    Nonce  = aect_test_utils:next_nonce(PubKey, S),
+    Height = maps:get(height, Options, 2),
+    Fee    = maps:get(fee, Options, 1),
+    TTL    = maps:get(ttl, Options, 1000),
+    {ok, NameAscii} = aens_utils:to_ascii(Name),
+    NameHash = aens_hash:name_hash(NameAscii),
+    {ok, Tx} = aens_claim_tx:new(#{ account   => PubKey,
+                                    nonce     => Nonce,
+                                    name      => Name,
+                                    name_salt => Salt,
+                                    fee       => Fee,
+                                    ttl       => TTL }),
+    PrivKey  = aect_test_utils:priv_key(PubKey, S),
+    {ok, S1} = sign_and_apply_transaction(Tx, PrivKey, S, ?MINER_PUBKEY, Height),
+    {NameHash, S1}.
+
+aens_update(PubKey, NameHash, Pointers, S) ->
+    aens_update(PubKey, NameHash, Pointers, #{}, S).
+
+aens_update(PubKey, NameHash, Pointers, Options, S) ->
+    Nonce     = aect_test_utils:next_nonce(PubKey, S),
+    Height    = maps:get(height, Options, 2),
+    Fee       = maps:get(fee, Options, 1),
+    TTL       = maps:get(ttl, Options, 1000),
+    ClientTTL = maps:get(client_ttl, Options, 1000),
+    NameTTL   = maps:get(name_ttl, Options, 1000),
+    {ok, Tx}  = aens_update_tx:new(#{ account    => PubKey,
+                                      nonce      => Nonce,
+                                      name_hash  => NameHash,
+                                      name_ttl   => NameTTL,
+                                      pointers   => Pointers,
+                                      client_ttl => ClientTTL,
+                                      fee        => Fee,
+                                      ttl        => TTL }),
+    PrivKey  = aect_test_utils:priv_key(PubKey, S),
+    {ok, S1} = sign_and_apply_transaction(Tx, PrivKey, S, ?MINER_PUBKEY, Height),
+    {ok, S1}.
+
+sophia_aens(_Cfg) ->
+    state(aect_test_utils:new_state()),
+    Acc      = ?call(new_account, 1000000),
+    Ct       = ?call(create_contract, Acc, aens, {}),
+    Name     = <<"foo.test">>,
+    NameKey  = 101101,
+    URL      = <<"shadycorp.com">>,
+    Pointers = [ {atom_to_binary(Key, utf8), aec_base58c:encode(Key, Val)}
+                || {Key, Val} <- [ {account_pubkey, <<NameKey:256>>},
+                                   {name, URL} ] ],
+    Salt  = ?call(preclaim, Acc, Name),
+    Hash  = ?call(claim, Acc, Name, Salt),
+    ok    = ?call(aens_update, Acc, Hash, Pointers),
+
+    {some, NameKey} = ?call(call_contract, Acc, Ct, lookup_word,   {option, word},   {Name, <<"account_pubkey">>}),
+    none            = ?call(call_contract, Acc, Ct, lookup_word,   {option, word},   {Name, <<"oracle_pubkey">>}),
+    {some, URL}     = ?call(call_contract, Acc, Ct, lookup_string, {option, string}, {Name, <<"name">>}),
+    ok.
+
 
 %%%===================================================================
 %%% Store

apps/aesophia/src/aeso_ast_to_icode.erl
@@ -186,6 +186,22 @@ ast_body({qid, _, ["Oracle", "query_fee"]})    -> error({underapplied_primitive,
 ast_body({qid, _, ["Oracle", "get_answer"]})   -> error({underapplied_primitive, 'Oracle.get_answer'});
 ast_body({qid, _, ["Oracle", "get_question"]}) -> error({underapplied_primitive, 'Oracle.get_question'});
 
+%% Name service
+ast_body(?qid_app(["AENS", "resolve"], [Name, Key], _, ?option_t(Type))) ->
+    case is_monomorphic(Type) of
+        true ->
+            case ast_type(Type) of
+                T when T == word; T == string -> ok;
+                _ -> error({invalid_result_type, 'AENS.resolve', Type})
+            end,
+            prim_call(?PRIM_CALL_AENS_LOOKUP, #integer{value = 0},
+                      [ast_body(Name), ast_body(Key), ast_type_value(Type)],
+                      [string, string, typerep], {option, ast_type(Type)});
+        false ->
+            error({unresolved_result_type, 'AENS.resolve', Type})
+    end;
+ast_body({qid, _, ["AENS", "resolve"]}) -> error({underapplied_primitive, 'AENS.resolve'});
+
 %% Maps
 
 %% -- map lookup  m[k]

apps/aevm/src/aevm_ae_primops.erl
@@ -24,7 +24,9 @@ call(Value, Data, State) ->
                 {?PRIM_CALL_SPEND, Recipient} = DecodeAs({tuple, [word, word]}),
                 spend(Recipient, Value, State);
             {PrimOp} when ?PRIM_CALL_IN_ORACLE_RANGE(PrimOp) ->
-                oracle_call(PrimOp, Value, Data, State)
+                oracle_call(PrimOp, Value, Data, State);
+            {PrimOp} when ?PRIM_CALL_IN_AENS_RANGE(PrimOp) ->
+                aens_call(PrimOp, Value, Data, State)
         end
     catch _:_Err ->
 	%% TODO: Better error for illegal call.
@@ -167,6 +169,24 @@ oracle_call_query_fee(_Value, Data, State) ->
     [Oracle] = get_args(ArgumentTypes, Data),
     Callback = fun(API, ChainState) -> API:oracle_query_fee(<<Oracle:256>>, ChainState) end,
     query_chain(Callback, State).
+
+%% ------------------------------------------------------------------
+%% AENS operations.
+%% ------------------------------------------------------------------
+
+aens_call(?PRIM_CALL_AENS_LOOKUP, _Value, Data, State) ->
+    aens_call_lookup(Data, State);
+aens_call(_, _, _, _) ->
+    {error, out_of_gas}.
+
+aens_call_lookup(Data, State) ->
+    [Name, Key, Type] = get_args([string, string, typerep], Data),
+    Callback = fun(API, ChainState) -> API:aens_lookup(Name, Key, Type, ChainState) end,
+    query_chain(Callback, State).
+
+%% ------------------------------------------------------------------
+%% Internal functions
+%% ------------------------------------------------------------------
 
 get_args(Types, Data) ->
     {ok, Val} = aeso_data:from_binary(?BASE_ADDRESS, {tuple, [word | Types]}, Data),

apps/aevm/src/aevm_chain_api.erl
@@ -45,8 +45,8 @@
                           Sign :: binary(),
                           QueryFee :: non_neg_integer(),
                           TTL :: non_neg_integer(),
-                          DecodedQType :: binary(),
-                          DecodedRType :: binary(),
+                          DecodedQType :: aeso_sophia:type(),
+                          DecodedRType :: aeso_sophia:type(),
                           ChainState :: chain_state()) ->
     {ok, OracleKey :: pubkey(), chain_state()} | {error, term()}.
 
@@ -100,6 +100,14 @@
 -callback oracle_query_fee(Oracle :: pubkey(),
                            ChainState :: chain_state()) ->
     {ok, non_neg_integer()} | {error, term()}.
+
+%% -- Name Services --
+
+-callback aens_lookup(Name :: binary(),
+                      Key :: binary(),
+                      Type :: aeso_sophia:type(),
+                      ChainState :: chain_state()) ->
+    {ok, none | {some, pubkey()}} | {error, term()}.
 
 %% Make a call to another contract.
 -callback call_contract(Contract  :: pubkey(),

aeternity/aeternity

apps/aecontract/test/aecontract_SUITE.erl
@@ -25,6 +25,7 @@
         , sophia_oracles/1
         , sophia_maps/1
         , sophia_fundme/1
+        , sophia_aens/1
         , create_store/1
         , update_store/1
         , read_store/1
@@ -69,7 +70,8 @@ groups() ->
                                  sophia_spend,
                                  sophia_oracles,
                                  sophia_maps,
-                                 sophia_fundme ]}
+                                 sophia_fundme,
+                                 sophia_aens ]}
     , {store, [sequence], [ create_store
                           , update_store
                           , read_store
@@ -825,6 +827,90 @@ sophia_fundme(_Cfg) ->
 
     run_scenario(Funded),
     run_scenario(NotFunded).
+
+%% AENS tests
+
+preclaim(PubKey, Name, S) ->
+    preclaim(PubKey, Name, #{}, S).
+
+preclaim(PubKey, Name, Options, S) ->
+    Salt   = rand:uniform(10000),
+    Nonce  = aect_test_utils:next_nonce(PubKey, S),
+    Height = maps:get(height, Options, 1),
+    Fee    = maps:get(fee, Options, 1),
+    TTL    = maps:get(ttl, Options, 1000),
+    {ok, NameAscii} = aens_utils:to_ascii(Name),
+    CHash = aens_hash:commitment_hash(NameAscii, Salt),
+    {ok, Tx} = aens_preclaim_tx:new(#{ account => PubKey,
+                                       nonce => Nonce,
+                                       commitment => CHash,
+                                       fee => Fee,
+                                       ttl => TTL }),
+    PrivKey  = aect_test_utils:priv_key(PubKey, S),
+    {ok, S1} = sign_and_apply_transaction(Tx, PrivKey, S, ?MINER_PUBKEY, Height),
+    {Salt, S1}.
+
+claim(PubKey, Name, Salt, S) ->
+    claim(PubKey, Name, Salt, #{}, S).
+
+claim(PubKey, Name, Salt, Options, S) ->
+    Nonce  = aect_test_utils:next_nonce(PubKey, S),
+    Height = maps:get(height, Options, 2),
+    Fee    = maps:get(fee, Options, 1),
+    TTL    = maps:get(ttl, Options, 1000),
+    {ok, NameAscii} = aens_utils:to_ascii(Name),
+    NameHash = aens_hash:name_hash(NameAscii),
+    {ok, Tx} = aens_claim_tx:new(#{ account   => PubKey,
+                                    nonce     => Nonce,
+                                    name      => Name,
+                                    name_salt => Salt,
+                                    fee       => Fee,
+                                    ttl       => TTL }),
+    PrivKey  = aect_test_utils:priv_key(PubKey, S),
+    {ok, S1} = sign_and_apply_transaction(Tx, PrivKey, S, ?MINER_PUBKEY, Height),
+    {NameHash, S1}.
+
+aens_update(PubKey, NameHash, Pointers, S) ->
+    aens_update(PubKey, NameHash, Pointers, #{}, S).
+
+aens_update(PubKey, NameHash, Pointers, Options, S) ->
+    Nonce     = aect_test_utils:next_nonce(PubKey, S),
+    Height    = maps:get(height, Options, 2),
+    Fee       = maps:get(fee, Options, 1),
+    TTL       = maps:get(ttl, Options, 1000),
+    ClientTTL = maps:get(client_ttl, Options, 1000),
+    NameTTL   = maps:get(name_ttl, Options, 1000),
+    {ok, Tx}  = aens_update_tx:new(#{ account    => PubKey,
+                                      nonce      => Nonce,
+                                      name_hash  => NameHash,
+                                      name_ttl   => NameTTL,
+                                      pointers   => Pointers,
+                                      client_ttl => ClientTTL,
+                                      fee        => Fee,
+                                      ttl        => TTL }),
+    PrivKey  = aect_test_utils:priv_key(PubKey, S),
+    {ok, S1} = sign_and_apply_transaction(Tx, PrivKey, S, ?MINER_PUBKEY, Height),
+    {ok, S1}.
+
+sophia_aens(_Cfg) ->
+    state(aect_test_utils:new_state()),
+    Acc      = ?call(new_account, 1000000),
+    Ct       = ?call(create_contract, Acc, aens, {}),
+    Name     = <<"foo.test">>,
+    NameKey  = 101101,
+    URL      = <<"shadycorp.com">>,
+    Pointers = [ {atom_to_binary(Key, utf8), aec_base58c:encode(Key, Val)}
+                || {Key, Val} <- [ {account_pubkey, <<NameKey:256>>},
+                                   {name, URL} ] ],
+    Salt  = ?call(preclaim, Acc, Name),
+    Hash  = ?call(claim, Acc, Name, Salt),
+    ok    = ?call(aens_update, Acc, Hash, Pointers),
+
+    {some, NameKey} = ?call(call_contract, Acc, Ct, resolve_word,   {option, word},   {Name, <<"account_pubkey">>}),
+    none            = ?call(call_contract, Acc, Ct, resolve_word,   {option, word},   {Name, <<"oracle_pubkey">>}),
+    {some, URL}     = ?call(call_contract, Acc, Ct, resolve_string, {option, string}, {Name, <<"name">>}),
+    ok.
+
 
 %%%===================================================================
 %%% Store

apps/aesophia/src/aeso_ast_to_icode.erl
@@ -186,6 +186,22 @@ ast_body({qid, _, ["Oracle", "query_fee"]})    -> error({underapplied_primitive,
 ast_body({qid, _, ["Oracle", "get_answer"]})   -> error({underapplied_primitive, 'Oracle.get_answer'});
 ast_body({qid, _, ["Oracle", "get_question"]}) -> error({underapplied_primitive, 'Oracle.get_question'});
 
+%% Name service
+ast_body(?qid_app(["AENS", "resolve"], [Name, Key], _, ?option_t(Type))) ->
+    case is_monomorphic(Type) of
+        true ->
+            case ast_type(Type) of
+                T when T == word; T == string -> ok;
+                _ -> error({invalid_result_type, 'AENS.resolve', Type})
+            end,
+            prim_call(?PRIM_CALL_AENS_RESOLVE, #integer{value = 0},
+                      [ast_body(Name), ast_body(Key), ast_type_value(Type)],
+                      [string, string, typerep], {option, ast_type(Type)});
+        false ->
+            error({unresolved_result_type, 'AENS.resolve', Type})
+    end;
+ast_body({qid, _, ["AENS", "resolve"]}) -> error({underapplied_primitive, 'AENS.resolve'});
+
 %% Maps
 
 %% -- map lookup  m[k]

apps/aevm/src/aevm_ae_primops.erl
@@ -24,7 +24,9 @@ call(Value, Data, State) ->
                 {?PRIM_CALL_SPEND, Recipient} = DecodeAs({tuple, [word, word]}),
                 spend(Recipient, Value, State);
             {PrimOp} when ?PRIM_CALL_IN_ORACLE_RANGE(PrimOp) ->
-                oracle_call(PrimOp, Value, Data, State)
+                oracle_call(PrimOp, Value, Data, State);
+            {PrimOp} when ?PRIM_CALL_IN_AENS_RANGE(PrimOp) ->
+                aens_call(PrimOp, Value, Data, State)
         end
     catch _:_Err ->
 	%% TODO: Better error for illegal call.
@@ -167,6 +169,24 @@ oracle_call_query_fee(_Value, Data, State) ->
     [Oracle] = get_args(ArgumentTypes, Data),
     Callback = fun(API, ChainState) -> API:oracle_query_fee(<<Oracle:256>>, ChainState) end,
     query_chain(Callback, State).
+
+%% ------------------------------------------------------------------
+%% AENS operations.
+%% ------------------------------------------------------------------
+
+aens_call(?PRIM_CALL_AENS_RESOLVE, _Value, Data, State) ->
+    aens_call_resolve(Data, State);
+aens_call(_, _, _, _) ->
+    {error, out_of_gas}.
+
+aens_call_resolve(Data, State) ->
+    [Name, Key, Type] = get_args([string, string, typerep], Data),
+    Callback = fun(API, ChainState) -> API:aens_resolve(Name, Key, Type, ChainState) end,
+    query_chain(Callback, State).
+
+%% ------------------------------------------------------------------
+%% Internal functions
+%% ------------------------------------------------------------------
 
 get_args(Types, Data) ->
     {ok, Val} = aeso_data:from_binary(?BASE_ADDRESS, {tuple, [word | Types]}, Data),

apps/aevm/src/aevm_chain_api.erl
@@ -45,8 +45,8 @@
                           Sign :: binary(),
                           QueryFee :: non_neg_integer(),
                           TTL :: non_neg_integer(),
-                          DecodedQType :: binary(),
-                          DecodedRType :: binary(),
+                          DecodedQType :: aeso_sophia:type(),
+                          DecodedRType :: aeso_sophia:type(),
                           ChainState :: chain_state()) ->
     {ok, OracleKey :: pubkey(), chain_state()} | {error, term()}.
 
@@ -100,6 +100,14 @@
 -callback oracle_query_fee(Oracle :: pubkey(),
                            ChainState :: chain_state()) ->
     {ok, non_neg_integer()} | {error, term()}.
+
+%% -- Name Services --
+
+-callback aens_resolve(Name :: binary(),
+                       Key :: binary(),
+                       Type :: aeso_sophia:type(),
+                       ChainState :: chain_state()) ->
+    {ok, none | {some, term()}} | {error, term()}.
 
 %% Make a call to another contract.
 -callback call_contract(Contract  :: pubkey(),