fac521379c024672adfc70c479f8e8bb4adb95f5
Tobias Lindahl <tobias.lindahl@gmail.com>
2018-07-06 14:58:39 +0200

    Allow base58 parsing to and from an aec_id:id()

--- apps/aecore/src/aec_base58c.erl
+++ apps/aecore/src/aec_base58c.erl
@@ -20,12 +20,16 @@
                     | commitment
                     | peer_pubkey
                     | state
-                    | poi.
+                    | poi
+                    | id_hash. %% Actual type is in the aec_id:id()
 
 -type payload() :: binary().
 -type encoded() :: binary().
 
--spec encode(known_type(), payload()) -> encoded().
+-spec encode(known_type(), payload() | aec_id:id()) -> encoded().
+encode(id_hash, Payload) ->
+    {IdType, Val} = aec_id:specialize(Payload),
+    encode(id2type(IdType), Val);
 encode(Type, Payload) ->
     Pfx = type2pfx(Type),
     Enc = base58_check(Payload),
@@ -59,6 +63,10 @@ type_size_check(Type, Bin) ->
 
 safe_decode(Type, Enc) ->
     try decode(Enc) of
+        {ActualType, Dec} when Type =:= id_hash ->
+            try {ok, aec_id:create(type2id(ActualType), Dec)}
+            catch error:_ -> {error, invalid_prefix}
+            end;
         {Type, Dec} ->
             {ok, Dec};
         {_, _} ->
@@ -88,6 +96,20 @@ check_str(Bin) ->
     <<C:32/bitstring,_/binary>> =
         aec_hash:sha256_hash(aec_hash:sha256_hash(Bin)),
     C.
+
+id2type(account)    -> account_pubkey;
+id2type(channel)    -> channel;
+id2type(commitment) -> commitment;
+id2type(contract)   -> contract_pubkey;
+id2type(name)       -> name;
+id2type(oracle)     -> oracle_pubkey.
+
+type2id(account_pubkey)  -> account;
+type2id(channel)         -> channel;
+type2id(commitment)      -> commitment;
+type2id(contract_pubkey) -> contract;
+type2id(name)            -> name;
+type2id(oracle_pubkey)   -> oracle.
 
 
 type2pfx(block_hash)       -> <<"bh">>;
