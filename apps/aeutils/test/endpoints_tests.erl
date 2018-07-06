0765ecb34ee7b181b119fb44bbfabf46cff90eae
Juraj Hlista <jurajhlista@gmail.com>
2018-07-06 13:29:52 +0200

    Fix eunit tests

--- apps/aeutils/test/endpoints_tests.erl
+++ apps/aeutils/test/endpoints_tests.erl
@@ -5,20 +5,20 @@
 -include_lib("eunit/include/eunit.hrl").
 
 operation_method_test() ->
-   ?assertEqual([get], maps:keys(endpoints:operation('GetTop'))).
+   ?assertEqual([get], maps:keys(endpoints:operation('GetTopBlock'))).
 
 path_without_test() ->
-   ?assertEqual(<<"/v2/blocks/top">>, endpoints:path(get, 'GetTop', #{})).
+   ?assertEqual(<<"/v2/blocks/top">>, endpoints:path(get, 'GetTopBlock', #{})).
 
 path_with_params_test() ->
    ?assertEqual(<<"/v2/block/height/3">>,
-      iolist_to_binary(endpoints:path(get, 'GetKeyBlockByHeight',
+      iolist_to_binary(endpoints:path(get, 'GetKeyBlockByHeightObsolete',
                                       #{"height" => 3, "tx_objects" => true}))),
    ?assertEqual(<<"/v2/block/height/3">>,
-      iolist_to_binary(endpoints:path(get, 'GetKeyBlockByHeight',
+      iolist_to_binary(endpoints:path(get, 'GetKeyBlockByHeightObsolete',
                                       #{"height" => 3}))),
    ?assertThrow({error, _},
-      iolist_to_binary(endpoints:path(get, 'GetKeyBlockByHeight',
+      iolist_to_binary(endpoints:path(get, 'GetKeyBlockByHeightObsolete',
                                       #{}))).
 
 -endif.
