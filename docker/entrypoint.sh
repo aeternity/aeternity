27feba10552570b53203b09464530217933ce93f
Luca Favatella <luca.favatella@erlang-solutions.com>
2018-07-12 10:22:03 +0100

    Remove unneeded Docker default config now that external config is mandatory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-# Use TestNet example config as configuration template
-cp docs/examples/epoch_testnet.yaml epoch.yaml
-
 # Using console with extra arguments because "foreground" does not handle SIGTERM/SIGQUIT
 exec ./bin/epoch console -noshell -noinput $@
