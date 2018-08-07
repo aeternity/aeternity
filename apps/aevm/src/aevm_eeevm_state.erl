c21a3bfe2887f6457a5ea21f2a06b0e2b7197320
Luca Favatella <luca.favatella@erlang-solutions.com>
2018-08-07 10:53:00 +0100

    Remove default VM code - needs to be binary (see `jumpdests`) not map
    Also it is always set so unused.  All code paths explicit code; the
    only area where code may be not explicated is in external tests though
    that code seems set there too
    [example](https://github.com/ethereum/tests/blob/348b0633f4a6ee3c100368bf0f4fca71394b4d01/VMTests/vmArithmeticTest/add0.json#L22).

--- apps/aevm/src/aevm_eeevm_state.erl
+++ apps/aevm/src/aevm_eeevm_state.erl
@@ -145,7 +145,7 @@ init(#{ env  := Env
          },
 
     init_vm(State,
-            maps:get(code, Exec, #{}),
+            maps:get(code, Exec),
             maps:get(mem, Exec, #{mem_size => 0}),
             ChainAPI:get_store(ChainState)).
 
