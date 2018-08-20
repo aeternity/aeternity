498bda232d23256a5bb3543b38afd18203f95ad6
Hans Svensson <hans.svensson@quviq.com>
2018-08-20 11:30:12 +0200

    Remove unused function determine_delta_header_height/1

--- apps/aecore/src/aec_target.erl
+++ apps/aecore/src/aec_target.erl
@@ -2,30 +2,9 @@
 
 %% API
 -export([recalculate/2,
-         determine_delta_header_height/1,
          verify/2]).
 
 -include("blocks.hrl").
-
-%% Return height of the header to be used as a start point for target calculations,
-%% based on the following formula:
-%% delta_height(Header) = Header.height - aec_governance:key_blocks_to_check_difficulty_count().
-%% Returns {error | chain_too_short_to_recalculate_target} if initial height is a negative value
-%% or it points to genesis block.
--spec determine_delta_header_height(
-        aec_headers:header()) -> {ok, non_neg_integer()}
-                         | {error, chain_too_short_to_recalculate_target}.
-determine_delta_header_height(Header) ->
-    Height = aec_headers:height(Header),
-    BlocksCount = aec_governance:key_blocks_to_check_difficulty_count(),
-    InitialHeight = Height - BlocksCount,
-    GenesisHeight = aec_block_genesis:height(),
-    case InitialHeight > GenesisHeight of
-        true ->
-            {ok, InitialHeight};
-        false ->
-            {error, chain_too_short_to_recalculate_target}
-    end.
 
 %% Target recalculation.
 %%
