ee57391fb14def066ee6d5a6a610aff6f8b14493
Thomas Arts <thomas.arts@quviq.com>
2018-08-29 11:16:06 +0200

    API changes to system tests
    GetPendingTransactions instead of GetTxs
    
    This moved from external to a different tag..
    
    Enable internal HTTP interface in aest_sync_SUITE
    
    Skip channel tests
    
    fix space insertion

--- system_test/helpers/aest_nodes.erl
+++ system_test/helpers/aest_nodes.erl
@@ -184,7 +184,7 @@ ct_cleanup(Ctx) ->
     wait_for_exit(Pid, ?CALL_TIMEOUT),
     case Result of
         {error, Reason} ->
-            %% returning fail will cause common test to see it as test failure 
+            %% returning fail will cause common test to see it as test failure
             {fail, Reason};
         ok -> ok
     end.
@@ -321,6 +321,7 @@ spec(Name, Peers, Spec) ->
 request(Node, Id, Params) ->
     aehttp_client:request(Id, Params, [
         {ext_http, aest_nodes_mgr:get_service_address(Node, ext_http)},
+        {int_http, aest_nodes_mgr:get_service_address(Node, int_http)},
         {ct_log, true}
     ]).
 
@@ -346,7 +347,7 @@ get_block(NodeName, Height) ->
     end.
 
 get_top(NodeName) ->
-    {ok, 200, Top} = request(NodeName, 'GetTop', #{}),
+    {ok, 200, Top} = request(NodeName, 'GetTopBlock', #{}),
     Top.
 
 -spec wait_for_value({balance, binary(), non_neg_integer()}, [atom()], milliseconds(), test_ctx()) -> ok;
@@ -355,7 +356,7 @@ get_top(NodeName) ->
 wait_for_value({balance, PubKey, MinBalance}, NodeNames, Timeout, _Ctx) ->
     CheckF =
         fun(Node) ->
-                case request(Node, 'GetAccountBalance', #{address => PubKey}) of
+                 case request(Node, 'GetAccountByPubkey', #{pubkey => PubKey}) of
                     {ok, 200, #{balance := Balance}} when Balance >= MinBalance -> {done, #{PubKey => Balance}};
                     _ -> wait
                 end
