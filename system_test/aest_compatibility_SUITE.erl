352993f2a1c3288c10edd68ac2ec3bf972ed6d66
Sébastien Merle <s.merle@gmail.com>
2018-10-02 09:56:59 +0200

    Add test verifying compatibility between lean16 and mean16s-generic (#1610)

---
+++ system_test/aest_compatibility_SUITE.erl
@@ -0,0 +1,107 @@
+-module(aest_compatibility_SUITE).
+
+%=== EXPORTS ===================================================================
+
+% Common Test exports
+-export([all/0]).
+-export([init_per_suite/1]).
+-export([init_per_testcase/2]).
+-export([end_per_testcase/2]).
+-export([end_per_suite/1]).
+
+% Test cases
+-export([
+    test_mining_algorithms_compatibility/1
+]).
+
+-import(aest_nodes, [
+    setup_nodes/2,
+    start_node/2,
+    wait_for_value/4,
+    wait_for_startup/3,
+    get_block/2
+]).
+
+%=== INCLUDES ==================================================================
+
+-include_lib("eunit/include/eunit.hrl").
+
+%=== MACROS ====================================================================
+
+-define(MINING_TIMEOUT,   3000).
+
+-define(NODE1, #{
+    name    => node1,
+    peers   => [],
+    backend => aest_docker,
+    source  => {pull, "aeternity/epoch:local"}
+}).
+
+-define(NODE2, #{
+    name    => node2,
+    peers   => [node1],
+    backend => aest_docker,
+    source  => {pull, "aeternity/epoch:local"}
+}).
+
+%=== COMMON TEST FUNCTIONS =====================================================
+
+all() -> [
+    test_mining_algorithms_compatibility
+].
+
+init_per_suite(Config) ->
+    [
+        {node_startup_time, 20000}, %% Time may take to get the node to respond to http
+        {node_shutdown_time, 20000} %% Time it may take to stop node cleanly
+    | Config].
+
+init_per_testcase(_TC, Config) ->
+    aest_nodes:ct_setup(Config).
+
+end_per_testcase(_TC, Config) ->
+    aest_nodes:ct_cleanup(Config).
+
+end_per_suite(_Config) -> ok.
+
+%=== TEST CASES ================================================================
+
+test_mining_algorithms_compatibility(Cfg) ->
+    Length = 20,
+
+    #{ public := PubKey1 } = enacl:sign_keypair(),
+    #{ public := PubKey2 } = enacl:sign_keypair(),
+    Beneficiary1 = aec_base58c:encode(account_pubkey, PubKey1),
+    Beneficiary2 = aec_base58c:encode(account_pubkey, PubKey2),
+
+    %% Setup nodes
+    setup([?NODE1], #{ beneficiary => Beneficiary1, algorithm => <<"mean16s-generic">> }, Cfg),
+    setup([?NODE2], #{ beneficiary => Beneficiary2, algorithm => <<"lean16">> }, Cfg),
+    start_node(node1, Cfg),
+    start_node(node2, Cfg),
+    wait_for_startup([node1, node2], 0, Cfg),
+
+    wait_for_value({height, Length + 1}, [node1, node2],
+                   (Length + 1) * ?MINING_TIMEOUT, Cfg),
+
+    B1 = get_block(node1, Length),
+    B2 = get_block(node2, Length),
+
+    ?assertNotEqual(undefined, B1),
+    ?assertEqual(B1, B2),
+
+    % Be sure both nodes created some blocks
+    Beneficiaries = lists:foldl(fun(L, M) ->
+        #{ beneficiary := Beneficiary } = get_block(node1, L),
+        M#{ Beneficiary => true }
+    end, #{}, lists:seq(Length, 1, -1)),
+
+    ?assertEqual(lists:sort([Beneficiary1, Beneficiary2]),
+                 lists:sort(maps:keys(Beneficiaries))),
+
+    ok.
+
+%=== INTERNAL FUNCTIONS ========================================================
+
+setup(NodeSpecs, Config, Cfg) ->
+    setup_nodes([maps:put(config, Config, N) || N <- NodeSpecs], Cfg).
