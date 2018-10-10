d2782f4a86bd8af5522d34681e7fabf21740762f
Luca Favatella <luca.favatella@erlang-solutions.com>
2018-10-10 16:43:47 +0100

    Bump consensus version since last release

--- py/tests/integration/test_use_cases.py
+++ py/tests/integration/test_use_cases.py
@@ -304,7 +304,7 @@ keys:
 
 chain:
     hard_forks:
-        "24": 0
+        "25": 0
 
 mining:
     autostart: true
@@ -334,7 +334,7 @@ keys:
 
 chain:
     hard_forks:
-        "24": 0
+        "25": 0
 
 mining:
     autostart: false
