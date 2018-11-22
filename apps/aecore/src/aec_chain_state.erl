coderntester/epoch

Lock excess tokens not attributed to a fraudelent miner

apps/aecore/src/aec_chain_state.erl
@@ -879,40 +879,54 @@ grant_fees(Node, Trees, Delay, FraudStatus, State) ->
     Beneficiary1 = node_beneficiary(KeyNode1),
     Beneficiary2 = node_beneficiary(KeyNode2),
     Reward = aec_governance:block_mine_reward(),
+    Key1Reward = Reward,
     FraudReward = aec_governance:fraud_report_reward(),
     BeneficiaryReward1 = KeyFees * 4 div 10,
-    BeneficiaryReward2 = KeyFees - BeneficiaryReward1 + Reward,
-    case {FraudStatus1, FraudStatus2} of
-        {true, true} ->
-            %% The miner of KeyNode1 was reported by the miner of KeyNode2
-            %% but that miner was reported by the miner of KeyNode3.
-            %% No rewards at all.
-            Trees;
-        {true, false} ->
-            %% The miner of KeyNode1 was reported by the miner of KeyNode2
-            %% and that miner was a well behaved miner.
-            FinalReward = BeneficiaryReward2 + FraudReward,
-            aec_trees:grant_fee(Beneficiary2, Trees, FinalReward);
-        {false, true} ->
-            %% The miner of KeyNode2 was reported by the miner of KeyNode3
-            %% but that reward will come later.
-            case node_is_genesis(KeyNode1, State) of
-                true ->
-                    Trees;
-                false ->
-                    aec_trees:grant_fee(Beneficiary1, Trees, BeneficiaryReward1)
-            end;
-        {false, false} ->
-            %% No fraud in sight. Well done!
-            Trees1 =
+    KeyFeesBen2 = KeyFees - BeneficiaryReward1,
+    BeneficiaryReward2 = KeyFeesBen2 + Reward,
+    {Trees2, LockAmount} =
+        case {FraudStatus1, FraudStatus2} of
+            {true, true} ->
+                %% The miner of KeyNode1 was reported by the miner of KeyNode2
+                %% but that miner was reported by the miner of KeyNode3.
+                %% No rewards at all. Lock them both
+                {Trees, BeneficiaryReward1 + BeneficiaryReward2};
+            {true, false} ->
+                %% The miner of KeyNode1 was reported by the miner of KeyNode2
+                %% and that miner was a well behaved miner.
+                FinalReward = BeneficiaryReward2 + FraudReward,
+                %% lock the excess coins remaning after miner fees for the
+                %% fraudulent miner1 but also the mining reward for block1
+                Locked = Key1Reward + BeneficiaryReward1 - FraudReward,
+                {aec_trees:grant_fee(Beneficiary2, Trees, FinalReward), Locked};
+            {false, true} ->
+                %% The miner of KeyNode2 was reported by the miner of KeyNode3
+                %% but that reward will come later.
+                %% Lock the fees for the fraudulent miner2 but not the miner
+                %% reward, it will be locked on the next iteration
                 case node_is_genesis(KeyNode1, State) of
                     true ->
-                        Trees;
+                        {Trees, KeyFeesBen2};
                     false ->
-                        aec_trees:grant_fee(Beneficiary1, Trees, BeneficiaryReward1)
-                end,
-            aec_trees:grant_fee(Beneficiary2, Trees1, BeneficiaryReward2)
-    end.
+                        {aec_trees:grant_fee(Beneficiary1, Trees,
+                                            BeneficiaryReward1), KeyFeesBen2}
+                end;
+            {false, false} ->
+                %% No fraud in sight. Well done!
+                Trees1 =
+                    case node_is_genesis(KeyNode1, State) of
+                        true ->
+                            Trees;
+                        false ->
+                            aec_trees:grant_fee(Beneficiary1, Trees, BeneficiaryReward1)
+                    end,
+                % no fraud, no locking
+                {aec_trees:grant_fee(Beneficiary2, Trees1, BeneficiaryReward2), 0}
+        end,
+    Accounts0 = aec_trees:accounts(Trees2),
+    Accounts = aec_accounts_trees:lock_coins(LockAmount, Accounts0),
+    aec_trees:set_accounts(Trees2, Accounts).
+
 
 calculate_gas_fee(Calls) ->
     F = fun(_, SerCall, GasFeeIn) ->

apps/aecore/test/aecore_pof_SUITE.erl
@@ -98,7 +98,8 @@ siblings_on_key_block(Config) ->
 
     Top = lists:last(N1Blocks2),
     ?assertEqual(key, aec_blocks:type(Top)),
-    siblings_common(Top, N1, N2, Account1, Account2).
+    siblings_common(Top, N1, N2, Account1, Account2, _Fraud = false).
+
 
 siblings_on_micro_block(Config) ->
     ct:pal("Setting up nodes"),
@@ -129,7 +130,7 @@ siblings_on_micro_block(Config) ->
 
     ?assertEqual(micro, aec_blocks:type(Top)),
 
-    siblings_common(Top, N1, N2, Account1, Account2).
+    siblings_common(Top, N1, N2, Account1, Account2, _Fraud = true).
 
 ensure_top_is_a_micro(_Node,_Account, Nonce) when Nonce > 5 ->
     %% We give it five attempts then fail.
@@ -144,25 +145,28 @@ ensure_top_is_a_micro(Node, Account, Nonce) ->
         key -> ensure_top_is_a_micro(Node, Account, Nonce + 1)
     end.
 
-siblings_common(TopBlock, N1, N2, Account1, Account2) ->
+siblings_common(TopBlock, N1, N2, Account1, Account2, Fraud) ->
     ct:pal("Waiting for sync"),
     %% Wait until the chain is synced.
     {ok, ExpectedTop1} = aec_blocks:hash_internal_representation(TopBlock),
+    0 = get_lock_holder_balance(N1),
+    0 = get_lock_holder_balance(N2),
     aec_test_utils:wait_for_it_or_timeout(
       fun() -> rpc:call(N2, aec_chain, top_block_hash, []) end,
       ExpectedTop1,
       10000),
 
     N1KeyBlocksCount = aec_blocks:height(TopBlock),
+    SpendFee = 17000,
 
     ct:pal("Starting to test fraud"),
     %% Add a transaction and create the first micro block
-    {ok, _Tx1} = add_spend_tx(N1, 1000, 17000,  1,  10, Account1, new_pubkey()),
+    {ok, _Tx1} = add_spend_tx(N1, 1000, SpendFee,  1,  10, Account1, new_pubkey()),
     {ok, Micro1, _} =  rpc:call(N1, aec_block_micro_candidate, create, [TopBlock]),
     {ok, Micro1S} = rpc:call(N1, aec_keys, sign_micro_block, [Micro1]),
 
     %% Add another transaction and create the second micro block
-    {ok, _Tx2} = add_spend_tx(N1, 1000, 17000,  2,  10, Account1, new_pubkey()),
+    {ok, _Tx2} = add_spend_tx(N1, 1000, SpendFee,  2,  10, Account1, new_pubkey()),
     {ok, Micro2, _} =  rpc:call(N1, aec_block_micro_candidate, create, [TopBlock]),
     {ok, Micro2S} = rpc:call(N1, aec_keys, sign_micro_block, [Micro2]),
 
@@ -174,7 +178,7 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     {ok, [Key2]} = aecore_suite_utils:mine_key_blocks(N2, 1),
 
     %% Now we need a micro block to report the fraud in.
-    {ok, _} = add_spend_tx(N2, 1000, 17000,  1,  10, Account2, new_pubkey()),
+    {ok, _} = add_spend_tx(N2, 1000, SpendFee,  1,  10, Account2, new_pubkey()),
     {ok, MicroFraud, _} = rpc:call(N2, aec_block_micro_candidate, create, [Key2]),
     {ok, MicroFraudS} = rpc:call(N2, aec_keys, sign_micro_block, [MicroFraud]),
 
@@ -191,7 +195,7 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     end,
 
     %% Manually add the report and check that the fraud would not be reported again.
-    {ok, _} = add_spend_tx(N2, 1000, 17000,  2,  10, Account2, new_pubkey()),
+    {ok, _} = add_spend_tx(N2, 1000, SpendFee,  2,  10, Account2, new_pubkey()),
     ok = rpc:call(N2, aec_conductor, post_block, [MicroFraudS]),
     {ok, MicroNoFraud, _} = rpc:call(N2, aec_block_micro_candidate, create, [MicroFraudS]),
     ?assertEqual(no_fraud, aec_blocks:pof(MicroNoFraud)),
@@ -210,14 +214,15 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     {value, Acc1} = rpc:call(N2, aec_chain, get_account, [B1]),
     {value, Acc2} = rpc:call(N2, aec_chain, get_account, [B2]),
     ct:log("Balances: ~p and ~p", [Acc1, Acc2]),
-    Bal1 = aec_accounts:balance(Acc1),
-    Bal2 = aec_accounts:balance(Acc2),
 
     %% Node1 has mined N1KeyBlocksCount blocks, but should only get N1KeyBlocksCount - 1 rewards (fraud)
     %% Node2 has mined N2KeyBlocksCount blocks, but should only get N2KeyBlocksCount - 2 rewards (delay)
     %%                                                                               + 1 fraud reward.
     MR = aec_governance:block_mine_reward(),
     FR = aec_governance:fraud_report_reward(),
+
+    Bal1 = aec_accounts:balance(Acc1),
+    Bal2 = aec_accounts:balance(Acc2),
     case Bal1 >= (N1KeyBlocksCount - 1) * MR andalso Bal1 < (N1KeyBlocksCount - 1) * MR + 100000 of %% should get some fees
         true -> ok;
         false -> error({bad_balance1, Bal1})
@@ -231,7 +236,13 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     Top2 = rpc:call(N2, aec_chain, top_block, []),
     aecore_suite_utils:wait_for_height(N1, aec_blocks:height(Top2)),
     Top1 = rpc:call(N1, aec_chain, top_block, []),
-    ?assertEqual(Top1, Top2).
+    ?assertEqual(Top1, Top2),
+
+    Locked = get_lock_holder_balance(N1),
+    {Locked, Locked} = {Locked, get_lock_holder_balance(N2)},
+    true = Locked >= MR - FR, % some fees
+    true = Locked =< MR.
+
 
 %% ============================================================
 %% Helpers
@@ -259,3 +270,10 @@ new_pubkey() ->
     #{ public := PubKey } = enacl:sign_keypair(),
     PubKey.
 
+get_lock_holder_balance(N) ->
+    HolderPubKey = aec_governance:locked_coins_holder_account(),
+    case rpc:call(N, aec_chain, get_account, [HolderPubKey]) of
+        {value, Acc} ->
+            _Bal = aec_accounts:balance(Acc);
+        none -> 0
+    end.

aeternity/aeternity

apps/aecore/src/aec_chain_state.erl
@@ -859,43 +859,57 @@ grant_fees(Node, Trees, Delay, FraudStatus, State) ->
     Beneficiary1 = node_beneficiary(KeyNode1),
     Beneficiary2 = node_beneficiary(KeyNode2),
     %% We give the mining reward for the closing block of the generation.
-    MineReward = aec_governance:block_mine_reward(node_height(KeyNode2)),
+    MineReward1 = aec_governance:block_mine_reward(node_height(KeyNode1)),
+    MineReward2 = aec_governance:block_mine_reward(node_height(KeyNode2)),
     %% Fraud rewards is given for the opening block of the generation
     %% since this is the reward that was withheld.
     FraudReward = aec_governance:fraud_report_reward(node_height(KeyNode1)),
     BeneficiaryReward1 = KeyFees * 4 div 10,
-    BeneficiaryReward2 = KeyFees - BeneficiaryReward1 + MineReward,
-    case {FraudStatus1, FraudStatus2} of
-        {true, true} ->
-            %% The miner of KeyNode1 was reported by the miner of KeyNode2
-            %% but that miner was reported by the miner of KeyNode3.
-            %% No rewards at all.
-            Trees;
-        {true, false} ->
-            %% The miner of KeyNode1 was reported by the miner of KeyNode2
-            %% and that miner was a well behaved miner.
-            FinalReward = BeneficiaryReward2 + FraudReward,
-            aec_trees:grant_fee(Beneficiary2, Trees, FinalReward);
-        {false, true} ->
-            %% The miner of KeyNode2 was reported by the miner of KeyNode3
-            %% but that reward will come later.
-            case node_is_genesis(KeyNode1, State) of
-                true ->
-                    Trees;
-                false ->
-                    aec_trees:grant_fee(Beneficiary1, Trees, BeneficiaryReward1)
-            end;
-        {false, false} ->
-            %% No fraud in sight. Well done!
-            Trees1 =
+    KeyFeesBen2 = KeyFees - BeneficiaryReward1,
+    BeneficiaryReward2 = KeyFeesBen2 + MineReward2,
+    {Trees2, LockAmount} =
+        case {FraudStatus1, FraudStatus2} of
+            {true, true} ->
+                %% The miner of KeyNode1 was reported by the miner of KeyNode2
+                %% but that miner was reported by the miner of KeyNode3.
+                %% No rewards at all. Lock them both
+                {Trees, BeneficiaryReward1 + BeneficiaryReward2};
+            {true, false} ->
+                %% The miner of KeyNode1 was reported by the miner of KeyNode2
+                %% and that miner was a well behaved miner.
+                FinalReward = BeneficiaryReward2 + FraudReward,
+                %% lock the excess coins remaning after miner fees for the
+                %% fraudulent miner1 but also the mining reward for block1
+                Locked = MineReward1 + BeneficiaryReward1 - FraudReward,
+                {aec_trees:grant_fee(Beneficiary2, Trees, FinalReward), Locked};
+            {false, true} ->
+                %% The miner of KeyNode2 was reported by the miner of KeyNode3
+                %% but that reward will come later.
+                %% Lock the fees for the fraudulent miner2 but not the miner
+                %% reward, it will be locked on the next iteration
                 case node_is_genesis(KeyNode1, State) of
                     true ->
-                        Trees;
+                        {Trees, KeyFeesBen2};
                     false ->
-                        aec_trees:grant_fee(Beneficiary1, Trees, BeneficiaryReward1)
-                end,
-            aec_trees:grant_fee(Beneficiary2, Trees1, BeneficiaryReward2)
-    end.
+                        {aec_trees:grant_fee(Beneficiary1, Trees,
+                                            BeneficiaryReward1), KeyFeesBen2}
+                end;
+            {false, false} ->
+                %% No fraud in sight. Well done!
+                Trees1 =
+                    case node_is_genesis(KeyNode1, State) of
+                        true ->
+                            Trees;
+                        false ->
+                            aec_trees:grant_fee(Beneficiary1, Trees, BeneficiaryReward1)
+                    end,
+                % no fraud, no locking
+                {aec_trees:grant_fee(Beneficiary2, Trees1, BeneficiaryReward2), 0}
+        end,
+    Accounts0 = aec_trees:accounts(Trees2),
+    Accounts = aec_accounts_trees:lock_coins(LockAmount, Accounts0),
+    aec_trees:set_accounts(Trees2, Accounts).
+
 
 calculate_gas_fee(Calls) ->
     F = fun(_, SerCall, GasFeeIn) ->

apps/aecore/test/aecore_pof_SUITE.erl
@@ -101,7 +101,8 @@ siblings_on_key_block(Config) ->
 
     Top = lists:last(N1Blocks2),
     ?assertEqual(key, aec_blocks:type(Top)),
-    siblings_common(Top, N1, N2, Account1, Account2).
+    siblings_common(Top, N1, N2, Account1, Account2, _Fraud = false).
+
 
 siblings_on_micro_block(Config) ->
     ct:pal("Setting up nodes"),
@@ -135,7 +136,7 @@ siblings_on_micro_block(Config) ->
 
     ?assertEqual(micro, aec_blocks:type(Top)),
 
-    siblings_common(Top, N1, N2, Account1, Account2).
+    siblings_common(Top, N1, N2, Account1, Account2, _Fraud = true).
 
 ensure_top_is_a_micro(_Node,_Account, Nonce) when Nonce > 5 ->
     %% We give it five attempts then fail.
@@ -150,25 +151,28 @@ ensure_top_is_a_micro(Node, Account, Nonce) ->
         key -> ensure_top_is_a_micro(Node, Account, Nonce + 1)
     end.
 
-siblings_common(TopBlock, N1, N2, Account1, Account2) ->
+siblings_common(TopBlock, N1, N2, Account1, Account2, Fraud) ->
     ct:pal("Waiting for sync"),
     %% Wait until the chain is synced.
     {ok, ExpectedTop1} = aec_blocks:hash_internal_representation(TopBlock),
+    0 = get_lock_holder_balance(N1),
+    0 = get_lock_holder_balance(N2),
     aec_test_utils:wait_for_it_or_timeout(
       fun() -> rpc:call(N2, aec_chain, top_block_hash, []) end,
       ExpectedTop1,
       10000),
 
     N1KeyBlocksCount = aec_blocks:height(TopBlock),
+    SpendFee = 17000,
 
     ct:pal("Starting to test fraud"),
     %% Add a transaction and create the first micro block
-    {ok, _Tx1} = add_spend_tx(N1, 1000, 17000,  1,  10, Account1, new_pubkey()),
+    {ok, _Tx1} = add_spend_tx(N1, 1000, SpendFee,  1,  10, Account1, new_pubkey()),
     {ok, Micro1, _} =  rpc:call(N1, aec_block_micro_candidate, create, [TopBlock]),
     {ok, Micro1S} = rpc:call(N1, aec_keys, sign_micro_block, [Micro1]),
 
     %% Add another transaction and create the second micro block
-    {ok, _Tx2} = add_spend_tx(N1, 1000, 17000,  2,  10, Account1, new_pubkey()),
+    {ok, _Tx2} = add_spend_tx(N1, 1000, SpendFee,  2,  10, Account1, new_pubkey()),
     {ok, Micro2, _} =  rpc:call(N1, aec_block_micro_candidate, create, [TopBlock]),
     {ok, Micro2S} = rpc:call(N1, aec_keys, sign_micro_block, [Micro2]),
 
@@ -181,7 +185,7 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     {ok, [Key2]} = aecore_suite_utils:mine_key_blocks(N2, 1),
 
     %% Now we need a micro block to report the fraud in.
-    {ok, _} = add_spend_tx(N2, 1000, 17000,  1,  10, Account2, new_pubkey()),
+    {ok, _} = add_spend_tx(N2, 1000, SpendFee,  1,  10, Account2, new_pubkey()),
     {ok, MicroFraud, _} = rpc:call(N2, aec_block_micro_candidate, create, [Key2]),
     {ok, MicroFraudS} = rpc:call(N2, aec_keys, sign_micro_block, [MicroFraud]),
 
@@ -198,7 +202,7 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     end,
 
     %% Manually add the report and check that the fraud would not be reported again.
-    {ok, _} = add_spend_tx(N2, 1000, 17000,  2,  10, Account2, new_pubkey()),
+    {ok, _} = add_spend_tx(N2, 1000, SpendFee,  2,  10, Account2, new_pubkey()),
     ok = rpc:call(N2, aec_conductor, post_block, [MicroFraudS]),
     {ok, MicroNoFraud, _} = rpc:call(N2, aec_block_micro_candidate, create, [MicroFraudS]),
     ?assertEqual(no_fraud, aec_blocks:pof(MicroNoFraud)),
@@ -241,7 +245,14 @@ siblings_common(TopBlock, N1, N2, Account1, Account2) ->
     Top2 = rpc:call(N2, aec_chain, top_block, []),
     aecore_suite_utils:wait_for_height(N1, aec_blocks:height(Top2)),
     Top1 = rpc:call(N1, aec_chain, top_block, []),
-    ?assertEqual(Top1, Top2).
+    ?assertEqual(Top1, Top2),
+
+    Locked = get_lock_holder_balance(N1),
+    {Locked, Locked} = {Locked, get_lock_holder_balance(N2)},
+    MR = aec_governance:block_mine_reward(FraudHeight),
+    true = Locked >= MR - FraudReward, % some fees
+    true = Locked =< MR.
+
 
 %% ============================================================
 %% Helpers
@@ -269,3 +280,10 @@ new_pubkey() ->
     #{ public := PubKey } = enacl:sign_keypair(),
     PubKey.
 
+get_lock_holder_balance(N) ->
+    HolderPubKey = aec_governance:locked_coins_holder_account(),
+    case rpc:call(N, aec_chain, get_account, [HolderPubKey]) of
+        {value, Acc} ->
+            _Bal = aec_accounts:balance(Acc);
+        none -> 0
+    end.