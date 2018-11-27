4fd0f23d52bc4663545d124b17e97366f6c234b9
Hans Svensson <hanssv@gmail.com>
2018-11-27 01:59:55 +0100

    Prepare release 1.0.0-rc6 + version and db path bump (#1853)

--- apps/aecore/include/blocks.hrl
+++ apps/aecore/include/blocks.hrl
@@ -1,6 +1,6 @@
 -include("pow.hrl").
 
--define(PROTOCOL_VERSION, 32).
+-define(PROTOCOL_VERSION, 1).
 -define(GENESIS_VERSION, ?PROTOCOL_VERSION).
 -define(GENESIS_HEIGHT, 0).
 -define(GENESIS_TIME, 0).
