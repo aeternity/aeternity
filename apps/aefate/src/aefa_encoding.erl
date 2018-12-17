7ef8949ac3dec201471d792208ccb732ca82190a
Erik Stenman <happi@happihacking.com>
2018-12-17 15:34:49 +0100

    Removed unfinished typerep code.

--- apps/aefate/src/aefa_encoding.erl
+++ apps/aefate/src/aefa_encoding.erl
@@ -26,9 +26,7 @@
 
 -export([ deserialize/1
         , serialize/1
-        , types_to_typereps/1
-        , value_to_typerep/1
-        , deserialize_typereps/2]).
+        ]).
 
 -include("aefa_data.hrl").
 
@@ -40,14 +38,14 @@
 -define(LONG_STRING  , 2#00000001). %% 0000000  RLP encoded array, size >= 64
 -define(SHORT_STRING ,       2#01). %% xxxxxx0  [bytes], 0 < size < 64
 %%                                           11  Set below
--define(SHORT_LIST   ,     2#0011). %% xxxx00   typerep + [encoded elements],  0 < length < 16
+-define(SHORT_LIST   ,     2#0011). %% xxxx00   [encoded elements],  0 < length < 16
 %%                                     000001   FREE
 %%                                     xxxx01   FREE
 -define(LONG_TUPLE   , 2#00001011). %% 000010   RLP encoded (size - 16) + [encoded elements],
 -define(SHORT_TUPLE  ,     2#1011). %% xxxx10   [encoded elements], 0  <  size < 16
 %%                                         1111 Set below
--define(LONG_LIST    , 2#00011111). %% 0001     typerep + RLP encoded (length - 16) + [Elements]
--define(MAP          , 2#00101111). %% 0010     RLP encoded size + typerep(A) + typerep(B) + [encoded key, encoded value]
+-define(LONG_LIST    , 2#00011111). %% 0001     RLP encoded (length - 16) + [Elements]
+-define(MAP          , 2#00101111). %% 0010     RLP encoded size + [encoded key, encoded value]
 -define(EMPTY_TUPLE  , 2#00111111). %% 0011
 %%                                  %% 0100     FREE
 -define(EMPTY_STRING , 2#01011111). %% 0101
@@ -139,110 +137,6 @@ serialize(?FATE_VARIANT(Tag, Values)) when 0 =< Tag
 %% -----------------------------------------------------
 
 
-value_to_typerep(?FATE_UNIT)                 -> <<0>>;
-value_to_typerep(?FATE_TRUE)                 -> <<2>>;
-value_to_typerep(?FATE_FALSE)                -> <<2>>;
-value_to_typerep(I) when ?IS_FATE_INTEGER(I) -> <<1>>;
-value_to_typerep(A) when ?IS_FATE_ADDRESS(A) -> <<3>>;
-value_to_typerep(S) when ?IS_FATE_STRING(S)  -> <<4>>;
-value_to_typerep(T) when ?IS_FATE_TUPLE(T)   ->
-    ?FATE_TUPLE(Tuple) = T,
-    S = size(Tuple),
-    Es = << <<(value_to_typerep(E))/binary>> || E <- tuple_to_list(Tuple) >>,
-    <<5, S, Es/binary>>;
-value_to_typerep(?FATE_NIL) -> <<6>>;
-value_to_typerep(L) when ?IS_FATE_LIST(L) ->
-    [E|_] = ?FATE_LIST_VALUE(L),
-    <<7, (value_to_typerep(E))/binary >>;
-value_to_typerep(M) when ?IS_FATE_MAP(M) ->
-    Map = ?FATE_MAP_VALUE(M),
-    case maps:to_list(Map) of
-        [{A,B}|_] ->
-            <<8,
-              (value_to_typerep(A))/binary,
-              (value_to_typerep(B))/binary>>;
-        [] -> <<10>>
-    end;
-value_to_typerep(?FATE_VARIANT(Tag, T)) when Tag < 256 ->
-    L = tuple_to_list(T),
-    Size = size(T),
-    <<9,
-      (value_to_typerep(?MAKE_FATE_INTEGER(Tag)))/binary,
-      (value_to_typerep(?MAKE_FATE_INTEGER(Size)))/binary,
-      (<< << (value_to_typerep(Type))/binary >> || Type <- L >>)/binary
-    >>.
-
-
-type_to_typerep(unit)        -> <<0>>;
-type_to_typerep(integer)     -> <<1>>;
-type_to_typerep(boolean)     -> <<2>>;
-type_to_typerep(address)     -> <<3>>;
-type_to_typerep(string)      -> <<4>>;
-type_to_typerep(empty_tuple) -> <<5, 0>>;
-type_to_typerep({tuple, T}) ->
-    S = size(T),
-    Es = << <<(type_to_typerep(E))/binary>> || E <- tuple_to_list(T) >>,
-    <<5, S, Es/binary>>;
-type_to_typerep(nil)         -> <<6>>;
-type_to_typerep({list, T})   -> <<7, (type_to_typerep(T))/binary >>;
-type_to_typerep(empty_map)   -> <<10>>;
-type_to_typerep({map, A, B}) -> <<8, (type_to_typerep(A))/binary, (type_to_typerep(B))/binary >>;
-type_to_typerep({variant, Cases}) ->
-    <<9, (length(Cases)),
-      <<
-        << (length(Types)),
-           << << (type_to_typerep(Type))/binary >> || Type <- Types >>
-        >>
-        || Types <- Cases >>
-    >>.
-
-
-deserialize_typerep(<<0, R/binary>>) -> {unit, R};
-deserialize_typerep(<<1, R/binary>>) -> {integer, R};
-deserialize_typerep(<<2, R/binary>>) -> {boolean, R};
-deserialize_typerep(<<3, R/binary>>) -> {address, R};
-deserialize_typerep(<<4, R/binary>>) -> {string, R};
-deserialize_typerep(<<5, 0, R/binary>>) -> {empty_tuple, R};
-deserialize_typerep(<<5, S, R/binary>>) ->
-    {List, Rest} = deserialize_typerep_elements(S, R),
-    {{tuple, list_to_tuple(List)}, Rest};
-deserialize_typerep(<<6, R/binary>>) -> {nil, R};
-deserialize_typerep(<<7, T/binary>>) ->
-    {Type, Rest} = deserialize_typerep(T),
-    {{list, Type}, Rest};
-deserialize_typerep(<<8, R/binary>>) ->
-    {A, R1} = deserialize_typerep(R),
-    {B, R2} = deserialize_typerep(R1),
-    {{map, A, B}, R2};
-deserialize_typerep(<<9, Rest/binary>>) ->
-    {_Tag, Rest1} = deserialize_typerep(Rest),
-    {?MAKE_FATE_INTEGER(Size), Rest2} = deserialize_typerep(Rest1),
-    {List, Rest3} = deserialize_type_parameters(Size, Rest2),
-    {{variant, [List]}, Rest3};
-deserialize_typerep(<<10, Rest/binary>>) ->
-    {empty_map, Rest}.
-
-deserialize_typerep_elements(0, Rest) ->
-    {[], Rest};
-deserialize_typerep_elements(N, Es) ->
-    {E, Rest} = deserialize_typerep(Es),
-    {Tail, Rest2} = deserialize_typerep_elements(N-1, Rest),
-    {[E|Tail], Rest2}.
-
-
-deserialize_type_parameters(0, Rest) ->
-    {[], Rest};
-deserialize_type_parameters(N, <<S,Es/binary>>) ->
-    {List, Rest} = deserialize_typerep_elements(S, Es),
-    {Tail, Rest2} = deserialize_type_parameters(N-1, Rest),
-    {[List|Tail], Rest2}.
-
-
-
-types_to_typereps(Types) ->
-    << <<(type_to_typerep(Type))/binary >> || Type <- Types >>.
-
-
 rlp_integer(S) when S >= 0 ->
     aeu_rlp:encode(binary:encode_unsigned(S)).
 
@@ -329,14 +223,6 @@ deserialize2(<<?VARIANT, Rest/binary>>) ->
             {?FATE_TUPLE(T), Rest2} = deserialize2(Rest1),
             {?FATE_VARIANT(Tag, T), Rest2}
     end.
-
-deserialize_typereps(0, Rest) ->
-    {[], Rest};
-deserialize_typereps(N, Rest) ->
-    {T, Rest2} = deserialize_typerep(Rest),
-    {List, Rest3} = deserialize_typereps(N-1, Rest2),
-    {[T|List], Rest3}.
-
 
 insert_kv([], M) -> M;
 insert_kv([K,V|R], M) -> insert_kv(R, maps:put(K, V, M)).
