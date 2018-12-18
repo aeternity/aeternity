ab63f24f88431ff8baddd41f108363189f2c0a17
Tino Breddin <t@tolbrino.com>
2018-12-18 17:43:18 +0100

    Add build-only appveyor config

--- scripts/windows/msys2_prepare.bat
+++ scripts/windows/msys2_prepare.bat
@@ -3,8 +3,8 @@
 @rem Required vars:
 @rem    ERTS_VERSION
 @rem    OTP_VERSION
-@rem    OTP_PATH
-@rem    MSYS2_ROOT
+@rem    WIN_OTP_PATH
+@rem    WIN_MSYS2_ROOT
 @rem    PLATFORM
 
 SETLOCAL ENABLEEXTENSIONS
@@ -12,34 +12,35 @@ SETLOCAL ENABLEEXTENSIONS
 rem Set required vars defaults
 IF "%ERTS_VERSION%"=="" SET "ERTS_VERSION=9.3"
 IF "%OTP_VERSION%"=="" SET "OTP_VERSION=20.3"
-IF "%OTP_PATH%"=="" SET "OTP_PATH=C:\Program Files\erl"
-IF "%MSYS2_ROOT%"=="" SET "MSYS2_ROOT=C:\msys64"
+IF "%WIN_OTP_PATH%"=="" SET "WIN_OTP_PATH=C:\Program Files\erl"
+IF "%WIN_MSYS2_ROOT%"=="" SET "WIN_MSYS2_ROOT=C:\msys64"
 IF "%PLATFORM%"=="" SET "PLATFORM=x64"
+SET BASH_BIN="%WIN_MSYS2_ROOT%\usr\bin\bash"
 
 @echo Current time: %time%
 rem Set the paths appropriately
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\\vcvarsall.bat" %PLATFORM%
 @echo on
-SET PATH=%MSYS2_ROOT%\mingw64\bin;%MSYS2_ROOT%\usr\bin;%PATH%
+SET PATH=%WIN_MSYS2_ROOT%\mingw64\bin;%WIN_MSYS2_ROOT%\usr\bin;%PATH%
 
 @echo Current time: %time%
 rem Upgrade the MSYS2 platform
 
-bash -lc "pacman --noconfirm --needed -Sy pacman"
-bash -lc "pacman --noconfirm -Su"
+%BASH_BIN% -lc "pacman --noconfirm --needed -Sy pacman"
+%BASH_BIN% -lc "pacman --noconfirm --needed -Su"
 
 @echo Current time: %time%
 rem Install required tools
 
-bash -lc "pacman --noconfirm --needed -S base-devel cmake curl gcc gcc-fortran git make patch"
-bash -lc "pacman --noconfirm --needed -S mingw-w64-x86_64-SDL mingw-w64-x86_64-libsodium mingw-w64-x86_64-ntldd-git mingw-w64-x86_64-yasm"
-bash -lc "pacman --noconfirm --needed -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-ada mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-gcc-objc"
+%BASH_BIN% -lc "pacman --noconfirm --needed -S base-devel cmake curl gcc gcc-fortran git make patch"
+%BASH_BIN% -lc "pacman --noconfirm --needed -S mingw-w64-x86_64-SDL mingw-w64-x86_64-libsodium mingw-w64-x86_64-ntldd-git mingw-w64-x86_64-yasm"
+%BASH_BIN% -lc "pacman --noconfirm --needed -S mingw-w64-x86_64-gcc mingw-w64-x86_64-gcc-ada mingw-w64-x86_64-gcc-fortran mingw-w64-x86_64-gcc-objc"
 
 @echo Current time: %time%
 rem Ensure Erlang/OTP %OTP_VERSION% is installed
 
-IF EXIST "%OTP_PATH%%ERTS_VERSION%\bin\" GOTO OTPINSTALLED
+IF EXIST "%WIN_OTP_PATH%%ERTS_VERSION%\bin\" GOTO OTPINSTALLED
 SET OTP_PACKAGE=otp_win64_%OTP_VERSION%.exe
 PowerShell -Command "Invoke-WebRequest http://erlang.org/download/%OTP_PACKAGE% -OutFile %TMP%\%OTP_PACKAGE%"
 START "" /WAIT "%TMP%\%OTP_PACKAGE%" /S
@@ -48,12 +49,12 @@ START "" /WAIT "%TMP%\%OTP_PACKAGE%" /S
 @echo Current time: %time%
 rem Set up msys2 env variables
 
-COPY msys2_env_build.sh %MSYS2_ROOT%\etc\profile.d\env_build.sh
+COPY %~dp0\msys2_env_build.sh %WIN_MSYS2_ROOT%\etc\profile.d\env_build.sh
 
 @echo Current time: %time%
 rem Remove link.exe from msys2, so it does not interfere with MSVC's link.exe
 
-bash -lc "rm -f /bin/link.exe /usr/bin/link.exe"
+%BASH_BIN% -lc "rm -f /bin/link.exe /usr/bin/link.exe"
 
 @echo Current time: %time%
 rem Finished preparation
