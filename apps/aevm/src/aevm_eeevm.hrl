589675b4a11576b65f21eb5d6e059369b75daa96
Hans Svensson <hans.svensson@quviq.com>
2018-12-30 20:18:41 +0100

    Ensure GasLeft is always an integer
    In some places we accidentally returned the whole state - avoiding this
    means less failing contract transactions.

--- apps/aevm/src/aevm_eeevm.hrl
+++ apps/aevm/src/aevm_eeevm.hrl
@@ -1,5 +1,7 @@
--define(aevm_eval_error(___What___, ___State___),
-        {aevm_eval_error, ___What___, ___State___}).
+-define(aevm_eval_error(___What___, ___GasLeft___),
+        {aevm_eval_error, ___What___, ___GasLeft___}).
+-define(aevm_eval_stack_error(___What___, ___State___),
+        {aevm_eval_stack_error, ___What___, ___State___}).
 
 -define(WORDSIZE, 256).
 -define(MASK256, ((1 bsl 256) -1)).
