49e9ee16fb441a8fc40af2029ae8343917ae5238
skkw <skkw@users.noreply.github.com>
2019-01-15 11:45:41 +0100

    Add ranch handler module for server side connections
    This module is the central part of the pool manager. It handles
    connections (sends, receives messages over socket) and chain events.

--- apps/aestratum/src/aestratum_handler.erl
+++ apps/aestratum/src/aestratum_handler.erl
@@ -3,9 +3,6 @@
 -behaviour(ranch_protocol).
 -behaviour(gen_server).
 
-%% TODO: eunit
-%% TODO: type spec
-
 %% API
 -export([start_link/4]).
 
@@ -62,8 +59,6 @@ handle_info({SocketError, _Socket, Rsn}, State) when ?IS_ERROR(SocketError) ->
     handle_socket_error(Rsn, State);
 handle_info(timeout, State) ->
     handle_socket_timeout(State);
-handle_info({chain, Event}, State) ->
-    handle_chain_event(Event, State);
 handle_info(_Info, State) ->
 	{stop, normal, State}.
 
@@ -94,10 +89,6 @@ handle_socket_timeout(#state{module = Mod, session = Session} = State) ->
     Res = Mod:handle_event({conn, timeout}, Session),
     result(Res, State).
 
-handle_chain_event(Event, #state{module = Mod, session = Session} = State) ->
-    Res = Mod:handle_event({chain, Event}, Session),
-    result(Res, State).
-
 result({send, Data, #state{module = Mod} = Session},
        #state{socket = Socket, transport = Transport} = State) ->
     case send_data(Data, Socket, Transport) of
@@ -117,4 +108,3 @@ is_stop(_Other) -> false.
 
 send_data(Data, Socket, Transport) ->
     Transport:send(Socket, Data).
-
