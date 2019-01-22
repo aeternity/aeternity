a8cfd7acf3d05559098f2dd1abd21f833ae7d5f3
Dincho Todorov <dincho@users.noreply.github.com>
2019-01-22 14:43:45 +0200

    Update deployment/deploy_api_docs.sh
    Co-Authored-By: valerifilipov <14542503+valerifilipov@users.noreply.github.com>

--- deployment/deploy_api_docs.sh
+++ deployment/deploy_api_docs.sh
@@ -14,6 +14,6 @@ if [ -z "$STATUS" ]; then
 else
     git config user.name "CircleCI"
     git config user.email "circleci@aeternity.com"
-    git commit -a -m "Update aeternity version: $CIRCLE_TAG docs CircleCI";
+    git commit -a -m "Update Aeternity node API docs to $CIRCLE_TAG";
     git push origin master
 fi
