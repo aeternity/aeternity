36342b58494c05a4e76242ec28bc5eb0e4b83346
zp-sd <zp.software.development@gmail.com>
2019-01-23 11:15:07 +0100

    Bump erlang-nat lib, add better error handling in case of a crash in the lib

--- apps/aecore/src/aec_upnp.erl
+++ apps/aecore/src/aec_upnp.erl
@@ -54,25 +54,26 @@ start_link() ->
 
 init(Args) ->
     epoch_sync:info("Starting UPnP/NAT-PMP service"),
+    process_flag(trap_exit, true),
     erlang:send_after(rand:uniform(1000), self(), add_port_mapping),
     {ok, Args}.
 
 handle_call(Request, _From, State) ->
-    epoch_sync:error("Received unknown request: ~p", [Request]),
+    epoch_sync:warning("Received unknown request: ~p", [Request]),
     {reply, ok, State}.
 
 handle_cast(Other, State) ->
-    epoch_sync:error("Received unknown cast: ~p", [Other]),
+    epoch_sync:warning("Received unknown cast: ~p", [Other]),
     {noreply, State}.
 
 handle_info(add_port_mapping, State) ->
-    ok = add_port_mapping(),
+    proc_lib:spawn(fun() -> add_port_mapping() end),
     %% Give additional 10 secs for UPnP/NAT-PMP discovery and setup, to
     %% make sure there is continuity in port mapping.
     erlang:send_after(1000 * (?MAPPING_LIFETIME - 10), self(), add_port_mapping),
     {noreply, State};
 handle_info(Other, State) ->
-    epoch_sync:error("Received unknown info message: ~p", [Other]),
+    epoch_sync:warning("Received unknown info message: ~p", [Other]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
@@ -87,31 +88,43 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 
 add_port_mapping() ->
-    case nat:discover() of
-        {ok, Ctx} ->
-            InternalPort = aec_connection_sup:sync_port(),
-            ExternalPort = aec_connection_sup:ext_sync_port(),
-            case nat:add_port_mapping(Ctx, tcp, InternalPort, ExternalPort, ?MAPPING_LIFETIME) of
-                {ok, _, _, _, _} ->
-                    epoch_sync:info("UPnP/NAT-PMP mapping between ~p and ~p added", [InternalPort, ExternalPort]);
-                {error, _Reason} = Error ->
-                    epoch_sync:warning("Adding UPnP/NAT-PMP mapping between ~p and ~p failed: ~p", [InternalPort, ExternalPort, Error])
-            end;
-        no_nat ->
-            epoch_sync:warning("UPnP/NAT-PMP discovery failed")
+    try
+        case nat:discover() of
+            {ok, Ctx} ->
+                epoch_sync:info("UPnP/NAT-PMP discovery successful: ~p", [Ctx]),
+                InternalPort = aec_connection_sup:sync_port(),
+                ExternalPort = aec_connection_sup:ext_sync_port(),
+                case nat:add_port_mapping(Ctx, tcp, InternalPort, ExternalPort, ?MAPPING_LIFETIME) of
+                    {ok, _, _, _, _} ->
+                        epoch_sync:info("UPnP/NAT-PMP mapping between ~p and ~p added", [InternalPort, ExternalPort]);
+                    {error, _Reason} = Error ->
+                        epoch_sync:warning("Adding UPnP/NAT-PMP mapping between ~p and ~p failed: ~p", [InternalPort, ExternalPort, Error])
+                end;
+            no_nat ->
+                epoch_sync:warning("UPnP/NAT-PMP discovery failed")
+        end
+    catch
+        X:Y ->
+            epoch_sync:warning("UPnP NAT-PMP library failed: ~p:~p", [X, Y])
     end.
 
 delete_port_mapping() ->
-    case nat:discover() of
-        {ok, Ctx} ->
-            InternalPort = aec_connection_sup:sync_port(),
-            ExternalPort = aec_connection_sup:ext_sync_port(),
-            case nat:delete_port_mapping(Ctx, tcp, InternalPort, ExternalPort) of
-                ok ->
-                    epoch_sync:info("UPnP/NAT-PMP mapping between ~p and ~p removed", [InternalPort, ExternalPort]);
-                {error, _Reason} = Error ->
-                    epoch_sync:warning("Removal of UPnP/NAT-PMP mapping between ~p and ~p failed: ~p", [InternalPort, ExternalPort, Error])
-            end;
-        no_nat ->
-            epoch_sync:warning("UPnP/NAT-PMP discovery failed")
+    try
+        case nat:discover() of
+            {ok, Ctx} ->
+                epoch_sync:debug("UPnP/NAT-PMP discovery successful: ~p", [Ctx]),
+                InternalPort = aec_connection_sup:sync_port(),
+                ExternalPort = aec_connection_sup:ext_sync_port(),
+                case nat:delete_port_mapping(Ctx, tcp, InternalPort, ExternalPort) of
+                    ok ->
+                        epoch_sync:info("UPnP/NAT-PMP mapping between ~p and ~p removed", [InternalPort, ExternalPort]);
+                    {error, _Reason} = Error ->
+                        epoch_sync:warning("Removal of UPnP/NAT-PMP mapping between ~p and ~p failed: ~p", [InternalPort, ExternalPort, Error])
+                end;
+            no_nat ->
+                epoch_sync:warning("UPnP/NAT-PMP discovery failed")
+        end
+    catch
+        X:Y ->
+            epoch_sync:warning("UPnP NAT-PMP library failed: ~p:~p", [X, Y])
     end.
