c104ff33034ff5aa4338687ecd5ca2df9922b39e
Juraj Hlista <jurajhlista@gmail.com>
2019-02-01 11:25:34 +0100

    Remove unused function

--- apps/aecore/src/aec_mining.erl
+++ apps/aecore/src/aec_mining.erl
@@ -6,24 +6,8 @@
 
 -export([mine/5]).
 
--ifdef(TEST).
--export([get_miner_account_balance/0]).
--endif.
-
 -spec mine(binary(), aec_pow:sci_int(), aec_pow:nonce(),
            aec_pow:miner_config(), aec_pow:miner_instance() | 'undefined') ->  aec_pow:pow_result().
 mine(HeaderBin, Target, Nonce, MinerConfig, MinerInstance) ->
     aec_pow_cuckoo:generate(HeaderBin, Target, Nonce, MinerConfig, MinerInstance).
 
--ifdef(TEST).
--spec get_miner_account_balance() -> {ok, non_neg_integer()} |
-                                     {error, account_not_found}.
-get_miner_account_balance() ->
-    {ok, Pubkey} = aec_keys:pubkey(),
-    case aec_chain:get_account(Pubkey) of
-        {value, A} ->
-            {ok, aec_accounts:balance(A)};
-        none ->
-            {error, account_not_found}
-    end.
--endif.
