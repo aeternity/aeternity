e905a37469fede317b9687ec8b016b9eee72286d
Juraj Hlista <jurajhlista@gmail.com>
2019-02-01 11:44:43 +0100

    Move check_env/0 from aec_pow_cuckoo to aec_mining

--- apps/aecore/src/aec_mining.erl
+++ apps/aecore/src/aec_mining.erl
@@ -4,7 +4,27 @@
 
 -module(aec_mining).
 
--export([mine/5]).
+-export([check_env/0,
+         mine/5]).
+
+%%------------------------------------------------------------------------------
+%% Assert that configuration options 'mining > cuckoo > miners' and
+%% 'mining > cuckoo > edge_bits' are not used together with deprecated
+%% configuration property 'mining > cuckoo > miner'.
+%%------------------------------------------------------------------------------
+check_env() ->
+    case {aeu_env:user_map([<<"mining">>, <<"cuckoo">>, <<"miners">>]),
+          aeu_env:user_config([<<"mining">>, <<"cuckoo">>, <<"edge_bits">>])} of
+        {undefined, undefined} -> ok;
+        {_, _} ->
+            case aeu_env:user_config([<<"mining">>, <<"cuckoo">>, <<"miner">>]) of
+                undefined -> ok;
+                _ ->
+                    lager:error("Config error: deprecated property 'mining > cuckoo > miner' cannot be used "
+                                "together with 'mining > cuckoo > miners' or 'mining > cuckoo > edge_bits'"),
+                    exit(cuckoo_config_validation_failed)
+            end
+    end.
 
 -spec mine(binary(), aec_pow:sci_int(), aec_pow:nonce(),
            aec_pow:miner_config(), aec_pow:miner_instance() | 'undefined') ->  aec_pow:pow_result().
--- apps/aecore/src/aec_pow_cuckoo.erl
+++ apps/aecore/src/aec_pow_cuckoo.erl
@@ -18,8 +18,7 @@
 -behaviour(aec_pow).
 
 
--export([check_env/0,
-         generate/5,
+-export([generate/5,
          get_addressed_instances/1,
          get_miner_configs/0,
          get_repeats/1,
@@ -72,25 +71,6 @@
 %%%=============================================================================
 %%% API
 %%%=============================================================================
-
-%%------------------------------------------------------------------------------
-%% Assert that configuration options 'mining > cuckoo > miners' and
-%% 'mining > cuckoo > edge_bits' are not used together with deprecated
-%% configuration property 'mining > cuckoo > miner'.
-%%------------------------------------------------------------------------------
-check_env() ->
-    case {aeu_env:user_map([<<"mining">>, <<"cuckoo">>, <<"miners">>]),
-          aeu_env:user_config([<<"mining">>, <<"cuckoo">>, <<"edge_bits">>])} of
-        {undefined, undefined} -> ok;
-        {_, _} ->
-            case aeu_env:user_config([<<"mining">>, <<"cuckoo">>, <<"miner">>]) of
-                undefined -> ok;
-                _ ->
-                    lager:error("Config error: deprecated property 'mining > cuckoo > miner' cannot be used "
-                                "together with 'mining > cuckoo > miners' or 'mining > cuckoo > edge_bits'"),
-                    exit(cuckoo_config_validation_failed)
-            end
-    end.
 
 %%------------------------------------------------------------------------------
 %% Proof of Work generation with default settings
