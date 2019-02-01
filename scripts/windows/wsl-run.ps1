59855eb891c6fc7110d57501bef63b02dc9f1143
Luca Favatella <lucafavatella@users.noreply.github.com>
2019-02-01 13:31:01 +0000

    Amend scripts re software renaming (#2107)
    * Descriptions in install scripts messages.
    * Links in install scripts messages.
    * Links in scripts fetching package.
    
    The usage of `bin/epoch` in scripts `install.sh` and
    `windows/wsl-run.ps1` is kept so to enable scripts to be still valid
    for old releases.

--- scripts/windows/wsl-run.ps1
+++ scripts/windows/wsl-run.ps1
@@ -1,4 +1,4 @@
-echo "This script will run epoch synchronously inside Ubuntu"
+echo "This script will run the Aeternity node synchronously inside Ubuntu"
 Pause
 
 bash -c "cd ~/aeternity/node && ./bin/aeternity console"
