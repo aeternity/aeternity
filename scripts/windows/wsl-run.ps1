e2ecc0af291d0e3ac92dd16eb884c92cdc45b086
Luca Favatella <lucafavatella@users.noreply.github.com>
2019-02-01 11:27:21 +0000

    Prefer non-deprecated operational script in Win
    Win package was introduced recently as experimental
    so using new ops binary shall not impact user experience.

--- scripts/windows/wsl-run.ps1
+++ scripts/windows/wsl-run.ps1
@@ -1,6 +1,6 @@
 echo "This script will run epoch synchronously inside Ubuntu"
 Pause
 
-bash -c "cd ~/aeternity/node && ./bin/epoch console"
+bash -c "cd ~/aeternity/node && ./bin/aeternity console"
 
 Pause
