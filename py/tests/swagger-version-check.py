618886cd2830749066efd7262581802682e877c9
Luca Favatella <luca.favatella@erlang-solutions.com>
2019-02-01 14:36:12 +0000

    Rename variable in script
    Addresses https://github.com/aeternity/aeternity/pull/2108/files#r253024652

--- py/tests/swagger-version-check.py
+++ py/tests/swagger-version-check.py
@@ -10,16 +10,16 @@ parser.add_argument('yaml_file', type=open)
 parser.add_argument('json_file', type=open)
 args = parser.parse_args()
 
-epoch_version = args.version_file.read().strip()
+node_version = args.version_file.read().strip()
 yaml_version = yaml.safe_load(args.yaml_file)['info']['version']
 json_version = json.load(args.json_file)['info']['version']
 
-print("Node version: %s" % epoch_version)
+print("Node version: %s" % node_version)
 print("Swagger yaml version: %s" % yaml_version)
 print("Swagger json version: %s" % json_version)
 
-if yaml_version != epoch_version:
+if yaml_version != node_version:
     sys.exit(1)
 
-if json_version != epoch_version:
+if json_version != node_version:
     sys.exit(1)
