e89a694e801cfa31c01d1f7fa9f509acf2970e17
zp-sd <zp.software.development@gmail.com>
2019-02-08 10:14:34 +0100

    WIP

---
+++ apps/aeutils/priv/extensions/rename_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Wrapper to run rename_db
+
+PATH=$BINDIR:$PATH
+export ERL_LIBS=$ROOTDIR/lib
+APPS_VSN=${REL_VSN:?}
+"$ERTS_DIR/bin/escript" "$ROOTDIR/lib/aeutils-${APPS_VSN:?}/priv/rename_db" "$*"
+
+exit $?
