69805fb76d146bfdf2bd337f656ab25c5e528f68
zp-sd <zp.software.development@gmail.com>
2019-02-08 13:59:45 +0100

    Refactor node rename script

--- apps/aeutils/src/aeu_db.erl
+++ apps/aeutils/src/aeu_db.erl
@@ -11,7 +11,6 @@
 
 %% TODO: Backup SCHEMA.DAT before starting dets manipulations!
 %% TODO: Maybe parametrize FROM_NAME & TO_NAME.
-%% This may be easier achieved with dets:select/3 instead of dets:traverse/2?
 -define(FROM_NAME, 'epoch@localhost').
 -define(TO_NAME, 'aeternity@localhost').
 -define(db_renaming_error(E), {db_renaming_error, E}).
@@ -32,10 +31,11 @@ change_node(SchemaDATFilePath) ->
 %%%===================================================================
 
 do_change_node(SchemaDatFile) ->
-    ok  = assert_file_present(SchemaDatFile),
-    Ref = dets_open_file(SchemaDatFile),
-    ok  = dets_change_node(Ref),
-    ok  = dets_close_file(Ref).
+    ok   = assert_file_present(SchemaDatFile),
+    Name = dets_open_file(SchemaDatFile),
+    ok   = dets_change_node(Name),
+    ok   = dets_sync(Name),
+    ok   = dets_close(Name).
 
 assert_file_present(SchemaDatFile) ->
     case filelib:is_regular(SchemaDatFile) of
@@ -46,48 +46,61 @@ assert_file_present(SchemaDatFile) ->
     end.
 
 dets_open_file(SchemaDatFile) ->
-    case dets:open_file(SchemaDatFile) of
-        {ok, Ref} -> Ref;
+    case dets:open_file(schema, [{file, SchemaDatFile}, {repair, false}, {keypos, 2}]) of
+        {ok, Name} -> Name;
         {error, Reason} ->
             io:fwrite("Opening SCHEMA.DAT file failed: ~p~n", [Reason]),
             db_renaming_error(Reason)
     end.
 
-dets_change_node(Ref) ->
-    case dets:traverse(Ref, fun rename_node/1) of
-        {error, Reason} ->
-            io:fwrite("Traversing SCHEMA.DAT file failed: ~p~n", [Reason]),
-            db_renaming_error(Reason);
-        UpdatedSchemaEntries ->
-            case dets:insert(Ref, UpdatedSchemaEntries) of
-                ok -> ok;
+dets_change_node(Name) ->
+    dets_change_node(Name, dets:first(Name)).
+
+dets_change_node(_Name, '$end_of_table') ->
+    ok;
+dets_change_node(Name, Key) ->
+    case dets:lookup(Name, Key) of
+        [{Name, Tab, Def}] ->
+            NewDef = rename_node(Def),
+            case dets:insert(Name, {Name, Tab, NewDef}) of
+                ok ->
+                    dets_change_node(Name, dets:next(Name, Key));
                 {error, Reason} ->
-                    io:fwrite("Inserting entries into SCHEMA.DAT failed: ~p~n", [Reason]),
+                    io:fwrite("Inserting into SCHEMA.DAT failed: ~p~n", [Reason]),
                     db_renaming_error(Reason)
-            end
+            end;
+        {error, Reason} ->
+            io:fwrite("Key ~p lookup in SCHEMA.DAT file failed: ~p", [Key, Reason]),
+            db_renaming_error(Reason)
     end.
 
-rename_node({schema, Name, Data}) ->
+rename_node(Def) ->
     ToBeRenamed =
         lists:filter(
           fun({_Backend, [?FROM_NAME]}) -> true;
              ({cookie, {_, ?FROM_NAME}}) -> true;
              ({version, {_, {?FROM_NAME, _}}}) -> true;
              (_Other) -> false
-          end, Data),
-    NewData =
-        lists:foldl(
-          fun({Backend, [?FROM_NAME]}, Acc) ->
-                  lists:keyreplace(Backend, 1, Acc, {Backend, [?TO_NAME]});
-             ({cookie, {A, ?FROM_NAME}}, Acc) ->
-                  lists:keyreplace(cookie, 1, Acc, {cookie, {A, ?TO_NAME}});
-             ({version, {Vsn, {?FROM_NAME, Ts}}}, Acc) ->
-                  lists:keyreplace(version, 1, Acc, {version, {Vsn, {?TO_NAME, Ts}}})
-          end, Data, ToBeRenamed),
-    {continue, {schema, Name, NewData}}.
+          end, Def),
+    lists:foldl(
+      fun({Backend, [?FROM_NAME]}, Acc) ->
+              lists:keyreplace(Backend, 1, Acc, {Backend, [?TO_NAME]});
+         ({cookie, {A, ?FROM_NAME}}, Acc) ->
+              lists:keyreplace(cookie, 1, Acc, {cookie, {A, ?TO_NAME}});
+         ({version, {Vsn, {?FROM_NAME, Ts}}}, Acc) ->
+              lists:keyreplace(version, 1, Acc, {version, {Vsn, {?TO_NAME, Ts}}})
+      end, Def, ToBeRenamed).
 
-dets_close_file(Ref) ->
-    case dets:close(Ref) of
+dets_sync(Name) ->
+    case dets:sync(Name) of
+        ok -> ok;
+        {error, Reason} ->
+            io:fwrite("Syncing SCHEMA.DAT file failed: ~p~n", [Reason]),
+            db_renaming_error(Reason)
+    end.
+
+dets_close(Name) ->
+    case dets:close(Name) of
         ok -> ok;
         {error, Reason} ->
             io:fwrite("Closing SCHEMA.DAT file failed: ~p~n", [Reason]),
