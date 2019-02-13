51ecd4942b00f9b10d2652e23bd8716fc3a10cd9
Tino Breddin <t@tolbrino.com>
2019-02-13 10:46:42 +0100

    Fix keys_gen handling of given password

--- apps/aeutils/priv/extensions/keys_gen.sh
+++ apps/aeutils/priv/extensions/keys_gen.sh
@@ -6,6 +6,6 @@
 PATH=$BINDIR:$PATH
 export ERL_LIBS=$ROOTDIR/lib
 APPS_VSN=${REL_VSN:?}
-"$ERTS_DIR/bin/escript" "$ROOTDIR/lib/aeutils-${APPS_VSN:?}/priv/keys_gen" "$*"
+"$ERTS_DIR/bin/escript" "$ROOTDIR/lib/aeutils-${APPS_VSN:?}/priv/keys_gen" "$@"
 
 exit $?
