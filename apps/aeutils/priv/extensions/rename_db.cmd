574ecc3dd11193afc24cdcda60d6f87e44fa6552
zp-sd <zp.software.development@gmail.com>
2019-02-22 11:01:18 +0100

    Adapt for Windows

---
+++ apps/aeutils/priv/extensions/rename_db.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+:: Wrapper to run rename_db
+
+set "PATH=%BINDIR%;%PATH%"
+set "ERL_LIBS=%ROOTDIR%\lib"
+set "APPS_VSN=%REL_VSN%"
+
+"%ERTS_DIR%\bin\escript" "%ROOTDIR%\lib\aeutils-%APPS_VSN%\priv\rename_db" %*
