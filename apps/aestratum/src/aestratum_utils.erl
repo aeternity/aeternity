9bba7331063dc411e1c9549770beb5630b3fa79b
skkw <skkw@users.noreply.github.com>
2019-03-01 11:52:29 +0100

    removed code which now lives in aestratum_lib repo

--- apps/aestratum/src/aestratum_utils.erl
+++
@@ -1,8 +0,0 @@
--module(aestratum_utils).
-
--export([timestamp/0]).
-
-%% Timestamp in milliseconds.
-timestamp() ->
-    {MegaSecs, Secs, MicroSecs} = os:timestamp(),
-    (MegaSecs * 1000000 + Secs) * 1000 + erlang:trunc(MicroSecs / 1000).
