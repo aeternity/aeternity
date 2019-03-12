a17ce4dc5468b12a8013a8f0d8b2c67203f63254
skkw <skkw@users.noreply.github.com>
2019-03-12 09:17:20 +0100

    configuration for stratum

--- apps/aestratum/src/aestratum_node_sup.erl
+++ apps/aestratum/src/aestratum_node_sup.erl
@@ -17,18 +17,24 @@ start_link() ->
 %%% Supervisor callbacks
 %%%===================================================================
 init([]) ->
-
-    %% TODO: read N rounds back from config file
-    LastN = 2,
-
-    %% TODO: read beneficiaries from config file
-    Beneficiaries = #{<<"benef_1">> => 1,
-                      <<"benef_2">> => 2,
-                      <<"benef_4">> => 4,
-                      <<"benef_8">> => 8},
-
+    {ok, Conf} = aeu_env:user_config(<<"stratum">>),
+    #{<<"beneficiaries">> := Benefs,
+      <<"reward_last_rounds">> := LastN} = maps:from_list(Conf),
+    Beneficiaries =
+        lists:foldl(fun (Benef, M) ->
+                            [Addr, ShareBin] = binary:split(Benef, <<":">>),
+                            Share = binary_to_number(ShareBin),
+                            M#{Addr => maps:get(Addr, M, 0) + Share}
+                    end, #{}, Benefs),
     Childs = [{aestratum_chain, {aestratum_chain, start_link, []},
                permanent, 5000, worker, [aestratum_chain]},
               {aestratum_reward, {aestratum_reward, start_link, [LastN, Beneficiaries]},
                permanent, 5000, worker, [aestratum_reward]}],
     {ok, {{rest_for_one, 5, 10}, Childs}}.
+
+
+binary_to_number(B) ->
+    case catch binary_to_integer(B) of
+        {'EXIT', {badarg, _}} -> binary_to_float(B);
+        I -> I
+    end.
