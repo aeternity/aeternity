6a9256ba88bf02a7416e05537139f7da09714c59
Luca Favatella <luca.favatella@erlang-solutions.com>
2019-03-13 11:36:08 +0000

    Rename test case for consistency with other ones

--- system_test/common/aest_db_SUITE.erl
+++ system_test/common/aest_db_SUITE.erl
@@ -14,7 +14,7 @@
          node_can_reuse_db_of_other_node/1,
          roma_node_can_reuse_db_of_other_roma_node/1,
          minerva_node_with_epoch_db_can_reuse_db_of_roma_node/1,
-         minerva_node_can_reuse_db_of_roma_node/1
+         node_can_reuse_db_of_roma_node/1
         ]).
 
 %=== INCLUDES ==================================================================
@@ -33,7 +33,7 @@ all() -> [
           node_can_reuse_db_of_other_node,
           roma_node_can_reuse_db_of_other_roma_node,
           minerva_node_with_epoch_db_can_reuse_db_of_roma_node,
-          minerva_node_can_reuse_db_of_roma_node
+          node_can_reuse_db_of_roma_node
          ].
 
 init_per_suite(Config) ->
@@ -58,7 +58,7 @@ roma_node_can_reuse_db_of_other_roma_node(Cfg) ->
 minerva_node_with_epoch_db_can_reuse_db_of_roma_node(Cfg) ->
     node_can_reuse_db_of_other_node_(fun roma_node_spec/2, fun minerva_with_epoch_name_in_db_spec/2, Cfg).
 
-minerva_node_can_reuse_db_of_roma_node(Cfg) ->
+node_can_reuse_db_of_roma_node(Cfg) ->
     node_can_reuse_db_of_other_node_(fun roma_node_spec/2, fun node_spec/2,
                                      [{rename_db_fun, fun run_rename_db_script/2} | Cfg]).
 
