500667bd115223f871edf5ac4e802e070f5a9e94
skkw <skkw@users.noreply.github.com>
2019-04-04 12:32:28 +0200

    payout out of the rewards using contract

--- apps/aestratum/src/aestratum_node_sup.erl
+++ apps/aestratum/src/aestratum_node_sup.erl
@@ -18,17 +18,24 @@ start_link() ->
 %%%===================================================================
 init([]) ->
     {ok, Conf} = aeu_env:user_config(<<"stratum">>),
-    #{<<"beneficiaries">> := Benefs,
-      <<"reward_last_rounds">> := LastN} = maps:from_list(Conf),
-    Beneficiaries =
-        lists:foldl(fun (Benef, M) ->
-                            [Addr, ShareBin] = binary:split(Benef, <<":">>),
-                            Share = binary_to_number(ShareBin),
-                            M#{Addr => maps:get(Addr, M, 0) + Share}
-                    end, #{}, Benefs),
-    Childs = [{aestratum_chain, {aestratum_chain, start_link, []},
+    #{<<"beneficiaries">> := BenefAddrPctShares,
+      <<"reward_last_rounds">> := LastN,
+      <<"keys">> := [{<<"dir">>, Dir}]} = maps:from_list(Conf),
+    ReadKey = fun (F) -> file:read_file(filename:join([code:priv_dir(aestratum), Dir, F])) end,
+    {ok, PubKey}  = ReadKey("sign_key.pub"),
+    {ok, PrivKey} = ReadKey("sign_key"),
+    Benefs = lists:foldl(fun (Benef, Acc) ->
+                                 [Address, PctShare0] = binary:split(Benef, <<":">>),
+                                 {account_pubkey, BenefPK} = aehttp_api_encoder:decode(Address),
+                                 PctShare = binary_to_number(PctShare0),
+                                 true = PctShare > 0,
+                                 Acc#{BenefPK => maps:get(BenefPK, Acc, 0) + PctShare}
+                         end, #{}, BenefAddrPctShares),
+    SumPcts = lists:sum(maps:values(Benefs)),
+    SumPcts < 100 orelse error("sum of beneficiaries' percent shares is too high"),
+    Childs = [{aestratum_chain, {aestratum_chain, start_link, [SumPcts, {PubKey, PrivKey}]},
                permanent, 5000, worker, [aestratum_chain]},
-              {aestratum_reward, {aestratum_reward, start_link, [LastN, Beneficiaries]},
+              {aestratum_reward, {aestratum_reward, start_link, [LastN, {SumPcts, Benefs}]},
                permanent, 5000, worker, [aestratum_reward]}],
     {ok, {{rest_for_one, 5, 10}, Childs}}.
 
