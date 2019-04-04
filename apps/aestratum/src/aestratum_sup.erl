2229c979eaf268dc6212b2d48da285a62f4e3081
skkw <skkw@users.noreply.github.com>
2019-04-04 12:58:53 +0200

    fixed startup of stratum

--- apps/aestratum/src/aestratum_sup.erl
+++ apps/aestratum/src/aestratum_sup.erl
@@ -16,37 +16,17 @@ start_link(Config) ->
 %% supervisor callbacks.
 
 init(Config) ->
-    Procs =
-        [aestratum_chain(Config),
-         aestratum_reward(Config),
-         aestratum_extra_nonce_cache(Config),
-         aestratum_user_register(Config),
-         ranch_listener(Config)],
-    {ok, {{one_for_all, 1, 5}, Procs}}.
+    Childs = [aestratum_extra_nonce_cache(Config),
+              aestratum_user_register(Config),
+              ranch_listener(Config),
+              aestratum_node_sup(Config)],
+    {ok, {{one_for_all, 1, 5}, Childs}}.
 
 %% Internal functions.
 
-aestratum_chain(_Config) ->
-    {aestratum_chain,
-     {aestratum_chain, start_link, []},
-     permanent, 5000, worker, [aestratum_chain]}.
-
-aestratum_reward(_Config) ->
-    %% All the config should be read in aestratum_app, converted to a map
-    %% and this map should be passed to aestratum_sup. aeu_env won't be
-    %% used anywhere apart from aestratum_config/aestratum_app.
-    {ok, Conf} = aeu_env:user_config(<<"stratum">>),
-    #{<<"beneficiaries">> := Benefs,
-      <<"reward_last_rounds">> := LastN} = maps:from_list(Conf),
-    Beneficiaries =
-        lists:foldl(fun (Benef, M) ->
-                            [Addr, ShareBin] = binary:split(Benef, <<":">>),
-                            Share = binary_to_number(ShareBin),
-                            M#{Addr => maps:get(Addr, M, 0) + Share}
-                    end, #{}, Benefs),
-    {aestratum_reward,
-     {aestratum_reward, start_link, [LastN, Beneficiaries]},
-     permanent, 5000, worker, [aestratum_reward]}.
+aestratum_node_sup(Config) ->
+    {aestratum_node_sup, {aestratum_node_sup, start_link, [Config]},
+     permanent, 5000, supervisor, [aestratum_node_sup]}.
 
 aestratum_extra_nonce_cache(_Config) ->
     {aestratum_extra_nonce_cache,
@@ -58,15 +38,14 @@ aestratum_user_register(_Config) ->
      {aestratum_user_register, start_link, []},
      permanent, 5000, worker, [aestratum_user_register]}.
 
-ranch_listener(_Config) ->
-    {ok, Conf} = aeu_env:user_config(<<"stratum">>),
+ranch_listener(Config) ->
     %% TODO: ip - interface to listen on (all by default)
     %% %% The maximum number of connections is a soft limit. In practice, it can
     %% reach max_connections + the number of acceptors.
     #{<<"transport">> := Transport,
       <<"max_connections">> := MaxConnections,
       <<"num_acceptors">> := NumAcceptors,
-      <<"port">> := Port} = maps:from_list(Conf),
+      <<"port">> := Port} = maps:from_list(Config),
     TransportOpts = [{max_connections, MaxConnections},
                      {num_acceptors, NumAcceptors},
                      {port, Port}],
@@ -79,10 +58,3 @@ ranch_listener(_Config) ->
 
 ranch_mod(<<"tcp">>) -> ranch_tcp;
 ranch_mod(<<"ssl">>) -> ranch_ssl.
-
-binary_to_number(B) ->
-    case catch binary_to_integer(B) of
-        {'EXIT', {badarg, _}} -> binary_to_float(B);
-        I -> I
-    end.
-
