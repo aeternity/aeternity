6917e624ed6f9a8eb33a7371815c4dbadf1322b9
skkw <skkw@users.noreply.github.com>
2019-04-04 12:58:53 +0200

    fixed startup of stratum

--- apps/aestratum/src/aestratum_app.erl
+++ apps/aestratum/src/aestratum_app.erl
@@ -6,7 +6,8 @@
          stop/1]).
 
 start(_Type, _Args) ->
-    aestratum_sup:start_link(#{}).
+    {ok, Config} = aeu_env:user_config(<<"stratum">>),
+    aestratum_sup:start_link(Config).
 
 stop(_State) ->
 	ok.
