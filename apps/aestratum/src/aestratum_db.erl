eecade100a177e6e67bb7037173fde5bdeab858a
skkw <skkw@users.noreply.github.com>
2019-04-08 16:27:55 +0200

    moved database functionality to separate file

---
+++ apps/aestratum/src/aestratum_db.erl
@@ -0,0 +1,161 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2019,
+%%% @doc
+%%%
+%%% Aeternity stratum DB tables
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aestratum_db).
+
+-export([check_tables/1,
+         create_tables/1]).
+
+-export([sort_key/0,
+         is_empty/1,
+         get_hash/1,
+         store_share/3,
+         store_round/0,
+         store_reward/6,
+         shares_range/2,
+         shares_selector/2,
+         shares_slices/1,
+         delete_reward_records/1,
+         delete_payment_records/1]).
+
+-include("aestratum.hrl").
+-include_lib("aecore/include/blocks.hrl").
+-include_lib("stdlib/include/ms_transform.hrl").
+
+-define(SHARES_BATCH_LENGTH, 1000).
+
+-type sort_key() :: non_neg_integer().
+-type amount() :: non_neg_integer().
+
+-export_type([sort_key/0,
+              amount/0]).
+
+%%%===================================================================
+
+-define(TAB_DEF(Name, Type, Mode),
+        {Name, [aec_db:tab_copies(Mode),
+                {type, Type},
+                {record_name, Name},
+                {attributes, record_info(fields, Name)},
+                {user_properties, [{vsn, table_vsn(Name)}]}]}).
+
+create_tables(Mode) ->
+    Specs = [table_specs(Tab, Mode) || {missing_table, Tab} <- check_tables()],
+    [{atomic, ok} = mnesia:create_table(Tab, Spec) || {Tab, Spec} <- Specs].
+
+check_tables(Acc) ->
+    check_tables() ++ Acc.
+
+check_tables() ->
+    lists:foldl(fun ({T, S}, Acc) -> aec_db:check_table(T, S, Acc) end, [], tables_specs(disc)).
+
+table_specs(?HASHES_TAB, Mode) -> ?TAB_DEF(?HASHES_TAB, set, Mode);
+table_specs(?SHARES_TAB, Mode) -> ?TAB_DEF(?SHARES_TAB, ordered_set, Mode);
+table_specs(?ROUNDS_TAB, Mode) -> ?TAB_DEF(?ROUNDS_TAB, ordered_set, Mode);
+table_specs(?REWARDS_TAB, Mode) -> ?TAB_DEF(?REWARDS_TAB, ordered_set, Mode);
+table_specs(?PAYMENTS_TAB, Mode) -> ?TAB_DEF(?PAYMENTS_TAB, ordered_set, Mode).
+
+tables_specs(Mode) -> [table_specs(Tab, Mode) || Tab <- ?TABS].
+
+table_vsn(_) -> 1.
+
+%%%===================================================================
+
+-spec sort_key() -> sort_key().
+sort_key() ->
+    abs(erlang:unique_integer([monotonic])).
+
+-spec store_share(miner_pubkey(), pos_integer(), binary()) ->
+                         {ok, #aestratum_share{}, #aestratum_hash{}}.
+store_share(Miner, MinerTarget, Hash) ->
+    SortKey = sort_key(),
+    Share = #aestratum_share{key = SortKey,
+                             hash = Hash,
+                             target = MinerTarget,
+                             miner = Miner},
+    HashR = #aestratum_hash{hash = Hash,
+                            key = SortKey},
+    ok = mnesia:write(Share),
+    ok = mnesia:write(HashR),
+    {ok, Share, HashR}.
+
+-spec store_round() -> {ok, #aestratum_round{}}.
+store_round() ->
+    Round = #aestratum_round{key = sort_key()},
+    ok = mnesia:write(Round),
+    {ok, Round}.
+
+-spec store_reward(non_neg_integer(), binary(), map(), map(), amount(), sort_key()) ->
+                          {ok, #aestratum_reward{}}.
+store_reward(Height, Hash, BenefRewards, MinerRewards, Amount, LastRoundKey) ->
+    Reward = #aestratum_reward{height = Height,
+                               hash = Hash,
+                               pool = BenefRewards,
+                               miners = MinerRewards,
+                               amount = Amount,
+                               round_key = LastRoundKey},
+    ok = mnesia:write(Reward),
+    {ok, Reward}.
+
+
+get_hash(Hash) ->
+    mnesia:read(?HASHES_TAB, Hash).
+
+
+-spec shares_range(sort_key(), pos_integer()) ->
+                          {ok, sort_key(), sort_key()} |
+                          {error, no_range}.
+shares_range(RewardShareKey, N) ->
+    First = case mnesia:prev(?ROUNDS_TAB, RewardShareKey) of
+                '$end_of_table' -> RewardShareKey;
+                Val -> Val
+            end,
+    Selector = ets:fun2ms(fun (#aestratum_round{key = K} = R) when K >= First -> R end),
+    case mnesia:select(?ROUNDS_TAB, Selector, N + 1, read) of
+        {[_ | _] = Rounds, _Cont} ->
+            #aestratum_round{key = Last} = lists:last(Rounds),
+            {ok, First, Last};
+        _ ->
+            {error, no_range}
+    end.
+
+-spec shares_selector(sort_key(), sort_key()) -> ets:match_spec().
+shares_selector(FirstKey, LastKey) ->
+    ets:fun2ms(fun (#aestratum_share{key = SK} = Share)
+                     when SK >= FirstKey, SK =< LastKey -> Share
+               end).
+
+shares_slices(Selector) ->
+    mnesia:select(?SHARES_TAB, Selector, ?SHARES_BATCH_LENGTH, read).
+
+
+delete_reward_records(Height) ->
+    case mnesia:read(aestratum_reward, Height) of
+        [#aestratum_reward{round_key = RoundKey}] ->
+            RoundSpec = ets:fun2ms(fun (#aestratum_round{key = K}) when K > RoundKey -> K end),
+            Rounds    = mnesia:select(?ROUNDS_TAB, RoundSpec),
+            ShareSpec = ets:fun2ms(fun (#aestratum_share{key = K, hash = H}) when K > RoundKey ->
+                                           {K, H}
+                                   end),
+            {Shares, Hashes} = lists:unzip(mnesia:select(?SHARES_TAB, ShareSpec)),
+            [mnesia:delete(?SHARES_TAB, S, write) || S <- Shares],
+            [mnesia:delete(?HASHES_TAB, H, write) || H <- Hashes],
+            [mnesia:delete(?ROUNDS_TAB, R, write) || R <- Rounds],
+            mnesia:delete(?REWARDS_TAB, Height, write),
+            ?info("deleted records for height ~p", [Height]),
+            ok;
+        [] ->
+            %% this shouldn't happen, since rewards are deleted one by one
+            ok
+    end.
+
+delete_payment_records(TxHashes) ->
+    [mnesia:delete(?PAYMENTS_TAB, Tx, write) || Tx <- TxHashes].
+
+is_empty(Tab) ->
+    mnesia:table_info(Tab, size) == 0.
--- apps/aestratum/src/aestratum_reward.erl
+++ apps/aestratum/src/aestratum_reward.erl
@@ -12,11 +12,6 @@
 
 -include("aestratum.hrl").
 -include_lib("aecore/include/blocks.hrl").
--include_lib("stdlib/include/ms_transform.hrl").
-
-%% Mnesia
--export([check_tables/1,
-         create_tables/1]).
 
 %% API
 -export([start_link/2,
@@ -34,46 +29,9 @@
 
 -define(KEYBLOCK_ROUNDS_DELAY, 180).
 -define(MAX_ROUNDS, 10).
--define(SHARES_BATCH_LENGTH, 1000).
 
--define(LOG_INFO(Format, Args), io:format("REWARD [INFO]: " ++ Format ++ "\n", Args)).
--define(LOG_ERROR(Format, Args), io:format("REWARD [ERROR]: " ++ Format ++ "\n", Args)).
-
--type amount() :: non_neg_integer().
--type sort_key() :: non_neg_integer().
 -type reward_return() :: {ok, #aestratum_reward{} | not_our_share} |
                          {error, no_range | cant_get_top_key_block}.
-
-%%%===================================================================
-%%% MNESIA CREATE TABLE HOOKS (driven by aec_db)
-%%%===================================================================
-
--define(TAB_DEF(Name, Type, Mode),
-        {Name, [aec_db:tab_copies(Mode),
-                {type, Type},
-                {record_name, Name},
-                {attributes, record_info(fields, Name)},
-                {user_properties, [{vsn, table_vsn(Name)}]}]}).
-
-create_tables(Mode) ->
-    Specs = [table_specs(Tab, Mode) || {missing_table, Tab} <- check_tables([])],
-    [{atomic, ok} = mnesia:create_table(Tab, Spec) || {Tab, Spec} <- Specs].
-
-check_tables(Acc) ->
-    lists:foldl(
-      fun ({Tab, Spec}, Acc1) ->
-              aec_db:check_table(Tab, Spec, Acc1)
-      end, Acc, tables_specs(disc)).
-
-table_specs(?HASHES_TAB, Mode) -> ?TAB_DEF(?HASHES_TAB, set, Mode);
-table_specs(?SHARES_TAB, Mode) -> ?TAB_DEF(?SHARES_TAB, ordered_set, Mode);
-table_specs(?ROUNDS_TAB, Mode) -> ?TAB_DEF(?ROUNDS_TAB, ordered_set, Mode);
-table_specs(?REWARDS_TAB, Mode) -> ?TAB_DEF(?REWARDS_TAB, ordered_set, Mode);
-table_specs(?PAYMENTS_TAB, Mode) -> ?TAB_DEF(?PAYMENTS_TAB, ordered_set, Mode).
-
-tables_specs(Mode) -> [table_specs(Tab, Mode) || Tab <- ?TABS].
-
-table_vsn(_) -> 1.
 
 %%%===================================================================
 %%% STATE
@@ -84,10 +42,10 @@ table_vsn(_) -> 1.
          benefs :: {TotalCutPcts :: number(), #{miner_pubkey() => float()}}}).
 
 -record(reward_key_block,
-        {share_key :: sort_key(),
+        {share_key :: aestratum_db:sort_key(),
          height    :: non_neg_integer(),
          target    :: non_neg_integer(),
-         tokens    :: amount(),
+         tokens    :: aestratum_db:amount(),
          hash      :: binary()}).
 
 %%%===================================================================
@@ -118,12 +76,14 @@ confirm_payout(Height) ->
 init([LastN, {BeneficiariesPctShare, Beneficiaries}]) ->
     State = #state{last_n = LastN,
                    benefs = {BeneficiariesPctShare, Beneficiaries}},
-    transaction(fun () -> mnesia:table_info(?ROUNDS_TAB, size) == 0 andalso store_round() end),
+    transaction(fun () ->
+                        aestratum_db:is_empty(?ROUNDS_TAB) andalso aestratum_db:store_round()
+                end),
     {ok, State}.
 
 
 handle_cast(keyblock, State) ->
-    transaction(fun () -> store_round() end),
+    transaction(fun () -> aestratum_db:store_round() end),
     case maybe_compute_rewards(State) of
         {ok, #aestratum_reward{height = Height, pool = PoolRewards, miners = MinersRewards}} ->
             aestratum_chain:payout_rewards(Height, PoolRewards, MinersRewards);
@@ -135,11 +95,11 @@ handle_cast(keyblock, State) ->
     {noreply, State};
 
 handle_cast({submit_share, <<MinerPK:?MINER_PUB_BYTES/binary>>, MinerTarget, Hash}, State) ->
-    transaction(fun () -> store_share(MinerPK, MinerTarget, Hash) end),
+    transaction(fun () -> aestratum_db:store_share(MinerPK, MinerTarget, Hash) end),
     {noreply, State};
 
 handle_cast({confirm_payout, Height}, State) ->
-    transaction(fun () -> delete_old_records(Height) end),
+    transaction(fun () -> aestratum_db:delete_reward_records(Height) end),
     {noreply, State};
 
 handle_cast(_, State) ->
@@ -164,51 +124,15 @@ format_status(_Opt, Status) ->
 %%% Internal functions
 %%%===================================================================
 
--spec sort_key() -> sort_key().
-sort_key() ->
-    abs(erlang:unique_integer([monotonic])).
-
--spec store_share(miner_pubkey(), pos_integer(), binary()) ->
-                         {ok, #aestratum_share{}, #aestratum_hash{}}.
-store_share(Miner, MinerTarget, Hash) ->
-    SortKey = sort_key(),
-    Share = #aestratum_share{key = SortKey,
-                             hash = Hash,
-                             target = MinerTarget,
-                             miner = Miner},
-    HashR = #aestratum_hash{hash = Hash,
-                            key = SortKey},
-    ok = mnesia:write(Share),
-    ok = mnesia:write(HashR),
-    {ok, Share, HashR}.
-
--spec store_round() -> {ok, #aestratum_round{}}.
-store_round() ->
-    Round = #aestratum_round{key = sort_key()},
-    ok = mnesia:write(Round),
-    {ok, Round}.
-
--spec store_reward(non_neg_integer(), binary(), map(), map(), amount(), sort_key()) ->
-                          {ok, #aestratum_reward{}}.
-store_reward(Height, Hash, BenefRewards, MinerRewards, Amount, LastRoundKey) ->
-    Reward = #aestratum_reward{height = Height,
-                               hash = Hash,
-                               pool = BenefRewards,
-                               miners = MinerRewards,
-                               amount = Amount,
-                               round_key = LastRoundKey},
-    ok = mnesia:write(Reward),
-    {ok, Reward}.
-
 -spec maybe_compute_rewards(#state{}) -> reward_return().
 maybe_compute_rewards(State) ->
     on_reward_share(
       fun (#reward_key_block{height = Height, hash = Hash, tokens = Amount} = RewardKeyBlock) ->
               case compute_rewards(RewardKeyBlock, State) of
                   {ok, BenefRewards, MinerRewards, LastRoundShareKey} ->
-                      store_reward(Height, Hash,
-                                   BenefRewards, MinerRewards,
-                                   Amount, LastRoundShareKey);
+                      aestratum_db:store_reward(Height, Hash,
+                                                BenefRewards, MinerRewards,
+                                                Amount, LastRoundShareKey);
                   Error ->
                       Error
               end
@@ -222,7 +146,7 @@ on_reward_share(TxFun) ->
             Hash = aestratum_chain:hash_header(KeyHeader),
             transaction(
               fun () ->
-                      case mnesia:read(?HASHES_TAB, Hash) of
+                      case aestratum_db:get_hash(Hash) of
                           [#aestratum_hash{key = RewardShareKey}] ->
                               {ok, Height, Target, Tokens} =
                                   aestratum_chain:header_info({KeyHeader, Hash}),
@@ -240,15 +164,15 @@ on_reward_share(TxFun) ->
     end.
 
 -spec compute_rewards(#reward_key_block{}, #state{}) ->
-                             {ok, map(), map(), sort_key()} |
+                             {ok, map(), map(), aestratum_db:sort_key()} |
                              {error, no_range}.
 compute_rewards(#reward_key_block{share_key = RewardShareKey, target = BlockTarget},
                 #state{last_n = N,
                        benefs = {BenefSumPcts, Beneficiaries}}) ->
-    case shares_range(RewardShareKey, N) of
+    case aestratum_db:shares_range(RewardShareKey, N) of
         {ok, FirstShareKey, LastRoundShareKey} ->
-            Selector = shares_selector(FirstShareKey, LastRoundShareKey),
-            SliceCont = mnesia:select(?SHARES_TAB, Selector, ?SHARES_BATCH_LENGTH, read),
+            Selector = aestratum_db:shares_selector(FirstShareKey, LastRoundShareKey),
+            SliceCont = aestratum_db:shares_slices(Selector),
             {SumScores, MinerGroups} = sum_group_shares(SliceCont, BlockTarget),
             BenefRewards = fold_rewards(BenefSumPcts, Beneficiaries),
             MinerRewards = fold_rewards(SumScores, MinerGroups),
@@ -256,29 +180,6 @@ compute_rewards(#reward_key_block{share_key = RewardShareKey, target = BlockTarg
         {error, Reason} ->
             {error, Reason}
     end.
-
--spec shares_range(sort_key(), pos_integer()) ->
-                          {ok, sort_key(), sort_key()} |
-                          {error, no_range}.
-shares_range(RewardShareKey, N) ->
-    First = case mnesia:prev(?ROUNDS_TAB, RewardShareKey) of
-                '$end_of_table' -> RewardShareKey;
-                Val -> Val
-            end,
-    Selector = ets:fun2ms(fun (#aestratum_round{key = K} = R) when K >= First -> R end),
-    case mnesia:select(?ROUNDS_TAB, Selector, N + 1, read) of
-        {[_ | _] = Rounds, _Cont} ->
-            #aestratum_round{key = Last} = lists:last(Rounds),
-            {ok, First, Last};
-        _ ->
-            {error, no_range}
-    end.
-
--spec shares_selector(sort_key(), sort_key()) -> ets:match_spec().
-shares_selector(FirstKey, LastKey) ->
-    ets:fun2ms(fun (#aestratum_share{key = SK} = Share)
-                     when SK >= FirstKey, SK =< LastKey -> Share
-               end).
 
 
 sum_group_shares(SliceCont, BlockTarget) ->
@@ -304,28 +205,6 @@ fold_rewards(SumScores, Beneficiaries) ->
                       RelScore = Score / SumScores,
                       Rewards#{BenefPK => RelScore}
               end, #{}, Beneficiaries).
-
-
-delete_old_records(Height) ->
-    case mnesia:read(aestratum_reward, Height) of
-        [#aestratum_reward{round_key = RoundKey}] ->
-            RoundSpec = ets:fun2ms(fun (#aestratum_round{key = K}) when K > RoundKey -> K end),
-            Rounds    = mnesia:select(?ROUNDS_TAB, RoundSpec),
-            ShareSpec = ets:fun2ms(fun (#aestratum_share{key = K, hash = H}) when K > RoundKey ->
-                                           {K, H}
-                                   end),
-            {Shares, Hashes} = lists:unzip(mnesia:select(?SHARES_TAB, ShareSpec)),
-            [mnesia:delete(?SHARES_TAB, S, write) || S <- Shares],
-            [mnesia:delete(?HASHES_TAB, H, write) || H <- Hashes],
-            [mnesia:delete(?ROUNDS_TAB, R, write) || R <- Rounds],
-            mnesia:delete(?REWARDS_TAB, Height, write),
-            ?info("deleted records for height ~p", [Height]),
-            ok;
-        [] ->
-            %% this shouldn't happen, since rewards are deleted one by one
-            ok
-    end.
-
 
 -spec transaction(fun (() -> any())) -> any() | no_return().
 transaction(Fun) when is_function(Fun, 0) ->
