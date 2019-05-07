0e172ad339482804f673674b0fdd65d939d41f4b
Tobias Lindahl <tobias.lindahl@gmail.com>
2019-05-07 12:47:09 +0200

    Add fate testing on proper chain

--- apps/aecontract/test/aect_test_utils.erl
+++ apps/aecontract/test/aect_test_utils.erl
@@ -370,6 +370,10 @@ decode_data(Type, <<"cb_", _/binary>> = EncData) ->
 decode_data(Type, Data) ->
     decode_data_(Type, Data).
 
+decode_data_(fate, Data) ->
+    try {ok, aefate_test_utils:decode(aeb_fate_encoding:deserialize(Data))}
+    catch _:_ -> {error, <<"bad fate data">>}
+    end;
 decode_data_(Type, Data) ->
     case get_type(Type) of
         {ok, SophiaType} ->
