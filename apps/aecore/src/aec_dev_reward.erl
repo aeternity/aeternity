1c9734023e1220760525e62e375d1a52edabb790
sennui <michal@sennui.com>
2019-05-08 17:37:24 +0200

    use default reward split key from testnet-keys repository

--- apps/aecore/src/aec_dev_reward.erl
+++ apps/aecore/src/aec_dev_reward.erl
@@ -14,8 +14,8 @@
 
 -define(ENABLED, false).
 -define(TOTAL_SHARES, 1000). % 100 shares == 10% of the reward
-%%% for: "ak_2uN1CEVGs4D5QnFudhp8dSqrwJEMFiTfwvbCK3SJzVYRDiusfd:100" TODO - use actual pubkey for real account
--define(BENEFICIARIES, [{<<250,151,56,184,99,123,38,230,217,93,156,146,231,90,209,233,15,203,25,102,32,140,178,207,59,171,81,2,249,197,198,61>>, 100}]).
+%%% for: "ak_2A3PZPfMC2X7ZVy4qGXz2xh2Lbh79Q4UvZ5fdH7QVFocEgcKzU:100" TODO - use actual pubkey for mainnet account
+-define(BENEFICIARIES, [{<<152,57,168,5,218,153,177,254,226,207,243,133,11,50,143,68,121,242,94,41,187,198,158,67,133,88,6,71,55,26,85,54>>, 100}]).
 
 ensure_env() ->
     Enabled = cfg(<<"protocol_beneficiaries_enabled">>, ?ENABLED),
