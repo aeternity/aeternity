ef06af40f1acaf2c4c440555858d01ce477f43b6
skkw <skkw@users.noreply.github.com>
2019-05-24 17:02:00 +0200

    minor cleanup and refactor of supporting functionality

--- apps/aestratum/src/aestratum_fn.erl
+++ apps/aestratum/src/aestratum_fn.erl
@@ -8,9 +8,8 @@
          is_ok/1,
          ok_err/1, ok_err/2,
          ok_val_err/1, ok_val_err/2,
-         ok_or/2, ok_or_else/2, lazy_ok/3,
+         ok_or/2, ok_or_else/2,
          tag_val_err/3,
-         hd_or/2, hd_or_else/2, lazy_hd/3,
          key_val/2, val/2, val/3]).
 
 id(X) -> X.
@@ -19,7 +18,7 @@ is_ok({ok, _}) -> true;
 is_ok(_) -> false.
 
 ok() -> fun (X) -> ok(X) end.
-const(V) -> fun (_) -> V end.
+const(V) -> fun () -> V end.
 err() -> error.
 
 ok(X) -> {ok, X}.
@@ -44,20 +43,8 @@ ok_or(_, Default)      -> Default.
 ok_or_else({ok, _} = OK, _) -> OK;
 ok_or_else(_, DefaultFn)    -> DefaultFn().
 
-lazy_ok({ok, X}, T, _)  -> T(X);
-lazy_ok(Other, _, F) -> F(Other).
-
 tag_val_err({Tag, Val}, Tag, _ErrTag) -> Val;
 tag_val_err(Other, _Tag, ErrTag) -> error({ErrTag, Other}).
-
-hd_or([X | _], _)  -> X;
-hd_or([], Default) -> Default.
-
-hd_or_else([X | _], _)  -> X;
-hd_or_else([], DefaultFn) -> DefaultFn().
-
-lazy_hd([X | _], T, _)  -> T(X);
-lazy_hd([], _, F) -> F().
 
 key_val({K, Default}, #{} = M) when is_atom(K) ->
     {K, val(K, M, Default)};
--- apps/aestratum/src/aestratum_log.hrl
+++ apps/aestratum/src/aestratum_log.hrl
@@ -1,7 +1,8 @@
 
--define(DEBUG(Fmt, Args), aestratum:debug(Fmt, Args)).
--define(INFO(Fmt, Args), aestratum:info(Fmt, Args)).
--define(ERROR(Fmt, Args), aestratum:error(Fmt, Args)).
--define(WARN(Fmt, Args), aestratum:warning(Fmt, Args)).
--define(CRITICAL(Fmt, Args), aestratum:critical(Fmt, Args)).
+-define(LOG(Level, Fmt, Args), lager:log(aestratum_lager_event, Level, [], Fmt, Args)).
 
+-define(DEBUG(Fmt, Args),    ?LOG(debug, Fmt, Args)).
+-define(INFO(Fmt, Args),     ?LOG(info, Fmt, Args)).
+-define(ERROR(Fmt, Args),    ?LOG(error, Fmt, Args)).
+-define(WARN(Fmt, Args),     ?LOG(warn, Fmt, Args)).
+-define(CRITICAL(Fmt, Args), ?LOG(critical, Fmt, Args)).
