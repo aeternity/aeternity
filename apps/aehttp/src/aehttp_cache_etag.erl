e9afba8f6c0176a4d052965618db1d0a3b4a6b9a
Dincho Todorov <dincho@users.noreply.github.com>
2019-06-26 17:56:05 +0300

    Correctly handle generations etag

--- apps/aehttp/src/aehttp_cache_etag.erl
+++ apps/aehttp/src/aehttp_cache_etag.erl
@@ -53,11 +53,10 @@ generate('GetMicroBlockTransactionsCountByHash', Req, State) ->
     {{strong, Hash}, Req, State};
 
 generate('GetGenerationByHash', Req, State) ->
-    Hash = cowboy_req:binding(hash, Req),
-    {{strong, Hash}, Req, State};
+    handle_generation_hash(Req, State);
 
 generate('GetGenerationByHeight', Req, State) ->
-    handle_block_height(Req, State);
+    handle_generation_height(Req, State);
 
 generate('GetTransactionByHash', Req, State) ->
     Hash = cowboy_req:binding(hash, Req),
@@ -88,7 +87,37 @@ handle_block_height(Req, State) ->
             {undefined, Req, State}
     end.
 
--spec etag_block_height(integer()) -> {ok, aec_headers:header()} | undefined.
+-spec handle_generation_hash(cowboy_req:req(), any()) -> cowboy_etag_res().
+handle_generation_hash(Req, State) ->
+    Hash0 = cowboy_req:binding(hash, Req),
+    case aeser_api_encoder:safe_decode(key_block_hash, Hash0) of
+        {ok, Hash} ->
+            case etag_generation_hash(Hash) of
+                {ok, ETag} ->
+                    {{strong, ETag}, Req, State};
+                _ ->
+                    {undefined, Req, State}
+            end;
+        _ ->
+            {undefined, Req, State}
+    end.
+
+-spec handle_generation_height(cowboy_req:req(), any()) -> cowboy_etag_res().
+handle_generation_height(Req, State) ->
+    Height0 = cowboy_req:binding(height, Req),
+    case to_int(Height0) of
+        {ok, Height} ->
+            case etag_generation_height(Height) of
+                {ok, ETag} ->
+                    {{strong, ETag}, Req, State};
+                _ ->
+                    {undefined, Req, State}
+            end;
+        _ ->
+            {undefined, Req, State}
+    end.
+
+-spec etag_block_height(integer()) -> {ok, binary()} | undefined.
 etag_block_height(Height) ->
     case aehttp_logic:get_key_header_by_height(Height) of
         {ok, Header} ->
@@ -107,6 +136,33 @@ etag_block_header(Header) ->
     {ok, Hash} = aec_headers:hash_header(Header),
     Hex = aeu_hex:bin_to_hex(Hash),
     list_to_binary(Hex).
+
+-spec etag_generation_height(non_neg_integer()) -> {ok, binary()} | undefined.
+etag_generation_height(Height) ->
+    case aec_chain:get_generation_by_height(Height, forward) of
+        {ok, #{key_block := KeyBlock, micro_blocks := MBs}} ->
+            ETag = etag_generation(KeyBlock, MBs),
+            {ok, ETag};
+        _ ->
+            undefined
+    end.
+
+-spec etag_generation_hash(binary()) -> {ok, binary()} | undefined.
+etag_generation_hash(Hash) ->
+    case aec_chain:get_generation_by_hash(Hash, forward) of
+        {ok, #{key_block := KeyBlock, micro_blocks := MBs}} ->
+            ETag = etag_generation(KeyBlock, MBs),
+            {ok, ETag};
+        _ ->
+            undefined
+    end.
+-spec etag_generation(aec_blocks:key_block(), [aec_blocks:micro_block()]) -> binary().
+etag_generation(KeyBlock, []) ->
+    etag_block(KeyBlock);
+
+etag_generation(_KeyBlock, MBs) ->
+    Block = lists:last(MBs),
+    etag_block(Block).
 
 -spec to_int(binary()) -> {ok, integer()} | error.
 to_int(Data) ->
