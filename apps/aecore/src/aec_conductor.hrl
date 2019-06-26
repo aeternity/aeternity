f297039df6dbbb7a19e66cdf27fc95264325f2f2
skkw <47307028+skkw@users.noreply.github.com>
2019-06-26 07:52:35 +0200

    Fix type of stratum_mode field in apps/aecore/src/aec_conductor.hrl
    Co-Authored-By: Luca Favatella <lucafavatella@users.noreply.github.com>

--- apps/aecore/src/aec_conductor.hrl
+++ apps/aecore/src/aec_conductor.hrl
@@ -50,5 +50,5 @@
                 beneficiary                       :: <<_:(32*8)>> | 'undefined',
                 fraud_list              = []      :: list({binary(), aec_pof:pof()}),
                 pending_key_block                 :: aec_blocks:block() | 'undefined',
-                stratum_mode            = false   :: boolean() | 'undefined'
+                stratum_mode            = false   :: boolean()
                }).
