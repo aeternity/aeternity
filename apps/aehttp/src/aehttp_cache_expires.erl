a1142129df5b76e67b415292aded150168deaa75
Dincho Todorov <dincho@users.noreply.github.com>
2019-06-27 17:56:48 +0300

    Properly handle generations cache time

--- apps/aehttp/src/aehttp_cache_expires.erl
+++ apps/aehttp/src/aehttp_cache_expires.erl
@@ -31,49 +31,46 @@
 -define(DEFAULT_AGED_BLOCKS_CACHE_TIME, 24*60*60). % Cache aged key blocks for a day by default
 
 -type cowboy_expires_res() :: {calendar:datetime() | undefined, cowboy_req:req(), any()}.
+-type expire_type() :: key | micro.
+-type hash_param() :: key_block_hash | micro_block_hash.
 
 %%%===================================================================
 %%% External API
 
 -spec expires(atom(), cowboy_req:req(), any()) -> cowboy_expires_res().
 expires('GetTopBlock', Req, State) ->
-    CacheTime = expected_next_micro_block_time(),
+    CacheTime = expected_next_block_time(micro),
     Exp = unixtime_to_datetime(CacheTime),
     {Exp, Req, State};
 
 expires('GetCurrentKeyBlock', Req, State) ->
-    case aec_chain:top_key_block() of
-        {ok, Block} ->
-            Header = aec_blocks:to_header(Block),
-            Exp = expires_block_header(Header),
-            {Exp, Req, State};
-        _ ->
-            {undefined, Req, State}
-    end;
+    Header = top_key_block_header(),
+    Exp = expires_datetime(Header, key),
+    {Exp, Req, State};
 
 expires('GetKeyBlockByHash', Req, State) ->
-    handle_key_block_hash(Req, State);
+    handle_block_hash(key_block_hash, key, Req, State);
 
 expires('GetKeyBlockByHeight', Req, State) ->
-    handle_key_block_height(Req, State);
+    handle_block_height(key, Req, State);
 
 expires('GetMicroBlockHeaderByHash', Req, State) ->
-    handle_micro_block_hash(Req, State);
+    handle_block_hash(micro_block_hash, micro, Req, State);
 
 expires('GetMicroBlockTransactionsByHash', Req, State) ->
-    handle_micro_block_hash(Req, State);
+    handle_block_hash(micro_block_hash, micro, Req, State);
 
 expires('GetMicroBlockTransactionByHashAndIndex', Req, State) ->
-    handle_micro_block_hash(Req, State);
+    handle_block_hash(micro_block_hash, micro, Req, State);
 
 expires('GetMicroBlockTransactionsCountByHash', Req, State) ->
-    handle_micro_block_hash(Req, State);
+    handle_block_hash(micro_block_hash, micro, Req, State);
 
 expires('GetGenerationByHash', Req, State) ->
-    handle_key_block_hash(Req, State);
+     handle_block_hash(key_block_hash, micro, Req, State);
 
 expires('GetGenerationByHeight', Req, State) ->
-    handle_key_block_height(Req, State);
+    handle_block_height(micro, Req, State);
 
 expires('GetTransactionByHash', Req, State) ->
     handle_transaction_hash(Req, State);
@@ -97,35 +94,25 @@ aged_blocks_cache_time() ->
     aeu_env:user_config_or_env([<<"http">>, <<"cache">>, <<"aged_blocks_cache_time">>],
                                aehttp, [cache, aged_blocks_cache_time], ?DEFAULT_AGED_BLOCKS_CACHE_TIME).
 
--spec handle_key_block_hash(cowboy_req:req(), any()) -> cowboy_expires_res().
-handle_key_block_hash(Req, State) ->
+-spec handle_block_hash(hash_param(), expire_type(), cowboy_req:req(), any()) -> cowboy_expires_res().
+handle_block_hash(Param, CacheType, Req, State) ->
     Hash0 = cowboy_req:binding(hash, Req),
-    case aeser_api_encoder:safe_decode(key_block_hash, Hash0) of
+    case aeser_api_encoder:safe_decode(Param, Hash0) of
         {ok, Hash} ->
-            Exp = expires_block_hash(Hash),
+            Exp = expires_block_hash(Hash, CacheType),
             {Exp, Req, State};
         _ -> {undefined, Req, State}
     end.
 
--spec handle_key_block_height(cowboy_req:req(), any()) -> cowboy_expires_res().
-handle_key_block_height(Req, State) ->
+-spec handle_block_height(expire_type(), cowboy_req:req(), any()) -> cowboy_expires_res().
+handle_block_height(CacheType, Req, State) ->
     Height0 = cowboy_req:binding(height, Req),
     case to_int(Height0) of
         {ok, Height} ->
-            Exp = expires_block_height(Height),
+            Exp = expires_block_height(Height, CacheType),
             {Exp, Req, State};
         _ ->
             {undefined, Req, State}
-    end.
-
--spec handle_micro_block_hash(cowboy_req:req(), any()) -> cowboy_expires_res().
-handle_micro_block_hash(Req, State) ->
-    Hash0 = cowboy_req:binding(hash, Req),
-    case aeser_api_encoder:safe_decode(micro_block_hash, Hash0) of
-        {ok, Hash} ->
-            Exp = expires_block_hash(Hash),
-            {Exp, Req, State};
-        _ -> {undefined, Req, State}
     end.
 
 -spec handle_transaction_hash(cowboy_req:req(), any()) -> cowboy_expires_res().
@@ -135,7 +122,7 @@ handle_transaction_hash(Req, State) ->
         {ok, Hash} ->
             case aec_chain:find_tx_with_location(Hash) of
                 {BlockHash, _Tx} when is_binary(BlockHash) ->
-                    Exp = expires_block_hash(BlockHash),
+                    Exp = expires_block_hash(BlockHash, micro),
                     {Exp, Req, State};
                 _ ->
                     {undefined, Req, State}
@@ -144,66 +131,81 @@ handle_transaction_hash(Req, State) ->
             {undefined, Req, State}
     end.
 
--spec expires_block_height(non_neg_integer()) -> {ok, aec_headers:header()} | undefined.
-expires_block_height(Height) ->
+-spec expires_block_height(non_neg_integer(), expire_type()) -> calendar:datetime() | undefined.
+expires_block_height(Height, Type) ->
     case aehttp_logic:get_key_header_by_height(Height) of
-        {ok, Header} -> expires_block_header(Header);
+        {ok, Header} -> expires_datetime(Header, Type);
         _ -> undefined
     end.
 
--spec expires_block_hash(binary()) -> {ok, aec_headers:header()} | undefined.
-expires_block_hash(Hash) ->
+-spec expires_block_hash(binary(), expire_type()) -> calendar:datetime() | undefined.
+expires_block_hash(Hash, Type) ->
     case aehttp_logic:get_header_by_hash(Hash) of
-        {ok, Header} -> expires_block_header(Header);
+        {ok, Header} -> expires_datetime(Header, Type);
         _ -> undefined
     end.
 
--spec expires_block_header(aec_headers:header()) -> non_neg_integer() | error.
-expires_block_header(Header) ->
-    case block_header_cache_time(Header) of
-        CacheTime when is_integer(CacheTime) ->
-            unixtime_to_datetime(CacheTime);
-        _ -> error
+-spec expires_datetime(aec_headers:header(), expire_type()) -> calendar:datetime().
+expires_datetime(Header, Type) ->
+    unixtime_to_datetime(cache_time(Header, Type)).
+
+-spec cache_time(aec_headers:header(), key | micro ) -> non_neg_integer().
+cache_time(Header, key) ->
+    case is_aged(Header) of
+        true  ->
+            lager:info("Aged block."),
+            aeu_time:now_in_secs() + aged_blocks_cache_time();
+        false -> expected_next_block_time(key)
+    end;
+
+cache_time(Header, micro) ->
+    case is_top_generation(Header) of
+        true ->
+            lager:info("Part of top generation."),
+            expected_next_block_time(micro);
+        false -> cache_time(Header, key)
     end.
 
--spec block_header_cache_time(aec_headers:header()) -> non_neg_integer() | error.
-block_header_cache_time(Header) ->
+-spec is_aged(aec_headers:header()) -> boolean().
+is_aged(Header) ->
     Now = aeu_time:now_in_secs(),
     BlockTimestamp = aec_headers:time_in_secs(Header),
     Diff = Now - BlockTimestamp,
-    case Diff > aged_blocks_time() of
-        true ->
-            Now + aged_blocks_cache_time();
-        false ->
-            expected_next_key_block_time()
-    end.
+    Diff > aged_blocks_time().
 
--spec expected_next_key_block_time() -> non_neg_integer() | error.
-expected_next_key_block_time() ->
-    case top_key_block_header() of
-        {ok, Header} ->
-            BlockTimestamp = aec_headers:time_in_secs(Header),
-            Rate = aeu_time:msecs_to_secs(aec_governance:expected_block_mine_rate()),
-            BlockTimestamp + Rate;
-        _ -> error
-    end.
+-spec is_top_generation(aec_headers:header()) -> boolean().
+is_top_generation(Header) ->
+    TopKeyHeader = top_key_block_header(),
+    {ok, TopKeyHash} = aec_headers:hash_header(TopKeyHeader),
+    KeyHash = case aec_headers:type(Header) of
+        micro ->
+            aec_headers:prev_key_hash(Header);
+        key ->
+            {ok, Hash} = aec_headers:hash_header(Header),
+            Hash
+    end,
+    lager:info("Compare: ~p =:= ~p", [TopKeyHash, KeyHash]),
+    TopKeyHash =:= KeyHash.
 
--spec top_key_block_header() -> {ok, aec_headers:header()} | error.
+-spec top_key_block_header() -> aec_headers:header().
 top_key_block_header() ->
-    case aec_chain:top_key_block() of
-        {ok, Block} ->
-            {ok, aec_blocks:to_key_header(Block)};
-        _ ->
-            error
-    end.
+    {ok, Block} = aec_chain:top_key_block(),
+    aec_blocks:to_key_header(Block).
 
--spec expected_next_micro_block_time() -> non_neg_integer().
-expected_next_micro_block_time() ->
+-spec expected_next_block_time(key | micro) -> non_neg_integer().
+expected_next_block_time(Type) ->
     TopHeader = aec_chain:top_header(),
     BlockTimestamp = aec_headers:time_in_secs(TopHeader),
-    Rate = aeu_time:msecs_to_secs(aec_governance:micro_block_cycle()),
+    Rate = aeu_time:msecs_to_secs(block_rate(Type)),
     BlockTimestamp + Rate.
 
+-spec block_rate(key|micro) -> non_neg_integer().
+block_rate(key) ->
+    aec_governance:expected_block_mine_rate();
+block_rate(micro) ->
+    aec_governance:micro_block_cycle().
+
+-spec to_int(binary()) -> integer() | error.
 to_int(Data) ->
     try {ok, binary_to_integer(Data)}
     catch
