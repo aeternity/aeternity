d199130547297d2f1fe10d12d711318c1cf01391
skkw <skkw@users.noreply.github.com>
2019-08-06 14:32:57 +0200

    serialize definition via [{binary, [{binary, id}]}] scheme instead of [{binary, [{binary, binary}]}]

--- apps/aens/src/aens_subname_tx.erl
+++ apps/aens/src/aens_subname_tx.erl
@@ -171,7 +171,7 @@ serialization_template(?SUBNAME_TX_VSN) ->
     [ {account_id, id}
     , {nonce, int}
     , {name, binary}
-    , {definition, [{binary, [{binary, binary}]}]}
+    , {definition, [{binary, [{binary, id}]}]}
     , {fee, int}
     , {ttl, int}
     ].
