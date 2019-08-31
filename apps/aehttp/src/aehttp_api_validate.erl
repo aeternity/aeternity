9b83ea03b3891b83b2121d9db9353bd496ea46f4
Dincho Todorov <dincho@users.noreply.github.com>
2019-08-31 19:21:50 +0300

    Use swagger.yaml instead of swagger.json to server /api endpoint

--- apps/aehttp/src/aehttp_api_validate.erl
+++ apps/aehttp/src/aehttp_api_validate.erl
@@ -7,10 +7,9 @@
 
 -spec json_spec() -> jsx:json_text().
 json_spec() ->
-    {ok, AppName} = application:get_application(?MODULE),
-    Filename = filename:join(code:priv_dir(AppName), "swagger.json"),
-    {ok, Json} = file:read_file(Filename),
-    Json.
+    Yamls = yamerl_constr:file("config/swagger.yaml", [str_node_as_binary]),
+    Yaml = lists:last(Yamls),
+    jsx:prettify(jsx:encode(Yaml)).
 
 -spec validator() -> jesse_state:state().
 validator() ->
