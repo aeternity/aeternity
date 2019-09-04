c191faa1300d274bea33f85a2932714214e896f4
Ulf Norell <ulf.norell@gmail.com>
2019-09-04 09:24:22 +0200

    Add test case for higher order state (FATE only)

--- apps/aecontract/test/aecontract_SUITE.erl
+++ apps/aecontract/test/aecontract_SUITE.erl
@@ -154,6 +154,7 @@
         , sophia_bytes_to_x/1
         , sophia_address_checks/1
         , sophia_remote_gas/1
+        , sophia_higher_order_state/1
         , create_store/1
         , read_store/1
         , store_zero_value/1
@@ -382,7 +383,8 @@ groups() ->
                                  sophia_bytes,
                                  sophia_bytes_to_x,
                                  sophia_address_checks,
-                                 sophia_too_little_gas_for_mem
+                                 sophia_too_little_gas_for_mem,
+                                 sophia_higher_order_state
                                ]}
     , {sophia_oracles_ttl, [],
           %% Test Oracle TTL handling
@@ -1580,6 +1582,19 @@ sophia_remote_gas(_Cfg) ->
 
     ok.
 
+sophia_higher_order_state(_Cfg) ->
+    ?skipRest(not ?IS_FATE_SOPHIA(vm_version()), higher_order_state_only_in_fate),
+    state(aect_test_utils:new_state()),
+    Acc = ?call(new_account, 1000000000 * aec_test_utils:min_gas_price()),
+    Ct  = ?call(create_contract, Acc, higher_order_state, {}),
+    1   = ?call(call_contract, Acc, Ct, apply, word, {1}),
+    {}  = ?call(call_contract, Acc, Ct, inc,  {tuple, []}, {}),
+    2   = ?call(call_contract, Acc, Ct, apply, word, {1}),
+    {}  = ?call(call_contract, Acc, Ct, inc,  {tuple, []}, {}),
+    3   = ?call(call_contract, Acc, Ct, apply, word, {1}),
+    {}  = ?call(call_contract, Acc, Ct, inc,  {tuple, []}, {}),
+    4   = ?call(call_contract, Acc, Ct, apply, word, {1}),
+    ok.
 
 sophia_vm_interaction(Cfg) ->
     state(aect_test_utils:new_state()),
