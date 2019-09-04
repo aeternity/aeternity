a1156043b4424788cc6f01cd3534d35617f1e402
Juraj Hlista <jurajhlista@gmail.com>
2019-09-04 14:07:03 +0100

    Remove unused functions

--- apps/aecore/src/aec_validation.erl
+++ apps/aecore/src/aec_validation.erl
@@ -15,29 +15,6 @@
 %%% API
 %%%===================================================================
 
-validate_block(#{ key_block    := KeyBlock,
-                  micro_blocks := MicroBlocks,
-                  add_keyblock := AddKeyblock
-                }) ->
-    GenesisHeight = aec_block_genesis:height(),
-    case aec_blocks:height(KeyBlock) of
-        GenesisHeight ->
-            case aec_blocks:to_header(KeyBlock) == aec_block_genesis:genesis_header()
-                    andalso MicroBlocks == [] of
-                true  -> ok;
-                false -> {error, invalid_genesis_generation}
-            end;
-        Height when Height > GenesisHeight ->
-            case validate_micro_block_list(MicroBlocks) of
-                ok ->
-                    case AddKeyblock of
-                        true -> aec_blocks:validate_key_block(KeyBlock);
-                        false -> ok
-                    end;
-                {error, _} = Err ->
-                    Err
-            end
-    end;
 validate_block(Block) ->
     case aec_blocks:is_key_block(Block) of
         true ->
@@ -45,13 +22,3 @@ validate_block(Block) ->
         false ->
             aec_blocks:validate_micro_block(Block)
     end.
-
-validate_micro_block_list([Block|Left]) ->
-    case aec_blocks:validate_micro_block(Block) of
-        ok ->
-            validate_micro_block_list(Left);
-        {error, _} = Err ->
-            Err
-    end;
-validate_micro_block_list([]) ->
-    ok.
