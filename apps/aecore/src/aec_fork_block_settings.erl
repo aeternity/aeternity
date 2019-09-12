cac62c93e253b34de0519bb217178e9a70f9ac80
Tobias Lindahl <tobias.lindahl@gmail.com>
2019-09-13 00:40:10 +0200

    Extend contract file parsing

--- apps/aecore/src/aec_fork_block_settings.erl
+++ apps/aecore/src/aec_fork_block_settings.erl
@@ -49,7 +49,7 @@ lima_accounts() -> preset_accounts(?LIMA_PROTOCOL_VSN,
 
 -spec lima_contracts() -> list().
 lima_contracts() -> preset_contracts(?LIMA_PROTOCOL_VSN,
-                                     lima_contracts_file_failed).
+                                     lima_contracts_file_missing).
 
 
 -spec preset_accounts(aec_hard_forks:protocol_vsn(), atom()) -> list().
@@ -93,7 +93,7 @@ read_preset_accounts(Release) ->
 preset_contracts(Release, ErrorMsg) ->
     case read_preset_contracts(Release) of
         {error, {_Err, Msg}} ->
-            % no setup, no preset accounts
+            % no setup, no preset contracts
             erlang:error({ErrorMsg, Msg});
         {ok, JSONData} ->
             DecodedData =
@@ -108,11 +108,76 @@ preset_contracts(Release, ErrorMsg) ->
                 end,
             lists:map(
               fun({EncodedPubKey, Spec}) ->
-                      {ok, PubKey} = aeser_api_encoder:safe_decode(contract_pubkey, EncodedPubKey),
-                      Spec#{pubkey => PubKey}
+                      Map = maps:from_list([{<<"pubkey">>, EncodedPubKey}|Spec]),
+                      decode_contract_spec(Map)
               end,
               DecodedData)
     end.
+
+%%% ===========================================
+%%% Spec format for the hard fork contract file
+%%% ===========================================
+%%%
+%%% { <API encoded pubkey for contract> : { "amount" : <integer>,
+%%%                                         "vm_version" : <integer>,
+%%%                                         "abi_version" : <integer>,
+%%%                                         "nonce" : <integer>
+%%%                                         "code" : <API encoded contract byte array>
+%%%                                         "call_data" : <API encoded contract byte array>
+%%%                                       }
+%%% ...
+%%% }
+%%%
+%%% The locked token account will be the owner of the contracts.
+%%% The nonces must correspond to the nonces of the owner account.
+%%% The nonces must be consecutive (but not necessarity ordered)
+%%% The pubkey of the contract must correspond to the computed pubkey
+%%%    (based on owner account and nonce). This is mostly a fail safe to ensure
+%%%    that the contract pubkey is visible in the file.
+%%%
+%%% Partial example spec:
+%%% { "ct_asdf...."    : { "amount"      : 42,
+%%%                        "vm_version"  : 5,
+%%%                        "abi_version" : 3,
+%%%                        "nonce"       : 1,
+%%%                        "code"        : "cb_asdf....",
+%%%                        "call_data"   : "cb_asdf...."
+%%%                      }
+%%% }
+
+decode_contract_spec(#{ <<"amount">>       := Amount
+                      , <<"vm_version">>   := VMVersion
+                      , <<"abi_version">>  := ABIVersion
+                      , <<"nonce">>        := Nonce
+                      , <<"code">>         := EncodedCode
+                      , <<"call_data">>    := EncodedCallData
+                      , <<"pubkey">>       := EncodedPubKey} = SpecIn
+                    ) when is_integer(Amount),
+                           is_integer(VMVersion),
+                           is_integer(ABIVersion),
+                           is_integer(Nonce),
+                           is_binary(EncodedCode),
+                           is_binary(EncodedCallData),
+                           is_binary(EncodedPubKey) ->
+    try
+        7 = maps:size(SpecIn), %% Only the fields above
+        {ok, PubKey}   = aeser_api_encoder:safe_decode(contract_pubkey, EncodedPubKey),
+        {ok, Code}     = aeser_api_encoder:safe_decode(contract_bytearray, EncodedCode),
+        {ok, CallData} = aeser_api_encoder:safe_decode(contract_bytearray, EncodedCallData),
+        #{ amount       => Amount
+         , vm_version   => VMVersion
+         , abi_version  => ABIVersion
+         , nonce        => Nonce
+         , code         => Code
+         , call_data    => CallData
+         , pubkey       => PubKey}
+    catch _:_ ->
+        error({invalid_spec, SpecIn})
+    end;
+decode_contract_spec(SpecIn) ->
+    error({invalid_spec, SpecIn}).
+
+
 
 -spec read_preset_contracts(aec_hard_forks:protocol_vsn()) -> {ok, binary()}| {error, {atom(), string()}}.
 read_preset_contracts(?LIMA_PROTOCOL_VSN = Release) ->
@@ -133,7 +198,7 @@ accounts_json_file() ->
     "accounts_test.json".
 
 contracts_json_file() ->
-    "accounts_test.json".
+    "contracts_test.json".
 
 -else.
 accounts_json_file() ->
