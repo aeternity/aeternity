a944a5923faa2d58defe4e8a1d7ad8b5c50da7f9
Ivaylo Badinov <venimus@gmail.com>
2019-09-17 13:16:55 +0300

    Upload windows artifacts

--- .circleci/set_package_path.sh
+++ .circleci/set_package_path.sh
@@ -12,12 +12,11 @@ fi
 
 PKG_SUFFIX="${PKG_TARGET_OS}-"`uname -m`
 
-
 VERSION=${CIRCLE_SHA1:-unknown}
 if [[ -n $CIRCLE_TAG && $CIRCLE_TAG =~ ^v([0-9]+\.[0-9]+\.[0-9]+(-[a-z0-9\.\+]+)*)$ ]]; then
     VERSION=${BASH_REMATCH[1]}
 fi
 
-PACKAGE_TARBALL=${PACKAGES_DIR:?}/aeternity-${VERSION}-${PKG_SUFFIX}.tar.gz
+PACKAGE_TARBALL=${PACKAGES_DIR:?}/aeternity-${VERSION}-${PKG_SUFFIX}${PKG_EXT:-".tar.gz"}
 echo "export PKG_SUFFIX=${PKG_SUFFIX}" >> $BASH_ENV
 echo "export PACKAGE_TARBALL=${PACKAGE_TARBALL}" >> $BASH_ENV
