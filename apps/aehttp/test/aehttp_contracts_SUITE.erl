2510d2175661a6b281763c32fee52097a2763988
Ulf Norell <ulf.norell@gmail.com>
2019-10-01 11:44:53 +0200

    Update gas values in test

--- apps/aehttp/test/aehttp_contracts_SUITE.erl
+++ apps/aehttp/test/aehttp_contracts_SUITE.erl
@@ -1132,6 +1132,15 @@ events_contract(Config) ->
         false -> ?assertMatch(FATE, Res)
     end).
 
+-define(assertMatchVM(ExpVm1, ExpVm2, ExpVm3, ExpVm4, ExpFate, Res),
+    case aect_test_utils:vm_version() of
+        ?VM_AEVM_SOPHIA_1 -> ?assertMatch(ExpVm1, Res);
+        ?VM_AEVM_SOPHIA_2 -> ?assertMatch(ExpVm2, Res);
+        ?VM_AEVM_SOPHIA_3 -> ?assertMatch(ExpVm3, Res);
+        ?VM_AEVM_SOPHIA_4 -> ?assertMatch(ExpVm4, Res);
+        ?VM_FATE_SOPHIA_1 -> ?assertMatch(ExpFate, Res)
+    end).
+
 remote_gas_test_contract(Config) ->
     %% Set an account.
     Node = proplists:get_value(node_name, Config),
@@ -1155,7 +1164,7 @@ remote_gas_test_contract(Config) ->
     call_get(APub, APriv, EncC2Pub, Contract, 100),
     force_fun_calls(Node),
     Balance1 = get_balance(APub),
-    ?assertMatchVM(1600596, 1600042, (Balance0 - Balance1) div ?DEFAULT_GAS_PRICE),
+    ?assertMatchVM(1600596, 1600596, 1600596, 1600916, 1600042, (Balance0 - Balance1) div ?DEFAULT_GAS_PRICE),
 
     %% Test remote call with limited gas
     %% Call contract remote set function with limited gas
@@ -1163,7 +1172,7 @@ remote_gas_test_contract(Config) ->
     call_get(APub, APriv, EncC2Pub, Contract, 1),
     force_fun_calls(Node),
     Balance2 = get_balance(APub),
-    ?assertMatchVM(1610855, 1600391, (Balance1 - Balance2) div ?DEFAULT_GAS_PRICE),
+    ?assertMatchVM(1610855, 1610855, 1610855, 1611335, 1600271, (Balance1 - Balance2) div ?DEFAULT_GAS_PRICE),
 
     %% Test remote call with limited gas (3) that fails (out of gas).
     [] = call_func(APub, APriv, EncC1Pub, Contract, "call", [EncC2Pub, "2", "3"], error),
