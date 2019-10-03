0d489f4a3fbffeafe8cd24b6c9cf1945a724cfeb
Thomas Arts <thomas.arts@robuster.software>
2019-10-03 16:53:15 +0200

    HF on testnet a bit earlier

--- apps/aecore/src/aec_hard_forks.erl
+++ apps/aecore/src/aec_hard_forks.erl
@@ -80,7 +80,7 @@ protocols_from_network_id(<<"ae_uat">>) ->
     #{ ?ROMA_PROTOCOL_VSN     => 0
      , ?MINERVA_PROTOCOL_VSN  => 40900
      , ?FORTUNA_PROTOCOL_VSN => 82900
-     , ?LIMA_PROTOCOL_VSN =>  154460
+     , ?LIMA_PROTOCOL_VSN =>  154300
      };
 protocols_from_network_id(<<"local_roma_testnet">>) ->
     #{ ?ROMA_PROTOCOL_VSN     => 0
--- docs/release-notes/next-lima/PT-168425449-lima-heights.md
+++ docs/release-notes/next-lima/PT-168425449-lima-heights.md
@@ -1,2 +1,2 @@
-* Set Lima testnet hardfork height to 154460 (16th Oct 2019, 12:00 UTC)
+* Set Lima testnet hardfork height to 154300 (16th Oct 2019, 09:00 UTC)
 * Set Lima mainnet to 161150 (30th Oct 2019, 13:00 UTC)
