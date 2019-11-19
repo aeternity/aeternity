a0be0c45f4810e117d030b56fc53da289b5d8863
skkw <skkw@users.noreply.github.com>
2019-11-19 12:10:01 +0100

    fixed version check

--- apps/aecontract/src/aect_contracts.erl
+++ apps/aecontract/src/aect_contracts.erl
@@ -168,7 +168,7 @@ is_legal_version_at_protocol_(create, #{vm := ?VM_FATE_SOPHIA_1, abi := ?ABI_FAT
         ?MINERVA_PROTOCOL_VSN         -> false;
         ?FORTUNA_PROTOCOL_VSN         -> false;
         ?LIMA_PROTOCOL_VSN            -> true;
-        P when P > ?LIMA_PROTOCOL_VSN -> true
+        P when P > ?LIMA_PROTOCOL_VSN -> false
     end;
 is_legal_version_at_protocol_(create, #{vm := ?VM_FATE_SOPHIA_2, abi := ?ABI_FATE_SOPHIA_1}, Protocol) ->
     case Protocol of
