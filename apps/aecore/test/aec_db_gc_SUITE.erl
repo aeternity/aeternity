aa20064c5e182b275ccab25cd465e808244e5128
skkw <skkw@users.noreply.github.com>
2019-12-16 18:28:56 +0100

    rewritten common test

--- apps/aecore/test/aec_db_gc_SUITE.erl
+++ apps/aecore/test/aec_db_gc_SUITE.erl
@@ -7,21 +7,15 @@
          init_per_testcase/2, end_per_testcase/2]).
 
 %% test case exports
--export([write_unreachable_nodes/1,
-         create_accounts/1,
-         start_2nd_node_and_mine/1,
-         check_1st_node_gced/1,
-         restart_1st/1,
-         check_accounts/1,
-         mine_again_1st_node/1,
-         mine_again_2nd_node/1
-        ]).
+-export([main_test/1]).
 
 -include_lib("common_test/include/ct.hrl").
 
 -define(MAX_MINED_BLOCKS, 20).
 -define(NUM_ACCOUNTS, 20).
 -define(NUM_GCED_NODES, 20).
+-define(DUMMY_HASH, <<0:256>>).
+-define(GC_INTERVAL, 50).
 
 all() ->
     [{group, all_nodes}].
@@ -29,18 +23,7 @@ all() ->
 groups() ->
     [{all_nodes, [sequence], [{group, two_nodes}]},
      {two_nodes, [sequence],
-      [write_unreachable_nodes,
-       create_accounts,
-       start_2nd_node_and_mine,
-       check_1st_node_gced,
-       restart_1st,
-       check_accounts,
-       mine_again_1st_node,
-       write_unreachable_nodes,
-       mine_again_2nd_node,
-       check_1st_node_gced,
-       restart_1st,
-       check_accounts]}].
+      [main_test]}].
 
 suite() ->
     [].
@@ -52,31 +35,48 @@ init_per_suite(Config0) ->
                    #{<<"persist">> => true,
                      <<"hard_forks">> => Forks,
                      <<"garbage_collection">> => #{<<"enabled">> => true,
-                                                   <<"interval">> => 51,
-                                                   <<"history">> => 50}},
+                                                   <<"interval">> => ?GC_INTERVAL,
+                                                   <<"history">> => ?GC_INTERVAL + 10}},
                <<"sync">> => #{<<"single_outbound_per_group">> => false},
                <<"mempool">> => #{<<"tx_ttl">> => 100},
                <<"mining">> => #{<<"micro_block_cycle">> => 100}},
     Accounts = [new_pubkey() || _ <- lists:seq(1, ?NUM_ACCOUNTS)],
-    GCedNodes = [crypto:hash(sha256, crypto:strong_rand_bytes(32)) ||
-                    _ <- lists:seq(1, ?NUM_GCED_NODES)],
-    Config = aecore_suite_utils:init_per_suite([dev1, dev2], DefCfg,
-                                               [{add_peers, true}],
-                                               [{symlink_name, "latest.sync"},
-                                                {test_module, ?MODULE},
-                                                {accounts, Accounts},
-                                                {gced_nodes, GCedNodes}] ++ Config1),
-    aecore_suite_utils:start_node(dev1, Config),
+    Config2 = aecore_suite_utils:init_per_suite([dev1, dev2], DefCfg,
+                                                [{add_peers, true}],
+                                                [{symlink_name, "latest.sync"},
+                                                 {test_module, ?MODULE},
+                                                 {accounts, Accounts} | Config1]),
+    aecore_suite_utils:start_node(dev1, Config2),
     aecore_suite_utils:connect(aecore_suite_utils:node_name(dev1)),
-    ok = aecore_suite_utils:check_for_logs([dev1], Config),
-    Config.
+    ok = aecore_suite_utils:check_for_logs([dev1], Config2),
+    [N1, N2] = [aecore_suite_utils:node_name(X) || X <- [dev1, dev2]],
+
+    node_log_details(N1, up),
+
+    Fee   = 1500000 * aec_test_utils:min_gas_price(),
+    lists:foreach(
+      fun ({Nonce, PK}) ->
+              {ok, Tx} = add_spend_tx(N1, 10, Fee, Nonce, 100, PK),
+              aecore_suite_utils:mine_blocks_until_txs_on_chain(N1, [Tx], ?MAX_MINED_BLOCKS)
+      end, lists:zip(lists:seq(1, length(Accounts)), Accounts)),
+    check_accounts(Config2),
+
+    node_log_details(N1, accounts_on_chain),
+
+    ok = rpc:call(N1, mnesia, dirty_write,
+                  [{aec_account_state, ?DUMMY_HASH, lists:duplicate(17, <<>>)}]),
+
+    aecore_suite_utils:start_node(dev2, Config2),
+    aecore_suite_utils:connect(N2),
+    ok = aecore_suite_utils:check_for_logs([dev2], Config2),
+    Config2.
 
 end_per_suite(Config) ->
-    [begin
-         {ok, DbCfg} = node_db_cfg(X),
-         aecore_suite_utils:stop_node(X, Config),
-         aecore_suite_utils:delete_node_db_if_persisted(DbCfg)
-     end || X <- [dev1, dev2]].
+    [catch begin
+               {ok, DbCfg} = node_db_cfg(X),
+               aecore_suite_utils:stop_node(X, Config),
+               aecore_suite_utils:delete_node_db_if_persisted(DbCfg)
+           end || X <- [dev1, dev2]].
 
 init_per_group(two_nodes, Config) ->
     Config1 = config(Config),
@@ -110,54 +110,107 @@ end_per_testcase(_Case, Config) ->
 %% Test cases
 %% ============================================================
 
-write_unreachable_nodes(Config) ->
-    Nodes = ?config(gced_nodes, Config),
-    NVal  = lists:duplicate(17, <<>>),
-    N1    = aecore_suite_utils:node_name(dev1),
-    lists:foreach(
-      fun (NodeKey) ->
-              Node = {aec_account_state, NodeKey, NVal},
-              ok = rpc:call(N1, mnesia, dirty_write, [Node])
-      end, Nodes).
-
-create_accounts(Config) ->
-    PKs = ?config(accounts, Config),
-    N1  = aecore_suite_utils:node_name(dev1),
-    Fee = 1500000 * aec_test_utils:min_gas_price(),
-    lists:foreach(
-      fun ({Nonce, PK}) ->
-              {ok, Tx} = add_spend_tx(N1, 10, Fee, Nonce, 100, PK),
-              aecore_suite_utils:mine_blocks_until_txs_on_chain(N1, [Tx], ?MAX_MINED_BLOCKS)
-      end, lists:zip(lists:seq(1, length(PKs)), PKs)),
-    check_accounts(Config),
-    ok.
-
-start_2nd_node_and_mine(Config) ->
+main_test(_Config) ->
     [N1, N2] = [aecore_suite_utils:node_name(X) || X <- [dev1, dev2]],
-    aecore_suite_utils:start_node(dev2, Config),
-    aecore_suite_utils:connect(aecore_suite_utils:node_name(dev2)),
-    ok = aecore_suite_utils:check_for_logs([dev2], Config),
-    H  = aec_headers:height(rpc:call(N1, aec_chain, top_header, [])),
-    aecore_suite_utils:mine_key_blocks(N2, H + 9),
+    H1 = aec_headers:height(rpc:call(N1, aec_chain, top_header, [])),
+    true = H1 < ?GC_INTERVAL,
+    true = has_key(N1, ?DUMMY_HASH, aec_account_state),
+
+    %% we mine just enough to start first GC phase - collection of reachable hashes
+    aecore_suite_utils:mine_key_blocks(N2, ?GC_INTERVAL - H1),
+    node_log_details(N1, after_mining_0),
+
+    block_while(fun () ->
+                        case gc_state(N1) of
+                            {ready, Data} ->
+                                HashesTab = element(size(Data), Data),
+                                true = is_reference(HashesTab),
+                                self() ! {n1_hashes, rpc:call(N1, ets, tab2list, [HashesTab])},
+                                false;
+                            X ->
+                                ct:log("////////// GC STATE = ~p~n", [X]),
+                                true
+                        end
+                end, 20, 500),
+
+    GCedHashes = receive {n1_hashes, Xs} -> Xs end,
+
+    ct:log("////////// HASHES count = ~p~n", [length(GCedHashes)]),
+    node_log_details(N1, gc_ready),
+
+    monitor_node(N1, true),
+    %% Mining of another keyblock starts second GC phase - storing cache to mnesia table and restart
+    aecore_suite_utils:mine_key_blocks(N2, 1),
+    receive {nodedown, N1} -> ct:log("////////// ~p restarted~n", [N1]) end,
+
+    block_while(fun () -> not net_kernel:hidden_connect_node(N1) end, 30, 3000),
+
+    block_while(fun () -> has_table(N1, aec_account_state_gced) end, 50, 1000),
+
+    false = has_table(N1, aec_account_state_gced),
+    false = has_key(N1, ?DUMMY_HASH, aec_account_state),
+
+    Hashes = rpc:call(N1, mnesia, dirty_select, [aec_account_state, [{{'_','$1','$2'},[],[{{'$1','$2'}}]}]]),
+    [] = GCedHashes -- Hashes,
+
     ok.
 
-check_1st_node_gced(Config) ->
-    N1   = aecore_suite_utils:node_name(dev1),
-    true = lists:all(
-             fun (NodeKey) ->
-                     Ns = rpc:call(N1, mnesia, dirty_read, [aec_account_state, NodeKey]),
-                     length(Ns) == 0
-             end, ?config(gced_nodes, Config)),
+
+%% ==================================================
+%% Private functions
+%% ==================================================
+
+%% log(Fmt, Args) ->
+%%     file:write_file("/tmp/test.log", io_lib:format(Fmt, Args), [append]).
+
+
+gc_state(N) ->
+    rpc:call(N, sys, get_state, [aec_db_gc]).
+
+%% process_exists(N, ProcName) ->
+%%     is_pid(catch rpc:call(N, erlang, whereis, [ProcName])).
+
+%% mnesia_dir(N) ->
+%%     mnesia_system_info(N, directory).
+
+mnesia_system_info(N, X) ->
+    rpc:call(N, mnesia, system_info, [X]).
+
+%% uniq_key(N, NoCollideTab) ->
+%%     H = crypto:hash(sha256, integer_to_binary(erlang:system_time())),
+%%     case has_key(N, H, NoCollideTab) of
+%%         true  -> uniq_key(N, NoCollideTab);
+%%         false -> H
+%%     end.
+
+has_key(N, Key, Tab) ->
+    case rpc:call(N, mnesia, dirty_read, [Tab, Key]) of
+        [_|_] -> true;
+        _     -> false
+    end.
+
+has_table(N, Tab) ->
+    Ts = mnesia_system_info(N, tables),
+    is_list(Ts) andalso lists:member(Tab, Ts).
+
+node_log_details(N, Prefix) ->
+    ct:log("////////// ~p | ~p S = ~p~n", [N, Prefix, catch rpc:call(N, sys, get_state, [aec_db_gc])]),
+    ct:log("////////// ~p | ~p H = ~p~n", [N, Prefix, catch rpc:call(N, aec_chain, top_header, [])]),
     ok.
 
-restart_1st(Config) ->
-    {ok, DbCfg} = node_db_cfg(dev1),
-    aecore_suite_utils:stop_node(dev1, Config),
-    aecore_suite_utils:start_node(dev1, Config),
-    N = aecore_suite_utils:node_name(dev1),
-    aecore_suite_utils:connect(N),
-    ct:log("~w restarted", [dev1]),
-    ok.
+
+block_while(Test, Repeats, MilliSecs) ->
+    block_while_(0, Test, Repeats, MilliSecs).
+
+block_while_(X, _Test, Repeats, _MilliSecs) when is_integer(Repeats), X > Repeats ->
+    {exhausted, Repeats};
+block_while_(X, Test, Repeats, MilliSecs) ->
+    case Test() of
+        false -> ok;
+        true  ->
+            timer:sleep(MilliSecs),
+            block_while_(X + 1, Test, Repeats, MilliSecs)
+    end.
 
 check_accounts(Config) ->
     N1 = aecore_suite_utils:node_name(dev1),
@@ -167,23 +220,6 @@ check_accounts(Config) ->
       end, ?config(accounts, Config)),
     ok.
 
-mine_again_1st_node(_Config) ->
-    N = aecore_suite_utils:node_name(dev1),
-    H = aec_headers:height(rpc:call(N, aec_chain, top_header, [])),
-    aecore_suite_utils:mine_key_blocks(N, H + 1),
-    ok.
-
-mine_again_2nd_node(_Config) ->
-    [N1, N2] = [aecore_suite_utils:node_name(X) || X <- [dev1, dev2]],
-    H = aec_headers:height(rpc:call(N1, aec_chain, top_header, [])),
-    %% aecore_suite_utils:mine_key_blocks(N1, H + 1),
-    aecore_suite_utils:mine_key_blocks(N2, H + 2),
-    ok.
-
-
-%% ==================================================
-%% Private functions
-%% ==================================================
 
 config(Config) ->
     [{devs, [dev1]}, {nodes, [aecore_suite_utils:node_tuple(dev1)]} | Config].
