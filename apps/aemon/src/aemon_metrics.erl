5fb4c50f4a6afb999d65beb081331e3da1181d4f
Tino Breddin <t@tolbrino.com>
2019-12-17 15:57:41 +0100

    Adds new metrics to ae.epoch.aemon namespace
    - `block.propagation_time.key`   : Time until key-blocks reached this node in milliseconds
    - `block.propagation_time.micro` : Time until micro-blocks reached this node in milliseconds
    - `block.time_since_prev.key`    : Time between key-blocks in milliseconds
    - `block.time_since_prev.micro`  : Time between micro-blocks in milliseconds
    - `chain.top.difficulty`         : Difficulty of the top block

--- apps/aemon/src/aemon_metrics.erl
+++ apps/aemon/src/aemon_metrics.erl
@@ -1,64 +1,37 @@
+%% @doc This module provides helper functions which mask exometer and
+%% aec_metric calls for the user.
 -module(aemon_metrics).
 
+%% generic API
 -export([create/1]).
 
+%% specific metrics
 -export([ fork_micro/0
         , confirmation_delay/1
-        ]).
--export([ gen_stats_tx/1
+        , gen_stats_tx/1
         , gen_stats_tx_monitoring/1
         , gen_stats_microblocks/1
-        ]).
--export([ publisher_balance/1
+        , publisher_balance/1
         , publisher_post_tx/1
-        ]).
--export([ queue_size/1
+        , queue_size/1
         , ttl_expired/1
+        , block_propagation_time/2
+        , block_time_since_prev/2
+        , chain_top_difficulty/1
         ]).
 
-%% on chain forks & confirmation delay
+%% ==================================================================
+%% generic API
 
-fork_micro() ->
-    update([fork, micro], 1).
-
-confirmation_delay(Count) ->
-    update([confirmation, delay], Count).
-
-%% generation stats
-
-gen_stats_tx(Count) ->
-    update([gen_stats, tx, total], Count).
-
-gen_stats_tx_monitoring(Count) ->
-    update([gen_stats, tx, monitoring], Count).
-
-gen_stats_microblocks(Count) ->
-    update([gen_stats, microblocks, total], Count).
-
-
-%% publisher
-
-publisher_balance(Error) when is_atom(Error) ->
-    update([publisher, balance, Error], 1);
-publisher_balance(Balance) ->
-    update([publisher, balance], Balance).
-
-publisher_post_tx(Action) ->
-    update([publisher, post_tx, Action], 1).
-
-%% ttl
-
-queue_size(Count) ->
-    update([publisher, queue, size], Count).
-
-ttl_expired(Count) ->
-    update([publisher, queue, ttl_expired], Count).
-
-%% on start metric creation
-
+%% @doc Creates a set of metrics based on the given group identifier.
 create(on_chain) ->
     create([forks, micro], counter),
     create([confirmation, delay], histogram),
+    create([block, propagation_time, key], histogram),
+    create([block, propagation_time, micro], histogram),
+    create([block, time_since_prev, key], histogram),
+    create([block, time_since_prev, micro], histogram),
+    create([chain, top, difficulty], gauge),
     ok;
 create(gen_stats) ->
     create([gen_stats, tx, total], histogram),
@@ -77,11 +50,58 @@ create(ttl) ->
     create([publisher, queue, ttl_expired], histogram),
     ok.
 
-%% internals
+%% ==================================================================
+%% specific metrics
+
+fork_micro() ->
+    update([fork, micro], 1).
+
+confirmation_delay(Count) ->
+    update([confirmation, delay], Count).
+
+gen_stats_tx(Count) ->
+    update([gen_stats, tx, total], Count).
+
+gen_stats_tx_monitoring(Count) ->
+    update([gen_stats, tx, monitoring], Count).
+
+gen_stats_microblocks(Count) ->
+    update([gen_stats, microblocks, total], Count).
+
+publisher_balance(Error) when is_atom(Error) ->
+    update([publisher, balance, Error], 1);
+publisher_balance(Balance) ->
+    update([publisher, balance], Balance).
+
+publisher_post_tx(Action) ->
+    update([publisher, post_tx, Action], 1).
+
+queue_size(Count) ->
+    update([publisher, queue, size], Count).
+
+ttl_expired(Count) ->
+    update([publisher, queue, ttl_expired], Count).
+
+block_propagation_time(Type, Time) when Time < 1 ->
+    lager:debug("Ignoring metric update of block_propagation_time.~p, Time = ~p", [Type, Time]),
+    ok;
+block_propagation_time(Type, Time) when Type =:= key; Type =:= micro ->
+    update([block, propagation_time, Type], Time).
+
+block_time_since_prev(Type, Time) when Time < 1 ->
+    lager:debug("Ignoring metric update of block_time_since_prev.~p, Time = ~p", [Type, Time]),
+    ok;
+block_time_since_prev(Type, Time) when Type =:= key; Type =:= micro ->
+    update([block, time_since_prev, Type], Time).
+
+chain_top_difficulty(Difficulty) ->
+    update([chain, top, difficulty], Difficulty).
+
+%% ==================================================================
+%% internal functions
 
 create(Name, Type) ->
-    exometer:ensure(metric(Name), Type, []),
-    ok.
+    ok = exometer:ensure(metric(Name), Type, []).
 
 update(Name, Value) ->
     aec_metrics:try_update(metric(Name), Value).
--- apps/aemon/src/aemon_sup.erl
+++ apps/aemon/src/aemon_sup.erl
@@ -1,28 +1,38 @@
 -module(aemon_sup).
+
 -behaviour(supervisor).
 
+%% API
 -export([start_link/0]).
--export([init/1]).
 
+%% supervisor callbacks
+-export([init/1]).
 
 -define(SERVER, ?MODULE).
 -define(CHILD(Mod,N,Type), {Mod,{Mod,start_link,[]},permanent,N,Type,[Mod]}).
 
+%% ==================================================================
+%% API
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+%% ==================================================================
+%% supervisor callbacks
+
 init(_) ->
-    ChildSpecs = case aemon_config:is_active() of
-        false -> [];
-        true -> [
-                 ?CHILD(aemon_mon_ttl, 5000, worker),
-                 ?CHILD(aemon_mon_on_chain, 5000, worker),
-                 ?CHILD(aemon_mon_gen_stats, 5000, worker),
-                 ?CHILD(aemon_mon, 5000, worker),
-
-                 ?CHILD(aemon_publisher, 5000, worker)
-                ]
-    end,
-
+    ChildSpecs = child_specs(aemon_config:is_active()),
     {ok, {{one_for_one, 5, 10}, ChildSpecs}}.
+
+%% ==================================================================
+%% internal functions
+
+child_specs(false) ->
+    [];
+child_specs(true) ->
+    [ ?CHILD(aemon_mon_ttl, 5000, worker)
+    , ?CHILD(aemon_mon_on_chain, 5000, worker)
+    , ?CHILD(aemon_mon_gen_stats, 5000, worker)
+    , ?CHILD(aemon_mon, 5000, worker)
+    , ?CHILD(aemon_publisher, 5000, worker)
+    ].
