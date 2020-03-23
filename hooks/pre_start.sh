e1b6e5fa6be9da503e38b28e2715544167d12d58
Tino Breddin <t@tolbrino.com>
2020-03-23 12:43:09 +0100

    Add additional link to wiki for ulimit issues

--- hooks/pre_start.sh
+++ hooks/pre_start.sh
@@ -5,5 +5,6 @@ OPEN_FILES_LIMIT=$(ulimit -n)
 
 if [ "$OPEN_FILES_RECOMMENDED" -gt "$OPEN_FILES_LIMIT" ]; then
     echo "WARNING: ulimit -n is $OPEN_FILES_LIMIT; $OPEN_FILES_RECOMMENDED is the recommended minimum."
-    echo "You are recommended to ensure the node is stopped and raise the maximum number of open files (try 'ulimit -n $OPEN_FILES_RECOMMENDED') before starting the node."
+    echo "You are recommended to ensure the node is stopped and raise the maximum number of open files before starting the node."
+    echo "Try 'ulimit -n $OPEN_FILES_RECOMMENDED' or visit https://github.com/aeternity/aeternity/wiki/Setting-Number-of-Max-Open-Files for additional pointers."
 fi
