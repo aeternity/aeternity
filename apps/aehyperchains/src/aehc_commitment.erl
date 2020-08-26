ca1334d519d42c4800e4fd6406328c1b5bd16e80
Grzegorz Uriasz <gorbak25@gmail.com>
2020-08-26 16:49:40 +0200

    Hyperchains app and initial db (#3338)
    * HC App
    
    * Initial HC DB design

--- apps/aehyperchains/src/aehc_commitment.erl
+++ apps/aehyperchains/src/aehc_commitment.erl
@@ -1,12 +1,6 @@
 -module(aehc_commitment).
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 -export([]).
 
 %%-record()
-=======
->>>>>>> 8238dfe9... Config structure for Hyperchains; Module names; (#3330)
-=======
 -export([]).
->>>>>>> ec818405... Intial run Hyperchains application;
