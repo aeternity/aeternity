86fed35205b793854fbe479673eb5f45fe3b71fa
Hans Svensson <hans.svensson@happihacking.se>
2021-01-05 08:43:23 +0100

    Limit AENS pointers from IRIS onwards
    - no duplicate keys on chain
    - not more than 32 pointers per name
    - pointer key shouldn't be more than 256 bytes.
    
    From IRIS we should not allow duplicate keys, but for legacy names we
    should make sure to use the first key.

--- apps/aens/src/aens_pointer.erl
+++ apps/aens/src/aens_pointer.erl
@@ -3,6 +3,7 @@
 -export([new/2,
          key/1,
          id/1,
+         sanitize_pointers/1,
          serialize_for_client/1
         ]).
 
@@ -20,6 +21,8 @@
               id/0,
               pointer/0
              ]).
+
+-include_lib("aecontract/include/hard_forks.hrl").
 
 -spec new(key(), id()) -> pointer().
 new(Key, Id) ->
@@ -52,3 +55,26 @@ assert_id_type(channel)  -> ok;
 assert_id_type(contract) -> ok;
 assert_id_type(oracle)   -> ok.
 
+
+%% From IRIS pointers should be:
+%%  - unique (no overlapping keys)
+%%  - not more than 32
+%%  - keys should be no bigger than 256 bytes.
+-spec sanitize_pointers(list(pointer())) -> list(pointer()).
+sanitize_pointers(Pts) ->
+    MaxCount   = aec_governance:name_pointers_max_count(?IRIS_PROTOCOL_VSN),
+    MaxKeySize = aec_governance:name_pointer_max_key_size(?IRIS_PROTOCOL_VSN),
+    sanitize_pointers(Pts, #{}, 0, MaxCount, MaxKeySize).
+
+sanitize_pointers([], _, _, _, _) -> [];
+sanitize_pointers(_, _, N, N, _) -> [];
+sanitize_pointers([Pt | Pts], Seen, N, MaxCount, MaxKeySize) ->
+    Key = key(Pt),
+    case maps:is_key(Key, Seen) of
+        true -> sanitize_pointers(Pts, Seen, N, MaxCount, MaxKeySize);
+        false ->
+            case byte_size(Key) > MaxKeySize of
+                true -> sanitize_pointers(Pts, Seen, N, MaxCount, MaxKeySize);
+                false -> [Pt | sanitize_pointers(Pts, Seen#{Key => ok}, N + 1, MaxCount, MaxKeySize)]
+            end
+    end.
