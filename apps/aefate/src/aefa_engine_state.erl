7ef80fb03b2ff5b7a25bf1f52f182e9a6e278098
Hans Svensson <hanssv@gmail.com>
2021-04-23 16:07:41 +0200

    Don't loop in spend_gas_for_new_cells

--- apps/aefate/src/aefa_engine_state.erl
+++ apps/aefate/src/aefa_engine_state.erl
@@ -468,19 +468,22 @@ spend_gas(X, #es{gas = Gas} = ES) ->
 
 %% The gas price per cell increases by 1 for each kibiword (each 1024 64-bit word) used.
 -spec spend_gas_for_new_cells(integer(), state()) -> state().
-spend_gas_for_new_cells(NewCells, ES) when NewCells =< 0 ->
+spend_gas_for_new_cells(NewCells, ES) when NewCells < 0 ->
     case consensus_version(ES) of
-        P when P < ?IRIS_PROTOCOL_VSN  -> spend_gas_for_new_cells(NewCells, ES);
-        P when P >= ?IRIS_PROTOCOL_VSN -> spend_gas_for_new_cells(abs(NewCells) + 2, ES)
+        P when P < ?IRIS_PROTOCOL_VSN  -> spend_gas_for_new_cells1(NewCells, ES);
+        P when P >= ?IRIS_PROTOCOL_VSN -> spend_gas_for_new_cells1(abs(NewCells) + 2, ES)
     end;
-spend_gas_for_new_cells(NewCells, #es{ created_cells = Cells } = ES) when NewCells + Cells =< 1024 ->
+spend_gas_for_new_cells(NewCells, ES) ->
+    spend_gas_for_new_cells1(NewCells, ES).
+
+spend_gas_for_new_cells1(NewCells, #es{ created_cells = Cells } = ES) when NewCells + Cells =< 1024 ->
     TotalCells = Cells + NewCells,
     spend_gas(NewCells, ES#es{ created_cells = TotalCells });
-spend_gas_for_new_cells(1, #es{ created_cells = Cells } = ES) ->
+spend_gas_for_new_cells1(1, #es{ created_cells = Cells } = ES) ->
     TotalCells = Cells + 1,
     CellCost = 1 + (TotalCells bsr 10),
     spend_gas(CellCost, ES#es{ created_cells = TotalCells });
-spend_gas_for_new_cells(NewCells, #es{ created_cells = Cells } = ES) ->
+spend_gas_for_new_cells1(NewCells, #es{ created_cells = Cells } = ES) ->
     TotalCells = Cells + NewCells,
     CellCost = 1 + (TotalCells bsr 10),
     spend_gas(NewCells * CellCost, ES#es{ created_cells = TotalCells }).
