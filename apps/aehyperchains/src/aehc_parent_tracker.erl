5f07a5a473ba73cc9f1001a6574c403ef51640af
erlmachinedev <erlmachinedev@gmail.com>
2021-05-06 15:21:48 +0300

    Payload mock has removed

--- apps/aehyperchains/src/aehc_parent_tracker.erl
+++ apps/aehyperchains/src/aehc_parent_tracker.erl
@@ -304,9 +304,9 @@ commit_state(Data) ->
 commitment(Tx) ->
     Account = aeconnector_tx:account(Tx), %% TODO Place to substitute delegate via trees;
     Payload = aeconnector_tx:payload(Tx),
-%%    KeyblockHash = aeser_api_encoder:decode(Payload),
+    KeyblockHash = aeser_api_encoder:decode(Payload),
 
-    Header = aehc_commitment_header:new(Account, <<"KeyblockHash">>),
+    Header = aehc_commitment_header:new(Account, KeyblockHash),
     aehc_commitment:new(Header).
 
 -spec is_commitment(tx()) -> boolean().
