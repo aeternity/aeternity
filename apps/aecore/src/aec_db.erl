258154d7fc5799b50c5f400bf0d3413d3bafb41f
erlmachinedev <erlmachinedev@gmail.com>
2021-05-11 23:52:09 +0300

    Add hooks debug (should be removed)

--- apps/aecore/src/aec_db.erl
+++ apps/aecore/src/aec_db.erl
@@ -1162,6 +1162,7 @@ expand_mode(disc) -> disc_backend_mode();
 expand_mode(M) when is_map(M) -> M.
 
 run_hooks(Hook, Mode) ->
+    lager:info("~nHooks list is: ~p~n",[setup:find_env_vars(Hook)]),
     [M:F(Mode) || {_App, {M,F}} <- setup:find_env_vars(Hook)].
 
 fold_hooks(Hook, Acc0) ->
