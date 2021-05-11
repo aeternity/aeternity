ba873b1c1176bd027ef3659f6e927c0f29126bc3
erlmachinedev <erlmachinedev@gmail.com>
2021-05-11 22:48:22 +0300

    Database denug (should be removed later)

--- apps/aehyperchains/src/aehc_db.erl
+++ apps/aehyperchains/src/aehc_db.erl
@@ -9,10 +9,16 @@
 create_tables(Mode) ->
     case aehc_utils:hc_enabled() of
         true ->
-            AllSpecs = all_specs(Mode),
+            lager:info("~nHC is enabled ~p~n",[?LINE]),
+            AllSpecs = all_specs(Mode), lager:debug("~nAllSpecs: ~p~n",[AllSpecs]),
             Specs = lists:flatten([proplists:lookup(Table, AllSpecs) || {missing_table, Table} <- check_tables([])]),
+
+            lager:info("~nSpecs: ~p~n",[Specs]),
+            lager:info("~ncheck_tables/1: ~p~n",[check_tables([])]),
+
             [{atomic, ok} = mnesia:create_table(Tab, Spec) || {Tab, Spec} <- Specs];
         false ->
+            lager:info("~nHC is disabled ~p~n",[?LINE]),
             []
     end.
 
