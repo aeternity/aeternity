378f66d768c7290130f263e3f194069521876b79
Craig Everett <zxq9@zxq9.com>
2021-09-17 12:47:33 +0900

    Remove branch commit instruction for build_nix

--- webhook.nix
+++ webhook.nix
@@ -1,9 +1,5 @@
 let
-  pkgs = import (builtins.fetchGit {
-    url = "https://github.com/NixOS/nixpkgs/";                       
-    ref = "refs/heads/nixos-unstable";                     
-    rev = "860b56be91fb874d48e23a950815969a7b832fbc";                                           
-  }) {};  
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/860b56be91fb874d48e23a950815969a7b832fbc.tar.gz") {};
 in {
   aeternityEnv = pkgs.stdenv.mkDerivation {
     name = "aeternity-webhook";
