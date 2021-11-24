4849bc21c4b5a1e14a05a630593185e5aa3c8b02
Sean Hinde <sean.hinde@mac.com>
2021-11-24 18:02:00 +0100

    Use slightly less new aesophia which is still compatible with tests

--- apps/aefate/test/aefa_sophia_test.erl
+++ apps/aefate/test/aefa_sophia_test.erl
@@ -95,7 +95,7 @@ compile_contract(Code) ->
 compile_contract(Code, Options) ->
     try
         Ast       = aeso_parser:string(Code, Options),
-        {_, TypedAst, _Warnings = []}  = aeso_ast_infer_types:infer(Ast, Options),
+        {_, TypedAst}  = aeso_ast_infer_types:infer(Ast, Options),
         {#{child_con_env := ChildContracts}, FCode}
                        = aeso_ast_to_fcode:ast_to_fcode(TypedAst, Options),
         Fate      = aeso_fcode_to_fate:compile(ChildContracts, FCode, Options),
