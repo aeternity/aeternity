51f1504d7107a1cb57868ae9baa26a639eb9cb92
Chandru Mullaparthi <chandru.mullaparthi@bluehouse-technology.co.uk>
2022-02-14 09:52:15 +0000

    WIP

---
+++ apps/aeapi/src/aeapi.erl
@@ -0,0 +1,98 @@
+-module(aeapi).
+
+-export([
+         blockchain_name/0
+        , network_id/0
+        , node_revision/0
+        , node_version/0
+        , sync_progress/0
+        , connected_peers/0
+
+        , block_height/1
+        , current_block/0
+        , block_time_in_msecs/1
+        , block_txs/1
+        , key_block_by_height/1
+        , key_block_by_hash/1
+        , prev_block/1
+        , printable_block_hash/1
+        , top_key_block/0
+        ]).
+
+blockchain_name() ->
+    <<"aeternity">>. %% TODO: check hardcoding
+
+network_id() ->
+    aec_governance:get_network_id().
+
+node_revision() ->
+    aeu_info:get_revision().
+
+node_version() ->
+    aeu_info:get_version().
+
+top_key_block() ->
+    aec_chain:top_key_block().
+
+sync_progress() ->
+    aec_sync:sync_progress().
+
+connected_peers() ->
+    aec_peers:connected_peers().
+
+current_block() ->
+    case aec_chain:top_key_block() of
+        {ok, Block} ->
+            Block;
+        error ->
+            throw(block_not_found)
+    end.
+
+prev_block(Block) ->
+    PrevBlockHash = aec_blocks:prev_key_hash(Block),
+    case aec_chain:get_block(PrevBlockHash) of
+        {ok, PrevBlock} ->
+            PrevBlock;
+        error ->
+            throw(block_not_found)
+    end.
+
+block_height(Block) ->
+    aec_blocks:height(Block).
+
+block_time_in_msecs(Block) ->
+    aec_blocks:time_in_msecs(Block).
+
+block_txs(Block) ->
+    block_txs(aec_blocks:is_key_block(Block), Block).
+
+block_txs(true, Block) ->
+    {ok, BlockHash} = aec_headers:hash_header(aec_blocks:to_key_header(Block)),
+    case aec_chain:get_generation_by_hash(BlockHash, forward) of
+        {ok, #{micro_blocks := MicroBlocks}} ->
+            Txs = lists:foldl(
+              fun(MicBlock, Acc) ->
+                      [aec_blocks:txs(MicBlock) | Acc]
+              end, [], MicroBlocks),
+            lists:flatten(lists:reverse(Txs));
+        error ->
+            error
+    end;
+block_txs(false, Block) ->
+    aec_blocks:txs(Block).
+
+printable_block_hash(Block) ->
+    {ok, Hash} = aec_headers:hash_header(aec_blocks:to_header(Block)),
+    aeser_api_encoder:encode(key_block_hash, Hash).
+
+-spec key_block_by_height(aec_blocks:height()) -> {ok, aec_blocks:key_block()} | error.
+
+key_block_by_height(Height) when is_integer(Height) ->
+    aec_chain:get_key_block_by_height(Height).
+
+-spec key_block_by_hash(aeser_api_encoder:encoded()) -> {ok, aec_blocks:key_block()} | error.
+
+key_block_by_hash(Hash) when is_binary(Hash) ->
+    {key_block_hash, DecodedHash} = aeser_api_encoder:decode(Hash),
+    aec_chain:get_block(DecodedHash).
+
--- apps/aehttp/src/aehttp_dispatch_rosetta.erl
+++ apps/aehttp/src/aehttp_dispatch_rosetta.erl
@@ -20,6 +20,11 @@
 
 -define(TC(Expr, Msg), begin {Time, Res} = timer:tc(fun() -> Expr end), lager:debug("[~p] Msg = ~p", [Time, Msg]), Res end).
 
+-define(ROSETTA_ERR_NW_STATUS_ERR,   1).
+-define(ROSETTA_ERR_INVALID_NETWORK, 2).
+-define(ROSETTA_ERR_BLOCK_NOT_FOUND, 3).
+-define(ROSETTA_ERR_CHAIN_TOO_SHORT, 4).
+
 -spec forbidden( Mod :: module(), OperationID :: atom() ) -> boolean().
 forbidden(_Mod, _OpId) -> false.
 
@@ -87,28 +92,27 @@ queue(_)                        -> ?WRITE_Q.
 
 handle_request_('networkList', _, _Context) ->
     Resp = #{<<"network_identifiers">> => 
-                 [#{<<"blockchain">> => <<"aeternity">>, %% TODO: check hardcoding
-                    <<"network">> => aec_governance:get_network_id()
+                 [#{<<"blockchain">> => aecapi:get_blockchain_name(),
+                    <<"network">> => aeapi:get_network_id()
                    }
                  ]
             },
     {200, [], Resp};
 handle_request_('networkOptions', _, _Context) ->
     Resp = #{<<"version">> => #{<<"rosetta_version">> => <<"1.4.10">>,
-                                <<"node_version">> => aeu_info:get_version(),
-                                <<"metadata">> => #{<<"node_revision">> => aeu_info:get_revision()}},
+                                <<"node_version">> => aeapi:get_node_version(),
+                                <<"metadata">> => #{<<"node_revision">> => aeapi:get_node_revision()}},
              <<"allow">> => #{} %% TODO
-             },
+            },
     {200, [], Resp};
 handle_request_('networkStatus', _, _Context) ->
     try
-        {ok, Block} = aec_chain:top_key_block(),
-        CurrentBlockIdentifier = #{<<"index">> => aec_blocks:height(Block),
-                                   <<"hash">> => aeser_api_encoder:encode(key_block_hash, aec_blocks:root_hash(Block))},
-        CurrentBlockTimestamp = aec_blocks:time_in_msecs(Block),
-        GenesisBlockIdentifier = #{<<"index">> => aec_block_genesis:height(),
-                                   <<"hash">> => aeser_api_encoder:encode(key_block_hash, aec_consensus:get_genesis_hash())},
-        {Syncing, SyncProgress, TargetHeight} = aec_sync:sync_progress(),
+        {ok, TopBlock} = aeapi:top_key_block(),
+        CurrentBlock = aeapi:prev_block(TopBlock),
+        CurrentBlockIdentifier = format_block_identifier(CurrentBlock),
+        CurrentBlockTimestamp = aec_blocks:time_in_msecs(CurrentBlock),
+        GenesisBlockIdentifier = format_block_identifier(aec_chain:genesis_block()),
+        {Syncing, SyncProgress, TargetHeight} = aeapi:sync_progress(),
         Synced = case {Syncing, SyncProgress} of
                      {false, 100.0} -> true;
                      _ -> false
@@ -120,7 +124,7 @@ handle_request_('networkStatus', _, _Context) ->
                          false ->
                              SyncStatus0#{<<"target_index">> => TargetHeight}
                      end,
-        Peers = aec_peers:connected_peers(),
+        Peers = aeapi:connected_peers(),
         PeersFormatted = lists:map(
                            fun(Peer) ->
                                    #{<<"peer_id">> => aeser_api_encoder:encode(peer_pubkey, aec_peer:id(Peer)),
@@ -136,19 +140,55 @@ handle_request_('networkStatus', _, _Context) ->
     catch Class:Rsn:Stacktrace ->
             lager:error(
               ">>> Error determining networkStatus: ~p, ~p~n~p~n", [Class, Rsn, Stacktrace]),
-            ErrResp = #{<<"code">> => 500,
-                        <<"message">> => <<"Error determining networkStatus">>,
-                        <<"retriable">> => true
-                       },
-            {500, [], ErrResp}
+            ErrResp = rosetta_error_response(?ROSETTA_ERR_NW_STATUS_ERR),
+            {200, [], ErrResp}
     end;
 
 handle_request_('accountBalance', _, _Context) ->
     {501, [], #{}};
 handle_request_('accountCoins', _, _Context) ->
     {501, [], #{}};
-handle_request_('block', _, _Context) ->
-    {501, [], #{}};
+
+%% Data API - Block
+handle_request_('block', #{'BlockRequest' := 
+                               #{<<"network_identifier">> := 
+                                     #{<<"blockchain">> := <<"aeternity">>,
+                                       <<"network">> := Network}} = Req}, _Context) ->
+    try
+        case aec_governance:get_network_id() of
+            Network ->
+                ok;
+            _ ->
+                throw(invalid_network)
+        end,
+        Block = case maps:get(<<"block_identifier">>, Req) of
+                    #{<<"index">> := Index} ->
+                        case aeapi:key_block_by_height(Index) of
+                            {ok, Block0} ->
+                                Block0;
+                            {error, Err} ->
+                                throw(Err)
+                        end;
+                    #{<<"hash">> := Hash} ->
+                        case aeapi:key_block_by_hash(Hash) of
+                            error ->
+                                throw(block_not_found);
+                            {ok, Block0} ->
+                                Block0
+                        end;
+                    _ ->
+                        aeapi:prev_block(aeapi:current_block())
+                end,
+        BlockFmt = format_block(Block),
+        Resp = BlockFmt#{<<"metadata">> => #{}},
+        {200, [], Resp}
+    catch throw:invalid_network ->
+            {200, [], rosetta_error_response(?ROSETTA_ERR_INVALID_NETWORK)};
+          throw:block_not_found ->
+            {200, [], rosetta_error_response(?ROSETTA_ERR_BLOCK_NOT_FOUND)};
+          throw:chain_too_short ->
+            {200, [], rosetta_error_response(?ROSETTA_ERR_CHAIN_TOO_SHORT)}
+    end;
 handle_request_('blockTransaction', _, _Context) ->
     {501, [], #{}};
 handle_request_('call', _, _Context) ->
@@ -184,3 +224,45 @@ handle_request_(OperationID, Req, Context) ->
       [{OperationID, Req, Context}]
      ),
     {501, [], #{}}.
+
+-spec format_block(aec_blocks:block()) -> #{}.
+format_block(Block) ->
+    PrevBlock = aeapi:prev_block(Block),
+    #{
+      <<"block_identifier">> => format_block_identifier(Block),
+      <<"parent_block_identifier">> => format_block_identifier(PrevBlock),
+      <<"timestamp">> => aeapi:block_time_in_msecs(Block),
+      <<"transactions">> => format_block_txs(aeapi:block_txs(Block))
+     }.
+
+format_block_identifier(undefined) ->
+    #{<<"index">> => 0,
+      <<"hash">> => <<>>};
+format_block_identifier(Block) ->
+    #{<<"index">> => aeapi:block_height(Block),
+      <<"hash">> => aeapi:printable_block_hash(Block)}.
+
+format_block_txs(_Txs) ->
+    [].
+
+rosetta_error_response(ErrCode) ->
+    rosetta_error_response(ErrCode, true).
+
+rosetta_error_response(ErrCode, Retriable) when is_integer(ErrCode),
+                                                is_boolean(Retriable) ->
+    rosetta_error_response(ErrCode, Retriable, undefined).
+
+rosetta_error_response(ErrCode, Retriable, Details) when is_integer(ErrCode),
+                                                         is_boolean(Retriable) ->
+    Err = #{<<"code">> => ErrCode,
+            <<"message">> => rosetta_err_msg(ErrCode),
+            <<"retriable">> => Retriable
+           },
+    case Details of
+        undefined ->
+            Err;
+        #{} ->
+            Err#{<<"details">> => Details}
+    end.
+
+rosetta_err_msg(?ROSETTA_ERR_NW_STATUS_ERR) -> <<"Error determining networkStatus">>.
