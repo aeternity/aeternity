1c58061a352f1ec9a70525b06ee26980a6c3f821
Sean Hinde <sean.hinde@mac.com>
2022-05-04 19:28:03 +0200

    Base CLI listen socket path on Erlang node name

--- apps/aecli/src/aecli_app.erl
+++ apps/aecli/src/aecli_app.erl
@@ -16,9 +16,18 @@
 
 start(_StartType, _StartArgs) ->
     {ok, Pid} = aecli_sup:start_link(),
-    %% This starts a process under the cli application simple one for one
-    %% Does seem ideal..
-    {ok, _Pid} = ecli:open("/var/tmp/mgmtd.cli.socket", aecli),
+
+    %% Create listen socket path based on node name with callback module aecli
+    %% ecli socket related process will be created under the cli application
+    %% simple one for one sup
+    case os:type() of
+        {win32, _} ->
+            not_supported;
+        _ ->
+            Path = ecli_path(),
+            {ok, _} = ecli:open(Path, aecli),
+            lager:info("Listening for CLI connections at: ~s", [Path])
+    end,
     {ok, Pid}.
 
 %%--------------------------------------------------------------------
@@ -28,3 +37,14 @@ stop(_State) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+ecli_path() ->
+    case node_name() of
+        "nonode" ->
+            "/var/tmp/mgmtd.cli.socket";
+        Node ->
+            "/var/tmp/mgmtd." ++ Node ++ ".cli.socket"
+    end.
+
+node_name() ->
+    [N,_H] = re:split(atom_to_list(node()), "@", [{return,list}]),
+    N.
--- apps/aeutils/priv/extensions/cli.sh
+++ apps/aeutils/priv/extensions/cli.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-./bin/aecli
+node_name="$(echo $NAME | cut -d'@' -f1)"
+
+ECLI_SOCKET_PATH="/var/tmp/mgmtd.${node_name}.cli.socket" $ROOTDIR/bin/cli
