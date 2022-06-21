e128462fac6e8c797e58e19cecd22956e0e3a1d6
Sean Hinde <sean.hinde@mac.com>
2022-06-21 17:36:44 +0200

    Removed old export command and small tidy ups

--- apps/aeutils/priv/extensions/export_chain.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-:: Wrapper to run export_chain
-
-set "PATH=%BINDIR%;%PATH%"
-set "ERL_LIBS=%ROOTDIR%\lib"
-set "APPS_VSN=%REL_VSN%"
-
-"%ERTS_DIR%\bin\escript" "%ROOTDIR%\lib\aeutils-%APPS_VSN%\priv\export_chain" "%NAME_TYPE%" "%NAME%" -setcookie "%COOKIE%" %*
