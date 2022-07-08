66e9fbb8a9768afb9537fedba5f9856e375e3795
Sean Hinde <sean.hinde@mac.com>
2022-07-08 16:25:19 +0200

    No longer copy the ecli binary for prod releases only, run it out of ecli/priv

--- apps/aeutils/priv/extensions/cli.sh
+++ apps/aeutils/priv/extensions/cli.sh
@@ -2,4 +2,4 @@
 
 node_name="$(echo $NAME | cut -d'@' -f1)"
 
-ECLI_SOCKET_PATH="/var/tmp/mgmtd.${node_name}.cli.socket" $ROOTDIR/bin/cli
+ECLI_SOCKET_PATH="/var/tmp/mgmtd.${node_name}.cli.socket" $ROOTDIR/lib/ecli-0.1.0/priv/ecli
