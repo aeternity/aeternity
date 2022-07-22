eea659ccd9f319abb2a2c708c274ac1781f9306b
Fabian Krol <fabian@krol.io>
2022-07-22 12:23:23 +0200

    Add support for setting validator online delay

--- apps/aehttp/test/aehttp_stake_contract_SUITE.erl
+++ apps/aehttp/test/aehttp_stake_contract_SUITE.erl
@@ -104,11 +104,12 @@ init_per_suite(Config0) ->
             MinValidatorAmt = integer_to_list(trunc(math:pow(10,18) * math:pow(10, 6))), %% 1 mln AE
             MinStakeAmt = integer_to_list(trunc(math:pow(10,18) * 1)), %% 1 AE
             MinStakePercent = "30",
+            OnlineDelay = "0",
             #{ <<"pubkey">> := ConsensusContractPubkey
              , <<"owner_pubkey">> := ContractOwner } = C
                 = contract_create_spec(?STAKING_CONTRACT,
                                        [binary_to_list(StakingValidatorContract), "\"domat\"",
-                                        MinValidatorAmt, MinStakePercent, MinStakeAmt], 0, 2, Pubkey),
+                                        MinValidatorAmt, MinStakePercent, MinStakeAmt, OnlineDelay], 0, 2, Pubkey),
             {ok, CCId} = aeser_api_encoder:safe_decode(contract_pubkey,
                                                        ConsensusContractPubkey),
             Call1 =
