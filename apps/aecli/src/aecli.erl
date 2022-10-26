f540f0e1869fb4e0e8c1178eabd6a9621653a268
Sean Hinde <sean.hinde@mac.com>
2022-10-26 16:28:29 +0200

    Remove unfinished cli show block command

--- apps/aecli/src/aecli.erl
+++ apps/aecli/src/aecli.erl
@@ -114,29 +114,8 @@ operational_show_menu() ->
        desc => "Transaction pool commands",
        action => fun(J, _Item, _) -> show_tx_pool_summary(J) end,
        children => fun() -> operational_tx_pool_menu() end
-      },
-     #{role => cmd,
-       node_type => container,
-       name => "block",
-       desc => "Show block",
-       children => fun() -> operational_block_menu() end,
-       action => fun(J, Item, Value) -> show_block(J, Item, Value) end
       }
     ].
-
-operational_block_menu() ->
-    [#{role => schema,
-       node_type => leaf,
-       name => "hash",
-       desc => "Hash of block starting with mh_ or kh_",
-       type => {aecli, tx_hash}
-      },
-     #{role => schema,
-       node_type => leaf,
-       name => "height",
-       desc => "Height of block",
-       type => integer
-      }].
 
 operational_tx_pool_menu() ->
     [#{role => cmd,
@@ -373,14 +352,6 @@ show_tx_summary(J, [#{name := "hash", value := TxHash}]) ->
     end;
 show_tx_summary(J, _Item) ->
     {ok, "Command not understood", J}.
-
-show_block(J, #{name := "height", value := Height}, _Value) ->
-    case aeapi:generation_by_height(Height) of
-        {ok, Generation} ->
-            {ok, ecli:format(Generation), J};
-        {error, Reason} ->
-            {ok, Reason, J}
-    end.
 
 set_gas_price(J, [#{name := "minimum_gas_price", value := NewGasPrice}], _Value) ->
     Config = #{<<"mining">> => #{ <<"min_miner_gas_price">> => NewGasPrice}},
