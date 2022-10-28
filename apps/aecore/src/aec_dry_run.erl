609f633ba2fc1691a096496142e4f7b06c195c44
seanhinde <sean.hinde@mac.com>
2022-10-28 09:59:05 +0200

    Allow options tx_events and {tx_events, true}
    Co-authored-by: Ulf Wiger <ulf@wiger.net>

--- apps/aecore/src/aec_dry_run.erl
+++ apps/aecore/src/aec_dry_run.erl
@@ -96,7 +96,7 @@ dry_run_int([], _Trees, Env, _Opts, Acc) ->
 dry_run_int([{tx, TxOpts, Tx} | Txs], Trees, Env, Opts, Acc) ->
     Stateless = proplists:get_value(stateless, TxOpts, false),
     Env1 = prepare_env(Env, TxOpts),
-    EventsEnabled = lists:member(tx_events, Opts),
+    EventsEnabled = proplists:get_bool(tx_events, Opts),
     %% GH3283: Here we should collect and present the `internal_call_tx` events.
     %% This means expanding the return type, and breaking the api :scream_cat:.
     case aec_trees:apply_txs_on_state_trees([Tx], Trees, Env1, [strict, dont_verify_signature|Opts]) of
