84d6e084e95e383ecd8cf68d3748d0dff60c057f
Hans Svensson <hanssv@gmail.com>
2023-11-22 15:24:50 +0100

    Generalize test fixes to aect_test_utils

--- apps/aecontract/test/aecontract_SUITE.erl
+++ apps/aecontract/test/aecontract_SUITE.erl
@@ -4506,37 +4506,10 @@ sophia_fate_signatures_aens(Cfg) ->
 
 delegate_sign(Type, Material, KeyHolder) ->
     PrivKey  = aect_test_utils:priv_key(KeyHolder, state()),
-    delegate_sign_(Type, Material, PrivKey).
+    ?sig(aect_test_utils:delegation_signature(Type, Material, PrivKey)).
 
 delegate_sign_(Type, Material, PrivKey) ->
-    SigData =
-        case vm_version() of
-            VM when VM =< ?VM_FATE_SOPHIA_2 ->
-                aec_governance:add_network_id(delegate_sign_old(Type, Material));
-            _ ->
-                delegate_sign_new(Type, Material)
-        end,
-    ?sig(enacl:sign_detached(SigData, PrivKey)).
-    %% MaterialForNetworkId = aec_governance:add_network_id(Material),
-    %% ?sig(enacl:sign_detached(MaterialForNetworkId, PrivKey)).
-
-delegate_sign_old(aens_wild, {A, C}) -> <<A/binary, "AENS"/utf8, C/binary>>;
-delegate_sign_old(_, {A, B})         -> <<A/binary, B/binary>>;
-delegate_sign_old(_, {A, B, C})      -> <<A/binary, B/binary, C/binary>>.
-
-delegate_sign_new(aens_wild, {A, C}) ->
-    aeser_del(aens_sig, {aeser_id:create(account, A), aeser_id:create(contract, C)});
-delegate_sign_new(aens_preclaim, {A, C}) ->
-    aeser_del(aens_preclaim_sig, {aeser_id:create(account, A), aeser_id:create(contract, C)});
-delegate_sign_new(aens_name, {A, N, C}) ->
-    aeser_del(aens_name_sig, {aeser_id:create(account, A), aeser_id:create(name, N), aeser_id:create(contract, C)});
-delegate_sign_new(oracle, {A, C}) ->
-    aeser_del(oracle_sig, {aeser_id:create(account, A), aeser_id:create(contract, C)});
-delegate_sign_new(oracle_response, {Q, C}) ->
-    aeser_del(oracle_response_sig, {aeser_id:create(oracle, Q), aeser_id:create(contract, C)}).
-
-aeser_del(Fun, {A, B})    -> aeser_delegation:Fun(aec_governance:get_network_id(), A, B);
-aeser_del(Fun, {A, B, C}) -> aeser_delegation:Fun(aec_governance:get_network_id(), A, B, C).
+    ?sig(aect_test_utils:delegation_signature(Type, Material, PrivKey)).
 
 %% Testing map functions and primitives
 sophia_maps(_Cfg) ->
