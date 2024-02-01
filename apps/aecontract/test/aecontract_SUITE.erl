3a60c6429f16c77c281647d14de369dbe4bc55c2
Hans Svensson <hanssv@gmail.com>
2024-02-01 11:34:38 +0100

    Bump AEVM test timeout

--- apps/aecontract/test/aecontract_SUITE.erl
+++ apps/aecontract/test/aecontract_SUITE.erl
@@ -5933,7 +5933,7 @@ sophia_compiler_version(_Cfg) ->
     {code, Code} = aect_contracts:code(C),
     CMap = aeser_contract_code:deserialize(Code),
     ?assertMatchProtocol(maps:get(compiler_version, CMap, undefined),
-                         undefined, <<"2.1.0">>, <<"3.2.0">>, <<"unknown">>, <<"6.1.0">>, <<"8.0.0">>),
+                         undefined, <<"2.1.0">>, <<"3.2.0">>, <<"unknown">>, <<"6.1.0">>, <<"8.0.0-rc1">>),
     ok.
 
 sophia_protected_call(_Cfg) ->
