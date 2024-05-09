a06e9b169480ec0a749b4b254415ec8862e39843
Denis Davidyuk <denis_davidyuk@hotmail.com>
2024-05-09 18:58:57 +1000

    Update sdk in sdk_smoke_test to 13.3.2

--- sdk_smoke_test/aepp-sdk-js/test_integration.js
+++ sdk_smoke_test/aepp-sdk-js/test_integration.js
@@ -1,30 +1,20 @@
-const { AeSdk, Node, MemoryAccount } = require('@aeternity/aepp-sdk')
-const ignoreVersion = process.env.FORCE_COMPATIBILITY || true
+import { AeSdk, Node, MemoryAccount } from '@aeternity/aepp-sdk';
 
-const f = async () => {
-    console.log("Starting JS SDK smoke test")
-    const node = new Node("http://localhost:3013", { ignoreVersion });
+const ignoreVersion = process.env.FORCE_COMPATIBILITY === "true";
 
-    const sdk = new AeSdk({
-        nodes: [{name: "ae_sdk_smoke_test", instance: node}]
-    });
-    await sdk.addAccount(
-        new MemoryAccount({
-            keypair: {
-                publicKey: "ak_2a1j2Mk9YSmC1gioUq4PWRm3bsv887MbuRVwyv4KaUGoR1eiKi",
-                secretKey: "e6a91d633c77cf5771329d3354b3bcef1bc5e032c43d70b6d35af923ce1eb74dcea7ade470c9f99d9d4e400880a86f1d49bb444b62f11a9ebb64bbcfeb73fef3",
-            }
-        })
-    );
+console.log("Starting JS SDK smoke test");
+const node = new Node("http://localhost:3013", { ignoreVersion });
 
-    const balance = await sdk.getBalance("ak_2a1j2Mk9YSmC1gioUq4PWRm3bsv887MbuRVwyv4KaUGoR1eiKi")
-    console.log("Patron has " + balance + " aettos")
-    if(balance === "10000000000000000000000000000000") {
-        console.log("JS SDK smoke test passed");
-        process.exit()
-    } else {
-        console.log("JS SDK smoke test failed");
-        process.exit(1);
-    }
-}
-f()
+const sdk = new AeSdk({
+    nodes: [{ name: "ae_sdk_smoke_test", instance: node }],
+    accounts: [
+        new MemoryAccount("e6a91d633c77cf5771329d3354b3bcef1bc5e032c43d70b6d35af923ce1eb74dcea7ade470c9f99d9d4e400880a86f1d49bb444b62f11a9ebb64bbcfeb73fef3"),
+    ],
+});
+
+const balance = await sdk.getBalance(sdk.address);
+if (balance !== "10000000000000000000000000000000") {
+    throw new Error(`Unexpected balance: ${balance}`);
+};
+
+await sdk.spend(1e18, MemoryAccount.generate().address);
