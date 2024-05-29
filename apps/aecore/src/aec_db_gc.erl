eef495a8b7879d0b41f71289ed003232bf4dffcc
Hans Svensson <hanssv@gmail.com>
2024-05-29 15:02:38 +0200

    Log in main log file, not GC

--- apps/aecore/src/aec_db_gc.erl
+++ apps/aecore/src/aec_db_gc.erl
@@ -621,19 +621,19 @@ db_safe_access_scan(Tree) ->
     {ok, Hash} = aec_blocks:hash_internal_representation(Block),
     case get_mpt(Tree, Hash) of
         empty ->
-            ?DBG("Tree ~p empty at height ~p - no scan needed", [Tree, Height]),
+            lager:info("Tree ~p empty at height ~p - no scan needed", [Tree, Height]),
             {ok, 0};
         MPT ->
-            ?DBG("DB safe access FULL_SCAN for ~p at height ~p from root hash ~w",
+            lager:info("DB safe access FULL_SCAN for ~p at height ~p from root hash ~w",
                  [Tree, Height, aeu_mp_trees:root_hash(MPT)]),
             OldDBSafeAccess = persistent_term:get({aec_db, db_safe_access}, undefined),
             [ persistent_term:put({aec_db, db_safe_access}, true) || OldDBSafeAccess /= true ],
             try
                 N = visit_reachable(MPT),
-                ?DBG("DB safe access FULL_SCAN for ~p completed, checked ~p nodes", [Tree, N]),
+                lager:info("DB safe access FULL_SCAN for ~p completed, checked ~p nodes", [Tree, N]),
                 {ok, N}
             catch E:{Reason, _S} ->
-                ?DBG("DB safe access FULL_SCAN on ~p failed ~p:~p", [Tree, E, Reason]),
+                lager:info("DB safe access FULL_SCAN on ~p failed ~p:~p", [Tree, E, Reason]),
                 {error, Reason}
             after
                 case OldDBSafeAccess of
