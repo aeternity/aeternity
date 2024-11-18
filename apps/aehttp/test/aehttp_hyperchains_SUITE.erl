eaf4a8078ba6402d5d0996bd3d9cf568689a7997
Richard Carlsson <carlsson.richard@gmail.com>
2024-11-18 15:00:09 +0100

    Restore old value of child block time

--- apps/aehttp/test/aehttp_hyperchains_SUITE.erl
+++ apps/aehttp/test/aehttp_hyperchains_SUITE.erl
@@ -49,7 +49,7 @@
 -define(HC_CONTRACT, "HCElection").
 -define(CONSENSUS, hc).
 -define(CHILD_EPOCH_LENGTH, 10).
--define(CHILD_BLOCK_TIME, 400).
+-define(CHILD_BLOCK_TIME, 200).
 -define(CHILD_BLOCK_PRODUCTION_TIME, 80).
 -define(PARENT_EPOCH_LENGTH, 3).
 -define(PARENT_FINALITY, 2).
