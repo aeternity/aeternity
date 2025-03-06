974f9d72a739e0024770534e5acd800bb4503222
Justin Mitchell <mitchelli@users.noreply.github.com>
2025-03-06 19:21:54 +0100

    Add a behaviour to state machine

--- apps/aecore/src/aec_eoe_fork_vote.erl
+++ apps/aecore/src/aec_eoe_fork_vote.erl
@@ -6,19 +6,26 @@
 %%% -------------------------------------------------------------------
 
 -module(aec_eoe_fork_vote).
+-behaviour(aec_eoe_gen_vote).
 
 %% Export API functions
 -export([start_link/2, negotiate/7, get_finalize_transaction/1, add_parent_block/2]).
+
+%% Export aec_eoe_gen_vote callbacks
+-export([init/1]).
 
 -define(PROPOSAL_TYPE, 1).
 -define(VOTE_TYPE, 2).
 -define(COMMIT_TYPE, 3).
 
+
+-record(data, {}).
+
 %% API to start the state machine
 -spec start_link(#{binary() => binary()}, non_neg_integer())  -> {ok, pid()} | {error, atom()}.
 start_link(Stakers, BlockTime) ->
     VoteTypes = #{proposal => ?PROPOSAL_TYPE, vote => ?VOTE_TYPE, commit => ?COMMIT_TYPE},
-    aec_eoe_gen_vote:start_link(fork, VoteTypes, Stakers, BlockTime).
+    aec_eoe_gen_vote:start_link(fork, ?MODULE, VoteTypes, Stakers, BlockTime).
 
 %% Negotiate a fork, called with preferred fork and epoch length delta
 -spec negotiate(non_neg_integer(), non_neg_integer(), binary(), aec_keys:pubkey(), [{binary(), non_neg_integer()}], binary(), non_neg_integer()) -> ok.
@@ -33,3 +40,5 @@ add_parent_block(Epoch, ParentBlock) ->
 get_finalize_transaction(Trees) ->
     aec_eoe_gen_vote:get_finalize_transaction(fork, Trees).
 
+init(_Args) ->
+    #data{}.
