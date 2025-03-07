9adf5f2875162139647fdc58302867c0e6b9a8bc
Justin Mitchell <mitchelli@users.noreply.github.com>
2025-03-07 17:42:38 +0100

    Move contract call

--- apps/aecore/src/aec_eoe_fork_vote.erl
+++ apps/aecore/src/aec_eoe_fork_vote.erl
@@ -12,7 +12,7 @@
 -export([start_link/2, negotiate/7, get_finalize_transaction/1, add_parent_block/2]).
 
 %% Export aec_eoe_gen_vote callbacks
--export([init/1, init_state/6, reset_state/1, create_proposal/2, create_vote/3]).
+-export([init/1, init_state/6, reset_state/1, create_proposal/2, create_vote/3, finalize_call/2, vote_params/1]).
 
 %%% aec_eoe_gen_vote callbacks
 
@@ -24,8 +24,11 @@
 -define(EPOCH_DELTA_FLD, <<"epoch_length_delta">>).
 
 
+-define(FINALIZE_FUN_NAME, "finalize_epoch").
+
 -record(data, {
                 fork_hash                  :: binary() | undefined,
+                length                     :: non_neg_integer() | undefined,
                 length_delta               :: non_neg_integer() | undefined
         }).
 
@@ -48,13 +51,16 @@ add_parent_block(Epoch, ParentBlock) ->
 get_finalize_transaction(Trees) ->
     aec_eoe_gen_vote:get_finalize_transaction(fork, Trees).
 
+
+%%% aec_eoe_gen_vote callbacks
+
 init(_Args) ->
     #data{}.
 
 init_state(Epoch, Hash, ParentBlocks, CurrentLength, BlockTime, Data) ->
     LengthDelta = calculate_delta(Epoch, ParentBlocks, CurrentLength, BlockTime),
     lager:debug("Suggesting delta ~p for epoch ~p", [LengthDelta, Epoch]),
-    Data#data{fork_hash=Hash, length_delta=LengthDelta}.
+    Data#data{fork_hash=Hash, length=CurrentLength, length_delta=LengthDelta}.
 
 reset_state(_Data) ->
     #data{}.
@@ -78,7 +84,11 @@ create_vote(ProposalFields, VoteFlds0, #data{fork_hash=Hash, length_delta=Length
             {error, hash_mismatch}
     end.
 
+finalize_call(#{?HASH_FLD := Hash, ?EPOCH_DELTA_FLD := EpochDelta}, #data{length=EpochLength}) ->
+    {?FINALIZE_FUN_NAME, [{bytes, Hash}, EpochLength + EpochDelta]}.
 
+vote_params(#{?HASH_FLD := Hash, ?EPOCH_DELTA_FLD := EpochDelta}) ->
+    [{bytes, Hash}, EpochDelta].
 
 %% The first three epochs have the same seed
 calculate_delta(Epoch, _ParentBlocks, _CurrentLength, _BlockTime) when Epoch =< 4 ->
@@ -106,4 +116,3 @@ get_block_time_diff(ParentBlock1, ParentBlock2, ExpectedTimeDiff) when ParentBlo
 get_block_time_diff(ParentBlock1, ParentBlock2, _) ->
     aec_parent_chain_block:time(ParentBlock2) - aec_parent_chain_block:time(ParentBlock1).
 
-%%% aec_eoe_gen_vote callbacks
