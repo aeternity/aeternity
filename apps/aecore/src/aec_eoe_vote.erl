b55cef8b603f1e6368e4c984954c7af8b48dd9ef
Justin Mitchell <mitchelli@users.noreply.github.com>
2025-03-10 19:50:26 +0100

    Interim version of module vote split

--- apps/aecore/src/aec_eoe_vote.erl
+++ apps/aecore/src/aec_eoe_vote.erl
@@ -6,26 +6,41 @@
 %%% -------------------------------------------------------------------
 
 -module(aec_eoe_vote).
+-behaviour(supervisor).
 
 %% Export API functions
 -export([start_link/2, negotiate/7, get_finalize_transaction/1, add_parent_block/2]).
 
+%% Supervisor callbacks
+-export([init/1]).
 
-%% API to start the state machine
+
+%% API to start the supervisor
 -spec start_link(#{binary() => binary()}, non_neg_integer())  -> {ok, pid()} | {error, atom()}.
 start_link(Stakers, BlockTime) ->
-    aec_eoe_fork_vote:start_link(Stakers, BlockTime).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [Stakers, BlockTime]).
 
 %% Negotiate a fork, called with preferred fork and epoch length delta
 -spec negotiate(non_neg_integer(), non_neg_integer(), binary(), aec_keys:pubkey(), [{binary(), non_neg_integer()}], binary(), non_neg_integer()) -> ok.
 negotiate(Epoch, Height, Hash, Leader, Validators, Seed, CurrentLength) ->
-    aec_eoe_fork_vote:negotiate(Epoch, Height, Hash, Leader, Validators, Seed, CurrentLength).
+    call_children(fun(Module) -> Module:negotiate(Epoch, Height, Hash, Leader, Validators, Seed, CurrentLength) end).
 
 -spec add_parent_block(non_neg_integer(), aec_parent_chain_block:block()) -> ok.
 add_parent_block(Epoch, ParentBlock) ->
-    aec_eoe_fork_vote:add_parent_block(Epoch, ParentBlock).
+    call_children(fun(Module) -> Module:add_parent_block(Epoch, ParentBlock) end).
 
 -spec get_finalize_transaction(aec_trees:trees()) -> {ok, aetx_sign:signed_tx()} | {error, not_ready} | {error, term()}.
 get_finalize_transaction(Trees) ->
     aec_eoe_fork_vote:get_finalize_transaction(Trees).
 
+init(Args) ->
+    ChildSpecs = [child_spec(aec_eoe_fork_vote, Args), child_spec(aec_eoe_length_vote, Args)],
+    {ok, {{one_for_one, 3, 30}, ChildSpecs}}.
+
+child_spec(Mod, Args) ->
+    {Mod, {Mod, start_link, Args}, permanent, 3000, worker, [Mod]}.
+
+call_children(Function) ->
+    Children = supervisor:which_children(?MODULE),
+    lists:foreach(fun({_Id, _Pid, _Type, [Module]}) ->
+        Function(Module) end, Children).
